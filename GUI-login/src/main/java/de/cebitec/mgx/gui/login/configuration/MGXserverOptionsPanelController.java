6a9a96d429d880e0433f56df7877f26a3b7c3e5b
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2011-11-01 16:01:00 +0000

    initial GUI import
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@11 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ GUI-login/src/main/java/de/cebitec/mgx/gui/login/configuration/MGXserverOptionsPanelController.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.login.configuration;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+@OptionsPanelController.TopLevelRegistration(categoryName = "#OptionsCategory_Name_MGXserver",
+iconBase = "de/cebitec/mgx/gui/login/configuration/Computer.png",
+keywords = "#OptionsCategory_Keywords_MGXserver",
+keywordsCategory = "MGXserver")
+public final class MGXserverOptionsPanelController extends OptionsPanelController {
+
+    private MGXserverPanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    @Override
+    public void update() {
+        getPanel().load();
+        changed = false;
+    }
+
+    @Override
+    public void applyChanges() {
+        getPanel().store();
+        changed = false;
+    }
+
+    @Override
+    public void cancel() {
+        // need not do anything special, if no changes have been persisted yet
+    }
+
+    @Override
+    public boolean isValid() {
+        return getPanel().valid();
+    }
+
+    @Override
+    public boolean isChanged() {
+        return changed;
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return null; // new HelpCtx("...ID") if you have a help set
+    }
+
+    @Override
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private MGXserverPanel getPanel() {
+        if (panel == null) {
+            panel = new MGXserverPanel(this);
+        }
+        return panel;
+    }
+
+    void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+}
