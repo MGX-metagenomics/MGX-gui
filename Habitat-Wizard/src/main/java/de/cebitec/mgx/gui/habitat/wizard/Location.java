d3fd06e643b434157301122ea81e4ddbd8e58ec2
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2011-11-23 15:58:36 +0000

    added habitatwizard subproject
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@137 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ Habitat-Wizard/src/main/java/de/cebitec/mgx/gui/habitat/wizard/Location.java
@@ -0,0 +1,46 @@
+package de.cebitec.mgx.gui.habitat.wizard;
+
+import org.jdesktop.swingx.mapviewer.GeoPosition;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class Location {
+
+    private GeoPosition geoLoc;
+    private String name;
+
+    public Location(GeoPosition pgeoLoc, String pname) {
+        geoLoc = pgeoLoc;
+        name = pname;
+    }
+
+    /**
+     * @return the geoLoc
+     */
+    public GeoPosition getGeoLoc() {
+        return geoLoc;
+    }
+
+    /**
+     * @param geoLoc the geoLoc to set
+     */
+    public void setGeoLoc(GeoPosition geoLoc) {
+        this.geoLoc = geoLoc;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
