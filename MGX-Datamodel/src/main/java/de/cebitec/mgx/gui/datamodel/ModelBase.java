fed4b5353c80f1c989ca23f708522c6ba05e3e80
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2011-11-28 16:14:56 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@187 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/ModelBase.java
@@ -0,0 +1,27 @@
+package de.cebitec.mgx.gui.datamodel;
+
+/**
+ *
+ * @author sjaenick
+ */
+public abstract class ModelBase {
+
+    protected Long id;
+    protected MGXMasterI master;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public MGXMasterI getMaster() {
+        return master;
+    }
+
+//    public void setMaster(MGXMasterI master) {
+//        this.master = master;
+//    }
+}
