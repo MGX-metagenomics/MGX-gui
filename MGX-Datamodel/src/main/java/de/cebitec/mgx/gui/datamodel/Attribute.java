47b8b72cbfcc3aa125814eafb244a0ba8a4966f9
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-01-05 16:40:51 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@385 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/Attribute.java
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/Attribute.java
@@ -1,4 +1,3 @@
-
 package de.cebitec.mgx.gui.datamodel;
 
 /**
@@ -28,22 +27,47 @@ public class Attribute extends ModelBase {
         return this;
     }
 
+//    @Override
+//    public int hashCode() {
+//        int hash = 0;
+//        hash += (id != null ? id.hashCode() : 0);
+//        return hash;
+//    }
+//
+//    @Override
+//    public boolean equals(Object object) {
+//        if (!(object instanceof Attribute)) {
+//            return false;
+//        }
+//        Attribute other = (Attribute) object;
+//        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+//            return false;
+//        }
+//        return true;
+//    }
     @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof Attribute)) {
+    public boolean equals(Object obj) {
+        if (obj == null) {
             return false;
         }
-        Attribute other = (Attribute) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Attribute other = (Attribute) obj;
+        if ((this.type == null) ? (other.type != null) : !this.type.equals(other.type)) {
+            return false;
+        }
+        if ((this.value == null) ? (other.value != null) : !this.value.equals(other.value)) {
             return false;
         }
         return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + (this.type != null ? this.type.hashCode() : 0);
+        hash = 97 * hash + (this.value != null ? this.value.hashCode() : 0);
+        return hash;
     }
 }
