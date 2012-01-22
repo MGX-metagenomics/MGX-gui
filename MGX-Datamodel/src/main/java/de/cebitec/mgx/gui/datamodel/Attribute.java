fac3fcea4d4fa9f83e07b968568001876d4bdb10
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-01-22 17:13:02 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@409 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/Attribute.java
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/Attribute.java
@@ -5,17 +5,18 @@ package de.cebitec.mgx.gui.datamodel;
  * @author sjaenick
  */
 public class Attribute extends ModelBase {
-
-    protected Long type_id;
+    
+    protected AttributeType type;
     protected String value;
     protected Long job_id;
 
-    public Long getTypeId() {
-        return type_id;
+    
+    public AttributeType getTypeId() {
+        return type;
     }
 
-    public Attribute setType(Long typeId) {
-        this.type_id = typeId;
+    public Attribute setType(AttributeType type) {
+        this.type = type;
         return this;
     }
 
@@ -37,33 +38,8 @@ public class Attribute extends ModelBase {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Attribute other = (Attribute) obj;
-        if (this.type_id != other.type_id && (this.type_id == null || !this.type_id.equals(other.type_id))) {
-            return false;
-        }
-        if ((this.value == null) ? (other.value != null) : !this.value.equals(other.value)) {
-            return false;
-        }
-        if (this.job_id != other.job_id && (this.job_id == null || !this.job_id.equals(other.job_id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 59 * hash + (this.type_id != null ? this.type_id.hashCode() : 0);
-        hash = 59 * hash + (this.value != null ? this.value.hashCode() : 0);
-        hash = 59 * hash + (this.job_id != null ? this.job_id.hashCode() : 0);
-        return hash;
+    public String toString() {
+        return value;
     }
 
 }
