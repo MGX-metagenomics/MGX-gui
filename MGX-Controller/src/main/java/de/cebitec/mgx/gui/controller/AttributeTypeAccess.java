b2a2c4ff0095aa31ab869f1503ca89063ebbe258
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-01-26 17:03:46 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@416 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/controller/AttributeTypeAccess.java
@@ -0,0 +1,77 @@
+package de.cebitec.mgx.gui.controller;
+
+import de.cebitec.mgx.client.exception.MGXClientException;
+import de.cebitec.mgx.client.exception.MGXServerException;
+import de.cebitec.mgx.dto.dto.AttributeTypeDTO;
+import de.cebitec.mgx.gui.datamodel.AttributeType;
+import de.cebitec.mgx.gui.datamodel.Job;
+import de.cebitec.mgx.gui.dtoconversion.AttributeTypeDTOFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class AttributeTypeAccess extends AccessBase<AttributeType> {
+
+    @Override
+    public Long create(AttributeType obj) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public AttributeType fetch(Long id) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public List<AttributeType> fetchall() {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public void update(AttributeType obj) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public void delete(Long id) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    public List<AttributeType> BySeqRun(Long id) {
+        System.err.println("fetch atypes ");
+        List<AttributeType> all = new ArrayList<AttributeType>();
+        try {
+            for (AttributeTypeDTO dto : getDTOmaster().AttributeType().BySeqRun(id)) {
+                AttributeType aType = AttributeTypeDTOFactory.getInstance().toModel(dto);
+                aType.setMaster(getMaster());
+                all.add(aType);
+
+                System.err.println("recv " + aType.getName());
+            }
+        } catch (MGXServerException ex) {
+            Logger.getLogger(DNAExtractAccess.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MGXClientException ex) {
+            Logger.getLogger(DNAExtractAccess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return all;
+    }
+
+    public List<AttributeType> ByJob(Job job) {
+        List<AttributeType> all = new ArrayList<AttributeType>();
+        try {
+            for (AttributeTypeDTO dto : getDTOmaster().AttributeType().ByJob(job.getId())) {
+                AttributeType aType = AttributeTypeDTOFactory.getInstance().toModel(dto);
+                aType.setMaster(getMaster());
+                all.add(aType);
+            }
+        } catch (MGXServerException ex) {
+            Logger.getLogger(DNAExtractAccess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return all;
+    }
+}
