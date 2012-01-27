f84d5dce2d573aeb288c5c8c821a6cb57d640b50
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-01-27 17:03:54 +0000

    added sorting and toString methods, which are used by the visualization module
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@419 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/AttributeType.java
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/AttributeType.java
@@ -4,7 +4,7 @@ package de.cebitec.mgx.gui.datamodel;
  *
  * @author sjaenick
  */
-public class AttributeType extends ModelBase {
+public class AttributeType extends ModelBase implements Comparable<AttributeType> {
     
     protected String name;
     protected String value_type;
@@ -24,4 +24,49 @@ public class AttributeType extends ModelBase {
     public void setValueType(String value_type) {
         this.value_type = value_type;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AttributeType other = (AttributeType) obj;
+        if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
+            return false;
+        }
+        if ((this.value_type == null) ? (other.value_type != null) : !this.value_type.equals(other.value_type)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + (this.name != null ? this.name.hashCode() : 0);
+        hash = 97 * hash + (this.value_type != null ? this.value_type.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + value_type + ")";
+    }
+
+    @Override
+    public int compareTo(AttributeType o) {
+        // compareTo returns an int which is 0 if the two strings are identical, 
+        // positive if s1 > s2, and negative if s1 < s2.
+        
+        int ret = this.value_type.compareTo(o.value_type);
+        if (ret == 0) {
+            return this.name.compareTo(o.name);
+        }
+        return ret;
+    }
+    
+    
 }
