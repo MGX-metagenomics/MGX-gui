bfa0ca5ebc88718458596704b25ed41a37790048
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-03-31 15:10:49 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@437 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- GUI-login/src/main/java/de/cebitec/mgx/gui/login/dialog/LoginAction.java
+++ GUI-login/src/main/java/de/cebitec/mgx/gui/login/dialog/LoginAction.java
@@ -2,10 +2,10 @@ package de.cebitec.mgx.gui.login.dialog;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import org.openide.awt.ActionRegistration;
+import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
-import org.openide.awt.ActionID;
+import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(category = "File",
