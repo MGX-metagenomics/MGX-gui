04e9d4a411362a45272d25bb53aca330ad8d5138
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-05-10 11:01:41 +0000

    added job parameter access
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@476 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/JobParameter.java
@@ -0,0 +1,81 @@
+package de.cebitec.mgx.gui.datamodel;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class JobParameter {
+
+    long node_id;
+    String user_name;
+    String user_desc;
+    String configitem_name;
+    String configitem_value;
+    String type;
+    boolean optional;
+    String default_value = "";
+
+    public String getConfigItemName() {
+        return configitem_name;
+    }
+
+    public void setConfigItemName(String configitem_name) {
+        this.configitem_name = configitem_name;
+    }
+
+    public String getConfigItemValue() {
+        return configitem_value;
+    }
+
+    public void setConfigItemValue(String configitem_value) {
+        this.configitem_value = configitem_value;
+    }
+
+    public String getDefaultValue() {
+        return default_value;
+    }
+
+    public void setDefaultValue(String default_value) {
+        this.default_value = default_value;
+    }
+
+    public long getNodeId() {
+        return node_id;
+    }
+
+    public void setNodeId(long node_id) {
+        this.node_id = node_id;
+    }
+
+    public boolean isOptional() {
+        return optional;
+    }
+
+    public void setOptional(boolean optional) {
+        this.optional = optional;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUserDescription() {
+        return user_desc;
+    }
+
+    public void setUserDescription(String user_desc) {
+        this.user_desc = user_desc;
+    }
+
+    public String getUserName() {
+        return user_name;
+    }
+
+    public void setUserName(String user_name) {
+        this.user_name = user_name;
+    }
+}
--- MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/ModelBase.java
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/ModelBase.java
@@ -4,16 +4,15 @@ package de.cebitec.mgx.gui.datamodel;
  *
  * @author sjaenick
  */
-public abstract class ModelBase { //implements Transferable {
+public abstract class ModelBase {
     
     public final static long INVALID_IDENTIFIER = -1;
 
     protected long id = INVALID_IDENTIFIER;
     protected MGXMasterI master;
-    //private static DataFlavor nodeFlavor = null;
 
     public void setId(long id) {
-        assert this.id == -1; // prevent changing of internal ID field
+        assert this.id == INVALID_IDENTIFIER; // prevent changing of internal ID field
         this.id = id;
     }
 
@@ -29,34 +28,4 @@ public abstract class ModelBase { //implements Transferable {
         assert master == null; // prevent duplicate setting
         master = m;
     }
-
-//    public static DataFlavor getNodeFlavor() {
-//        if (nodeFlavor == null) {
-//            try {
-//                nodeFlavor = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType + ";class=" + ModelBase.class.getName());
-//            } catch (ClassNotFoundException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        return nodeFlavor;
-//    }
-//
-//    @Override
-//    public Transferable getTransferData(DataFlavor df) throws UnsupportedFlavorException, IOException {
-//        if (df == getNodeFlavor()) {
-//            return this;
-//        } else {
-//            throw new UnsupportedFlavorException(df);
-//        }
-//    }
-//
-//    @Override
-//    public DataFlavor[] getTransferDataFlavors() {
-//        return new DataFlavor[]{getNodeFlavor()};
-//    }
-//
-//    @Override
-//    public boolean isDataFlavorSupported(DataFlavor df) {
-//        return df == getNodeFlavor();
-//    }
 }
