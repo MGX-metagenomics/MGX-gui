e30583bb025450c45bb28b8a485ea3bb69487908
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-05-24 13:54:26 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@507 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Controller/src/main/java/de/cebitec/mgx/gui/dtoconversion/JobParameterDTOFactory.java
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/dtoconversion/JobParameterDTOFactory.java
@@ -1,7 +1,12 @@
 package de.cebitec.mgx.gui.dtoconversion;
 
+import de.cebitec.mgx.dto.dto.ChoicesDTO;
 import de.cebitec.mgx.dto.dto.JobParameterDTO;
+import de.cebitec.mgx.dto.dto.JobParameterDTO.Builder;
+import de.cebitec.mgx.dto.dto.KVPair;
 import de.cebitec.mgx.gui.datamodel.JobParameter;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 /**
  *
@@ -20,19 +25,35 @@ public class JobParameterDTOFactory extends DTOConversionBase<JobParameter, JobP
 
     @Override
     public JobParameterDTO toDTO(JobParameter p) {
-        JobParameterDTO.Builder b = JobParameterDTO.newBuilder()
+                Builder b = JobParameterDTO.newBuilder()
                 .setNodeId(p.getNodeId())
                 .setUserName(p.getUserName())
                 .setUserDesc(p.getUserDescription())
+                .setDisplayName(p.getDisplayName())
+                .setClassName(p.getClassName())
                 .setConfigitemName(p.getConfigItemName())
                 .setType(p.getType())
                 .setIsOptional(p.isOptional());
-
-        if (!"".equals(p.getDefaultValue())) {
-            b.setDefaultValue(p.getDefaultValue());
+        
+        // choices
+        if (p.getChoices() != null) {
+            ChoicesDTO.Builder choices = ChoicesDTO.newBuilder();
+            for (Entry<String, String> e : p.getChoices().entrySet()) {
+                KVPair kv = KVPair.newBuilder()
+                        .setKey(e.getKey())
+                        .setValue(e.getValue())
+                        .build();
+                choices.addEntry(kv);
+            }
+            b.setChoices(choices.build());
         }
+        
         if (p.getConfigItemValue() != null) {
             b.setConfigitemValue(p.getConfigItemValue());
+        }
+                
+        if (p.getDefaultValue() != null) {
+            b.setDefaultValue(p.getDefaultValue());
         }
         return b.build();
     }
@@ -43,16 +64,27 @@ public class JobParameterDTOFactory extends DTOConversionBase<JobParameter, JobP
         jp.setNodeId(dto.getNodeId());
         jp.setUserName(dto.getUserName());
         jp.setUserDescription(dto.getUserDesc());
+        jp.setDisplayName(dto.getDisplayName());
+        jp.setClassName(dto.getClassName());
         jp.setConfigItemName(dto.getConfigitemName());
         jp.setType(dto.getType());
         jp.setOptional(dto.getIsOptional());
-        if (dto.hasDefaultValue()) {
-            jp.setDefaultValue(dto.getDefaultValue());
+        
+        if (dto.hasChoices()) {
+            jp.setChoices(new HashMap<String, String>());
+            for (KVPair kv : dto.getChoices().getEntryList()) {
+                jp.getChoices().put(kv.getKey(), kv.getValue());
+            }
         }
+        
         if (dto.hasConfigitemValue()) {
             jp.setConfigItemValue(dto.getConfigitemValue());
         }
-
+        
+        if (dto.hasDefaultValue()) {
+            jp.setDefaultValue(dto.getDefaultValue());
+        }
+        
         return jp;
     }
 }
