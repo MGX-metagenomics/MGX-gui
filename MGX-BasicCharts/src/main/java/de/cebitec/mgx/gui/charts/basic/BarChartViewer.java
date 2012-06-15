9e375878d3c51ed438052261dc5eeae74eca5dda
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-06-15 17:14:12 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@531 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-BasicCharts/src/main/java/de/cebitec/mgx/gui/charts/basic/BarChartViewer.java
+++ MGX-BasicCharts/src/main/java/de/cebitec/mgx/gui/charts/basic/BarChartViewer.java
@@ -1,14 +1,16 @@
 package de.cebitec.mgx.gui.charts.basic;
 
-import de.cebitec.mgx.gui.groups.VisualizationGroup;
 import de.cebitec.mgx.gui.attributevisualization.viewer.CategoricalViewerI;
 import de.cebitec.mgx.gui.attributevisualization.viewer.ViewerI;
 import de.cebitec.mgx.gui.datamodel.Attribute;
 import de.cebitec.mgx.gui.datamodel.Distribution;
 import de.cebitec.mgx.gui.datamodel.Pair;
+import de.cebitec.mgx.gui.groups.VisualizationGroup;
 import java.awt.Color;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 import javax.swing.JComponent;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -16,6 +18,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.StandardChartTheme;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.LogarithmicAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
@@ -33,6 +36,7 @@ import org.openide.util.lookup.ServiceProvider;
 public class BarChartViewer extends CategoricalViewerI {
 
     private ChartPanel cPanel = null;
+    private BarChartCustomizer customizer = null;
 
     public BarChartViewer() {
     }
@@ -47,52 +51,76 @@ public class BarChartViewer extends CategoricalViewerI {
         return cPanel;
     }
 
+//    private List<Pair<VisualizationGroup, Distribution>> filter(List<Pair<VisualizationGroup, Distribution>> dists) {
+//
+//        if (getCustomizer().useFractions()) {
+//            VisFilterI fracFilter = new ToFractionFilter();
+//            dists = fracFilter.filter(dists);
+//        }
+//
+//        SortOrder sorter = new SortOrder(getAttributeType());
+//        dists = sorter.filter(dists);
+//
+//        return dists;
+//    }
+
     @Override
     public void show(List<Pair<VisualizationGroup, Distribution>> dists) {
 
-        boolean fractionMode = false;
+        dists = getCustomizer().filter(dists);
 
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
         for (Pair<VisualizationGroup, Distribution> groupDistribution : dists) {
+            Distribution d = groupDistribution.getSecond();
+            //Set<Entry<Attribute, Number>> sorted = groupDistribution.getSecond().entrySet();
+            
+//            if (getCustomizer().getSortAscending()) {
+//                Collections.reverse(d.keySet());
+//            }
 
-            List<Pair<Attribute, ? extends Number>> sorted = groupDistribution.getSecond().getSorted();
-            if (sortAscending()) {
-                Collections.reverse(sorted);
-            }
-
-            for (Pair<Attribute, ? extends Number> entry : sorted) {
-                dataset.addValue(entry.getSecond(), groupDistribution.getFirst().getName(), entry.getFirst().getValue());
-
-                // if we encounter any double values here, we are in fraction mode
-                fractionMode = entry.getSecond() instanceof Double || fractionMode;
+            for (Entry<Attribute, ? extends Number> entry : d.entrySet()) {
+                if (entry.getValue().doubleValue() <= 0.0) {
+                    System.err.println("error at "+entry.getKey().getValue());
+                }
+                dataset.addValue(entry.getValue(), groupDistribution.getFirst().getName(), entry.getKey().getValue());
             }
         }
-
         String xAxisLabel = "";
-        String yAxisLabel = fractionMode ? "Fraction" : "Count";
+        String yAxisLabel = getCustomizer().useFractions() ? "Fraction" : "Count";
 
         // disable the stupid glossy effect
         ChartFactory.setChartTheme(StandardChartTheme.createLegacyTheme());
-        BarRenderer.setDefaultShadowsVisible(false);
-        XYBarRenderer.setDefaultShadowsVisible(false);
+        BarRenderer.setDefaultShadowsVisible(
+                false);
+        XYBarRenderer.setDefaultShadowsVisible(
+                false);
 
         JFreeChart chart = ChartFactory.createBarChart(getTitle(), xAxisLabel, yAxisLabel, dataset, PlotOrientation.VERTICAL, true, true, false);
+
         chart.setBorderPaint(Color.WHITE);
+
         chart.setBackgroundPaint(Color.WHITE);
         cPanel = new ChartPanel(chart);
         CategoryPlot plot = chart.getCategoryPlot();
+
         plot.setBackgroundPaint(Color.WHITE);
         CategoryAxis domainAxis = plot.getDomainAxis();
+
         domainAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6));
 
+        if (getCustomizer().logY()) {
+            final NumberAxis rangeAxis = new LogarithmicAxis("log("+ yAxisLabel +")");
+            plot.setRangeAxis(rangeAxis);
+        }
+        
         NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
-        if (fractionMode) {
+        //plot.setRangeAxis(rangeAxis);
+        
+        if (getCustomizer().useFractions()) {
             rangeAxis.setStandardTickUnits(NumberAxis.createStandardTickUnits());
         } else {
             rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
         }
-
         // set the colors
         int i = 0;
         CategoryItemRenderer renderer = chart.getCategoryPlot().getRenderer();
@@ -104,5 +132,14 @@ public class BarChartViewer extends CategoricalViewerI {
     @Override
     public Class getInputType() {
         return Distribution.class;
+    }
+
+    @Override
+    public BarChartCustomizer getCustomizer() {
+        if (customizer == null) {
+            customizer = new BarChartCustomizer();
+            customizer.setAttributeType(getAttributeType());
+        }
+        return customizer;
     }
 }
--- MGX-BasicCharts/src/main/java/de/cebitec/mgx/gui/charts/basic/XYPlotViewer.java
+++ MGX-BasicCharts/src/main/java/de/cebitec/mgx/gui/charts/basic/XYPlotViewer.java
@@ -8,6 +8,7 @@ import de.cebitec.mgx.gui.datamodel.Pair;
 import de.cebitec.mgx.gui.groups.VisualizationGroup;
 import java.awt.Color;
 import java.util.List;
+import java.util.Map.Entry;
 import javax.swing.JComponent;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -29,6 +30,7 @@ import org.openide.util.lookup.ServiceProvider;
 public class XYPlotViewer extends NumericalViewerI {
 
     private ChartPanel cPanel = null;
+    private XYPlotCustomizer customizer = null;
 
     @Override
     public JComponent getComponent() {
@@ -42,24 +44,22 @@ public class XYPlotViewer extends NumericalViewerI {
 
     @Override
     public void show(List<Pair<VisualizationGroup, Distribution>> dists) {
-        boolean fractionMode = false;
+        
+        dists = getCustomizer().filter(dists);
 
         XYSeriesCollection dataset = new XYSeriesCollection();
 
         for (Pair<VisualizationGroup, Distribution> groupDistribution : dists) {
             XYSeries series = new XYSeries(groupDistribution.getFirst().getName());
 
-            for (Pair<Attribute, ? extends Number> entry : groupDistribution.getSecond().getSorted()) {
-                series.add(Double.parseDouble(entry.getFirst().getValue()), entry.getSecond());
-
-                // if we encounter any double values here, we are in fraction mode
-                fractionMode = entry.getSecond() instanceof Double || fractionMode;
+            for (Entry<Attribute, ? extends Number> entry : groupDistribution.getSecond().entrySet()) {
+                series.add(Double.parseDouble(entry.getKey().getValue()), entry.getValue());
             }
             dataset.addSeries(series);
         }
-        
+
         String xAxisLabel = "";
-        String yAxisLabel = fractionMode ? "Fraction" : "Count";
+        String yAxisLabel = getCustomizer().useFractions() ? "Fraction" : "Count";
 
         JFreeChart chart = ChartFactory.createXYLineChart(getTitle(), xAxisLabel, yAxisLabel, dataset, PlotOrientation.VERTICAL, true, true, false);
         chart.setBorderPaint(Color.WHITE);
@@ -67,18 +67,18 @@ public class XYPlotViewer extends NumericalViewerI {
         cPanel = new ChartPanel(chart);
         XYPlot plot = (XYPlot) chart.getPlot();
         plot.setBackgroundPaint(Color.WHITE);
-        
+
         ValueAxis valueAxis = plot.getDomainAxis();
-        valueAxis.setInverted(!sortAscending());
+        valueAxis.setInverted(!getCustomizer().getSortAscending());
         //valueAxis.    //setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6));
 
         NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
-        if (fractionMode) {
+        if (getCustomizer().useFractions()) {
             rangeAxis.setStandardTickUnits(NumberAxis.createStandardTickUnits());
         } else {
             rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
         }
-        
+
         // set the colors
         int i = 0;
         XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) plot.getRenderer();
@@ -86,6 +86,15 @@ public class XYPlotViewer extends NumericalViewerI {
             renderer.setSeriesPaint(i++, groupDistribution.getFirst().getColor());
         }
 
+    }
+
+    @Override
+    public XYPlotCustomizer getCustomizer() {
+        if (customizer == null) {
+            customizer = new XYPlotCustomizer();
+            customizer.setAttributeType(getAttributeType());
+        }
+        return customizer;
     }
 
     @Override
