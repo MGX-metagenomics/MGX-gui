61e1e85bdab040d04c4e725256a11715c91d9464
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-06-21 15:05:49 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@553 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/analysisaction/ExecuteAnalysisWorker/LocalToolWorker.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.nodes.analysisaction.ExecuteAnalysisWorker;
+
+import de.cebitec.mgx.gui.controller.MGXMaster;
+import de.cebitec.mgx.gui.datamodel.SeqRun;
+import de.cebitec.mgx.gui.datamodel.Tool;
+import de.cebitec.mgx.gui.wizard.configurations.action.WizardController;
+import de.cebitec.mgx.gui.wizard.configurations.progressscreen.ProgressBar;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.logging.Logger;
+import javax.swing.SwingWorker;
+
+/**
+ * Der LocalToolWorker laedt ein lokal vorliegendes Tool in das Projekt hoch.
+ *
+ * @author pbelmann
+ */
+public class LocalToolWorker extends SwingWorker implements ActionListener, WindowListener {
+
+    private final static Logger LOGGER =
+            Logger.getLogger(LocalToolWorker.class.getName());
+    /**
+     * Tool, welches hochgeladen werden soll.
+     */
+    private Tool tool;
+    /**
+     * ID des Tools.
+     */
+    private long tool_id;
+    /**
+     * GuiController.
+     */
+    private WizardController startUp;
+    /**
+     * Masterobjekt, welches fuer die Verbindung mit dem Server verantwortlich
+     * ist.
+     */
+    private MGXMaster master;
+    /**
+     * Sequenzierlauf.
+     */
+    private SeqRun seqRun;
+    private ProgressBar progress;
+
+    /**
+     * Konstruktor
+     *
+     * @param lTool lokales Tool.
+     * @param startup GuiController
+     * @param lMaster Masterobjekt.
+     * @param seqRun Sequenzierlauf.
+     */
+    public LocalToolWorker(Tool lTool, WizardController startup, MGXMaster lMaster,
+            SeqRun seqRun) {
+        tool = lTool;
+        this.seqRun = seqRun;
+        startUp = startup;
+        master = lMaster;
+    }
+
+    /**
+     * Methode, welche im Hintergrund arbeitet.
+     *
+     * @return Objekt.
+     */
+    @Override
+    protected Object doInBackground() {
+        LOGGER.info("localTool");
+        progress = new ProgressBar("Installing tool...",
+                300, 140);
+        tool_id = master.Tool().create(tool);
+        return null;
+    }
+
+    /**
+     * Methode die beim beenden des Swingworkers aufgerufen wird.
+     */
+    @Override
+    protected void done() {
+        LOGGER.info("localToolWorker");
+        if (tool != null) {
+            LOGGER.info("after localTool create TOOLID: " + tool.getId());
+        }
+
+        progress.setButton("The tool is installed and available in "
+                + "your project tools view.");
+
+//        ProgressBar bar = new ProgressBar();
+//        ShowParameterWorker showToolsWorker =
+//                new ShowParameterWorker(tool, startUp, master, seqRun);
+//        showToolsWorker.execute();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        GetToolsWorker worker = new GetToolsWorker(startUp, master, seqRun);
+        worker.execute();
+        progress.dispose();
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+
+        GetToolsWorker worker = new GetToolsWorker(startUp, master, seqRun);
+        worker.execute();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+    }
+}
---
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/analysisaction/ExecuteAnalysisWorker/ShowParameterWorker.java
@@ -0,0 +1,115 @@
+package de.cebitec.mgx.gui.nodes.analysisaction.ExecuteAnalysisWorker;
+
+import de.cebitec.mgx.client.exception.MGXServerException;
+import de.cebitec.mgx.gui.controller.MGXMaster;
+import de.cebitec.mgx.gui.datamodel.*;
+import de.cebitec.mgx.gui.wizard.configurations.action.MenuAction;
+import de.cebitec.mgx.gui.wizard.configurations.action.WizardController;
+import de.cebitec.mgx.gui.wizard.configurations.data.impl.Store;
+import de.cebitec.mgx.gui.wizard.configurations.data.util.Transform;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.swing.SwingWorker;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * ShowParameterWorker fuer die Anzeige von Parametern.
+ *
+ * @author pbelmann
+ */
+public class ShowParameterWorker extends SwingWorker {
+
+    private final static Logger LOGGER =
+            Logger.getLogger(ShowParameterWorker.class.getName());
+    Tool tool;
+    boolean globalToolIsPresent = false;
+    private long project_tool_id;
+    private Job job;
+    private long jobid;
+    private List<JobParameter> jobParameterList;
+    private SeqRun seqrun;
+    private Store store;
+    private WizardController startUp;
+    private MGXMaster master;
+    private ArrayList<JobParameter> list;
+
+    public ShowParameterWorker(Tool lTool,
+            WizardController startUp, MGXMaster master, SeqRun seqrun) {
+        this.master = master;
+        tool = lTool;
+        this.startUp = startUp;
+        this.seqrun = seqrun;
+    }
+
+    @Override
+    protected Object doInBackground() {
+
+//        master.Tool().delete(tool.getId());
+
+        job = new Job();
+        job.setSeqrun(seqrun);
+        job.setCreator(master.getLogin());
+        job.setTool(tool);
+//                    tool.setId(project_tool_id);
+
+        LOGGER.info("Before create JobID: " + jobid);
+
+        jobid = master.Job().create(job);
+
+        LOGGER.info("After create JobID: " + jobid);
+        jobParameterList = null;
+        //parsen
+        list = new ArrayList<JobParameter>();
+        Iterator iterator = null;
+
+        try {
+            iterator = master.Job().getParameters(jobid).iterator();
+            LOGGER.info("After Get Parameters");
+        } catch (MGXServerException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        if (!iterator.hasNext()) {
+            LOGGER.info("Iterator has no next");
+        }
+
+        while (iterator.hasNext()) {
+            list.add((JobParameter) iterator.next());
+        }
+
+        LOGGER.info("JobParameter: " + list.size());
+        Store store = Transform.getFromJobParameterNodeStore(list);
+        List<DirEntry> fetchall = master.File().fetchall();
+        LOGGER.info("after setStore");
+        if (list.size() > 0) {
+            this.store = startUp.startParameterConfiguration(store, fetchall,
+                    tool.getName());
+        }
+        return null;
+    }
+
+    @Override
+    protected void done() {
+        LOGGER.info("DONE SHOWTOOLWORKER");
+        if (list.size() > 0) {
+            if (startUp.getStatus() == MenuAction.again) {
+                LOGGER.info("Again SHOWTOOLWORKER");
+                GetToolsWorker worker = new GetToolsWorker(startUp, master, 
+                        seqrun);
+                worker.execute();
+            } else if (startUp.getStatus() == MenuAction.finish) {
+                jobParameterList = new ArrayList<JobParameter>();
+                jobParameterList =
+                        Transform.getFromNodeStoreJobParameter(store);
+                JobWorker worker = new JobWorker(jobParameterList, jobid, job, 
+                        master, startUp, seqrun);
+                worker.execute();
+                LOGGER.info("Finish SHOWTOOLWORKER");
+            } else if (startUp.getStatus() == MenuAction.cancel) {
+                LOGGER.info("Cancel SHOWTOOLWORKER");
+            }
+        }
+    }
+}
