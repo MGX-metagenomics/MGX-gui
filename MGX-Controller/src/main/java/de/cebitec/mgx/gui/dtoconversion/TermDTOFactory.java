74a9a1a106b15bee909860431ab8c6fe65598b61
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-06-21 15:29:00 +0000

    term conversion added
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@560 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/dtoconversion/TermDTOFactory.java
@@ -0,0 +1,29 @@
+package de.cebitec.mgx.gui.dtoconversion;
+
+import de.cebitec.mgx.dto.dto.TermDTO;
+import de.cebitec.mgx.gui.datamodel.Term;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class TermDTOFactory extends DTOConversionBase<Term, TermDTO>{
+
+    @Override
+    public TermDTO toDTO(Term a) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public Term toModel(TermDTO dto) {
+        Term t = new Term();
+        t.setId(dto.getId());
+        t.setName(dto.getName());
+        if (dto.hasParentId())
+            t.setParentId(dto.getParentId());
+        if (dto.hasDescription())
+            t.setDescription(dto.getDescription());
+        return t;
+    }
+    
+}
