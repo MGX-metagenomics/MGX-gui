c7d4fec0bd95324b9faf543521b65386c66a6302
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-07-02 18:37:13 +0000

    convert in-place
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@616 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-AttributeVisualization/src/main/java/de/cebitec/mgx/gui/attributevisualization/filter/ToFractionFilter.java
+++ MGX-AttributeVisualization/src/main/java/de/cebitec/mgx/gui/attributevisualization/filter/ToFractionFilter.java
@@ -4,10 +4,7 @@ import de.cebitec.mgx.gui.datamodel.Attribute;
 import de.cebitec.mgx.gui.datamodel.Distribution;
 import de.cebitec.mgx.gui.datamodel.Pair;
 import de.cebitec.mgx.gui.groups.VisualizationGroup;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 
 /**
@@ -18,26 +15,19 @@ public class ToFractionFilter implements VisFilterI<Distribution> {
 
     @Override
     public List<Pair<VisualizationGroup, Distribution>> filter(List<Pair<VisualizationGroup, Distribution>> dists) {
-        List<Pair<VisualizationGroup, Distribution>> ret = new ArrayList<>();
         for (Pair<VisualizationGroup, Distribution> pair : dists) {
-            VisualizationGroup vg = pair.getFirst();
-            Distribution dist = pair.getSecond();
-            ret.add(new Pair<>(vg, convertSingleDistribution(dist)));
+            convertSingleDistribution(pair.getSecond());
         }
-        
-        return ret;
+        return dists;
     }
 
     private Distribution convertSingleDistribution(Distribution dist) {
 
         // sum up
-        long sum = 0;
-        for (Entry<Attribute, ? extends Number> p : dist.entrySet()) {
-            sum += p.getValue().longValue();
-        }
+        long total = dist.getTotalClassifiedElements();
 
         for (Entry<Attribute, Number> e : dist.entrySet()) {
-            e.setValue((double)e.getValue().longValue() / sum);
+            e.setValue((double)e.getValue().longValue() / total);
         }
         
         return dist;
