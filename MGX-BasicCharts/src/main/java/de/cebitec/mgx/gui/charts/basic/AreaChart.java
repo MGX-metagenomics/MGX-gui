9ce221149473145c8e4f62815990839992667400
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-07-30 18:25:29 +0000

    xy area chart added
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@726 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-BasicCharts/src/main/java/de/cebitec/mgx/gui/charts/basic/AreaChart.java
@@ -0,0 +1,124 @@
+package de.cebitec.mgx.gui.charts.basic;
+
+import de.cebitec.mgx.gui.attributevisualization.viewer.NumericalViewerI;
+import de.cebitec.mgx.gui.attributevisualization.viewer.ViewerI;
+import de.cebitec.mgx.gui.charts.basic.util.JFreeChartUtil;
+import de.cebitec.mgx.gui.datamodel.misc.Distribution;
+import de.cebitec.mgx.gui.datamodel.misc.Pair;
+import de.cebitec.mgx.gui.groups.VisualizationGroup;
+import java.awt.Color;
+import java.util.List;
+import java.util.Locale;
+import javax.swing.JComponent;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.LogAxis;
+import org.jfree.chart.axis.LogarithmicAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.TickUnitSource;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author sj
+ */
+@ServiceProvider(service = ViewerI.class)
+public class AreaChart extends NumericalViewerI {
+
+    private ChartPanel cPanel = null;
+    private XYPlotCustomizer customizer = null;
+    private JFreeChart chart = null;
+
+    @Override
+    public JComponent getComponent() {
+        return cPanel;
+    }
+
+    @Override
+    public String getName() {
+        return "Area Chart";
+    }
+
+    @Override
+    public void show(List<Pair<VisualizationGroup, Distribution>> dists) {
+
+        dists = getCustomizer().filter(dists);
+        XYSeriesCollection dataset = JFreeChartUtil.createXYSeries(dists);
+
+        String xAxisLabel = "";
+        String yAxisLabel = getCustomizer().useFractions() ? "Fraction" : "Count";
+
+        chart = ChartFactory.createXYAreaChart(getTitle(), xAxisLabel, yAxisLabel, dataset, PlotOrientation.VERTICAL, true, true, false);
+
+        chart.setBorderPaint(Color.WHITE);
+        chart.setBackgroundPaint(Color.WHITE);
+        cPanel = new ChartPanel(chart);
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.WHITE);
+
+        // x axis
+        ValueAxis valueAxis;
+        final TickUnitSource tusX;
+        if (getCustomizer().logX()) {
+            valueAxis = new LogarithmicAxis("log(" + xAxisLabel + ")");
+            ((LogarithmicAxis) valueAxis).setStrictValuesFlag(false);
+            tusX = LogAxis.createLogTickUnits(Locale.getDefault());
+
+        } else {
+            valueAxis = (NumberAxis) plot.getDomainAxis();
+            tusX = NumberAxis.createIntegerTickUnits();
+        }
+        valueAxis.setStandardTickUnits(tusX);
+        valueAxis.setInverted(!getCustomizer().getSortAscending());
+        plot.setDomainAxis(valueAxis);
+
+
+        // y axis
+        final NumberAxis rangeAxis;
+        final TickUnitSource tus;
+        if (getCustomizer().logY()) {
+            rangeAxis = new LogarithmicAxis("log(" + yAxisLabel + ")");
+            ((LogarithmicAxis) rangeAxis).setStrictValuesFlag(false);
+            tus = LogAxis.createLogTickUnits(Locale.getDefault());
+
+        } else {
+            rangeAxis = (NumberAxis) plot.getRangeAxis();
+            if (getCustomizer().useFractions()) {
+                tus = NumberAxis.createStandardTickUnits();
+            } else {
+                tus = NumberAxis.createIntegerTickUnits();
+            }
+        }
+        rangeAxis.setStandardTickUnits(tus);
+        plot.setRangeAxis(rangeAxis);
+
+
+        // set the colors
+        int i = 0;
+        XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) plot.getRenderer();
+        for (Pair<VisualizationGroup, Distribution> groupDistribution : dists) {
+            renderer.setSeriesPaint(i++, groupDistribution.getFirst().getColor());
+        }
+
+    }
+
+    @Override
+    public XYPlotCustomizer getCustomizer() {
+        if (customizer == null) {
+            customizer = new XYPlotCustomizer();
+            customizer.setAttributeType(getAttributeType());
+        }
+        return customizer;
+    }
+
+    @Override
+    public Class getInputType() {
+        return Distribution.class;
+    }
+}
