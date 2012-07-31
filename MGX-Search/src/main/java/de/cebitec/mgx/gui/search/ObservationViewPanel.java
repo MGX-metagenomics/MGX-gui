99f5138fda65f7128fadfc22c3d765fede473873
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-07-31 15:51:20 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@735 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Search/src/main/java/de/cebitec/mgx/gui/search/ObservationViewPanel.java
+++ MGX-Search/src/main/java/de/cebitec/mgx/gui/search/ObservationViewPanel.java
@@ -1,11 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.cebitec.mgx.gui.search;
 
 import de.cebitec.mgx.gui.datamodel.Observation;
 import de.cebitec.mgx.gui.datamodel.Sequence;
+import java.awt.Graphics2D;
 import java.util.Collection;
 
 /**
@@ -13,6 +10,9 @@ import java.util.Collection;
  * @author sj
  */
 public class ObservationViewPanel extends javax.swing.JPanel {
+    
+    private final static int BORDER = 5;
+    private int seqLen;
 
     /**
      * Creates new form ObservationViewPanel
@@ -22,11 +22,20 @@ public class ObservationViewPanel extends javax.swing.JPanel {
     }
     
     public void setSequence(Sequence seq) {
-        readName.setText(seq.getName());
+        seqLen = seq.getLength();
+        readName.setText(seq.getName() + " ("+seq.getLength()+"bp)");
     }
     
     public void setObservations(Collection<Observation> obs) {
+        Graphics2D gfx = (Graphics2D) obsview.getGraphics();
+        assert gfx != null;
+        int height = obsview.getHeight();
+        int width = obsview.getWidth();
         
+        int midheight = height/2;
+        gfx.drawLine(BORDER, midheight, width - BORDER, midheight);
+        gfx.drawString("1", BORDER - 3, midheight + 5);
+        gfx.drawString(String.valueOf(seqLen), width - BORDER - 3, midheight + 5);
     }
 
     /**
@@ -39,8 +48,22 @@ public class ObservationViewPanel extends javax.swing.JPanel {
     private void initComponents() {
 
         readName = new javax.swing.JLabel();
+        obsview = new javax.swing.JPanel();
+
+        setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
         org.openide.awt.Mnemonics.setLocalizedText(readName, org.openide.util.NbBundle.getMessage(ObservationViewPanel.class, "ObservationViewPanel.readName.text")); // NOI18N
+
+        javax.swing.GroupLayout obsviewLayout = new javax.swing.GroupLayout(obsview);
+        obsview.setLayout(obsviewLayout);
+        obsviewLayout.setHorizontalGroup(
+            obsviewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 432, Short.MAX_VALUE)
+        );
+        obsviewLayout.setVerticalGroup(
+            obsviewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 120, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -48,18 +71,25 @@ public class ObservationViewPanel extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(readName)
-                .addContainerGap(448, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(obsview, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(readName)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(readName)
-                .addContainerGap(148, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(obsview, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel obsview;
     private javax.swing.JLabel readName;
     // End of variables declaration//GEN-END:variables
 }
