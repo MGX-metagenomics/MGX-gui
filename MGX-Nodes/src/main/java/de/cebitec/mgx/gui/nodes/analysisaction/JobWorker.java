7c96e5b2ec8671e784611acca78b73f5db4b957f
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-08-02 09:18:29 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@770 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/analysisaction/JobWorker.java
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/analysisaction/JobWorker.java
@@ -10,10 +10,8 @@ import de.cebitec.mgx.gui.datamodel.Job;
 import de.cebitec.mgx.gui.datamodel.JobParameter;
 import de.cebitec.mgx.gui.datamodel.SeqRun;
 import de.cebitec.mgx.gui.wizard.configurations.action.WizardController;
-import de.cebitec.mgx.gui.wizard.configurations.progressscreen.ProgressBar;
 import java.util.List;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
+import java.util.concurrent.ExecutionException;
 import javax.swing.SwingWorker;
 import org.openide.util.Exceptions;
 
@@ -22,49 +20,23 @@ import org.openide.util.Exceptions;
  *
  * @author pbelmann
  */
-public class JobWorker extends SwingWorker {
+public class JobWorker extends SwingWorker<Void, Void> {
 
-    private final static Logger LOGGER =
-            Logger.getLogger(LocalToolWorker.class.getName());
-    /**
-     * ParameterListe, die an den Server gesendet wird.
-     */
     private final List<JobParameter> jobParameterList;
-    /**
-     * Job Id des jobs.
-     */
-    private long jobId;
-    /**
-     * Job mit dem ausgewaehlten und bearbeiteten tool.
-     */
     private Job job;
-    /**
-     * Master objekt. der den Server aufruft.
-     */
     private MGXMaster master;
-    /**
-     * GuiController.
-     */
-    private WizardController startup;
-    private SeqRun run;
 
     /**
      * Konstruktor
      *
      * @param lParameter Parameter
-     * @param lJobId JobId
      * @param lJob Job mit dem Tool
      * @param lMaster Masterobjekt.
-     * @param lStartup GuiController
      */
-    public JobWorker(List<JobParameter> lParameter, long lJobId, Job lJob,
-            MGXMaster lMaster, WizardController lStartup, SeqRun lRun) {
-        run = lRun;
+    public JobWorker(List<JobParameter> lParameter, Job lJob, MGXMaster lMaster) {
         jobParameterList = lParameter;
-        jobId = lJobId;
         job = lJob;
         master = lMaster;
-        startup = lStartup;
     }
 
     /**
@@ -73,56 +45,14 @@ public class JobWorker extends SwingWorker {
      * @return
      */
     @Override
-    protected Object doInBackground() {
-        LOGGER.info("jobWorker");
-        String parameter = "";
-        // muster: nodeid.configname "wert"
-
-        if (jobParameterList != null) {
-            
+    protected Void doInBackground() {
+        if ((jobParameterList != null) && jobParameterList.size() > 0){
             try {
-//                job.setParameters(jobParameterList);
-                master.Job().setParameters(jobId, jobParameterList);
-            } catch (MGXServerException ex) {
-                Exceptions.printStackTrace(ex);
-            }  
-
-            try {
-                master.Job().setParameters(jobId, jobParameterList);
+                job.setParameters(jobParameterList);
+                master.Job().setParameters(job.getId(), jobParameterList);
             } catch (MGXServerException ex) {
                 Exceptions.printStackTrace(ex);
             }
-//            boolean job_is_ok = false;
-//            try {
-//                
-//                ProgressBar bar = new ProgressBar("Verifying tool parameters.",
-//                        "Waiting for the server",
-//                        300, 140);
-//                job_is_ok = master.Job().verify(jobId);
-//
-//                if (job_is_ok) {
-//                    LOGGER.info("job is ok");
-//                    bar.setUpdateText("Parameters are fine. Running tool.");
-//                    master.Job().execute(jobId);
-//                    bar.dispose();
-//                }
-//
-//            } catch (MGXServerException ex) {
-//                Exceptions.printStackTrace(ex);
-//            }
-//            Object[] options = {"Yes",
-//                "No",};
-//            int value = JOptionPane.showOptionDialog(null,
-//                    "Tool is executed. Do you want to return to the tool overview.", 
-//                    "Tool Execution.",
-//                    JOptionPane.YES_NO_OPTION,
-//                    JOptionPane.QUESTION_MESSAGE, null,
-//                    options, options[0]);
-//            if (value == JOptionPane.YES_OPTION) {
-//                GetToolsWorker worker = new GetToolsWorker(startup, master, run);
-//                worker.execute();
-//            } else if (value == JOptionPane.NO_OPTION) {
-//            }
         }
         return null;
     }
@@ -132,6 +62,10 @@ public class JobWorker extends SwingWorker {
      */
     @Override
     protected void done() {
-        LOGGER.info("jobWorker finish");
+        try {
+            get();
+        } catch (InterruptedException | ExecutionException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 }
