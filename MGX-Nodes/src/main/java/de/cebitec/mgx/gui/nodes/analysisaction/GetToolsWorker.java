3ce7c38319605d795bb5913b1eb7177008c4085b
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-08-02 09:24:24 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@771 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/analysisaction/GetToolsWorker.java
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/analysisaction/GetToolsWorker.java
@@ -4,12 +4,9 @@ import de.cebitec.mgx.client.exception.MGXServerException;
 import de.cebitec.mgx.gui.controller.MGXMaster;
 import de.cebitec.mgx.gui.datamodel.SeqRun;
 import de.cebitec.mgx.gui.datamodel.Tool;
+import de.cebitec.mgx.gui.datamodel.misc.ToolType;
 import de.cebitec.mgx.gui.wizard.configurations.action.WizardController;
-import de.cebitec.mgx.gui.wizard.configurations.progressscreen.ProgressBar;
-import java.util.Collection;
 import java.util.List;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 import org.openide.util.Exceptions;
 
@@ -17,30 +14,16 @@ import org.openide.util.Exceptions;
  *
  * @author pbelmann
  */
-public class GetToolsWorker extends SwingWorker {
-
-    private final static Logger LOGGER =
-            Logger.getLogger(GetToolsWorker.class.getName());
+public class GetToolsWorker extends SwingWorker<Void, Void> {
+  
     /**
      * Das Tool, welches ausgesucht wurde.
      */
     private Tool tool;
     /**
-     * Globale bzw. auf dem Server liegende Tools.
-     */
-    private final int GLOBAL = 0;
-    /**
-     * Lokale auf dem Rechner liegende Tools.
-     */
-    private final int LOCAL = 1;
-    /**
-     * Im Projekt liegende Tools.
-     */
-    private final int PROJECT = 2;
-    /**
      * Globale Tools.
      */
-    private Collection<Tool> globalTools;
+    private List<Tool> globalTools;
     /**
      * Projekt Tools.
      */
@@ -48,7 +31,7 @@ public class GetToolsWorker extends SwingWorker {
     /**
      * Tooltypen.
      */
-    private int toolType = 3;
+    private ToolType toolType;
     /**
      * Der GuiController.
      */
@@ -75,105 +58,33 @@ public class GetToolsWorker extends SwingWorker {
         this.master = master;
         seqRun = lSeqrun;
     }
-
-    /**
-     * Methode die im Hintergrund laeuft.
-     *
-     * @return Objekt.
-     */
+   
     @Override
-    protected Object doInBackground() {
-
-
-        toolType = 5;
-        ProgressBar bar = null;
+    protected Void doInBackground() {
         try {
-            bar = new ProgressBar("Loading tools from server. ", "Waiting for the server",
-                    300, 140);
-
             globalTools = master.Tool().listGlobalTools();
-
-            bar.setUpdateText("Loading tools from project.");
-
             projectTools = master.Tool().fetchall();
-            LOGGER.info("Globaltools Size: " + globalTools.size());
-            LOGGER.info("Projecttools Size: " + projectTools.size());
-
-
-            bar.dispose();
             tool = startUp.startToolViewStartUp(globalTools, projectTools);
-            LOGGER.info("tools set");
-            
         } catch (MGXServerException ex) {
             Exceptions.printStackTrace(ex);
         }
 
         isDelete = startUp.isToolDelete();
-        
-            if (startUp.getLastToolType().equals("Project-Tools")) {
-                toolType = PROJECT;
-                LOGGER.info("ProjectTool");
-            } else if (startUp.getLastToolType().equals("Global-Tools (Server)")) {
-                LOGGER.info("GlobalTool");
-                toolType = GLOBAL;
-            } else if (tool == null) {
-            } else if (startUp.getLastToolType().equals("Local-Tools")) {
-                LOGGER.info("localTool");
-                toolType = LOCAL;
-            }
-        
+        toolType = startUp.getLastToolType();
 
         return null;
     }
 
-    /**
-     * Die Methode, die beim Beenden des Workers aufgerufen wird.
-     */
     @Override
     protected void done() {
-//        startUp.closeProgressBar();
-        LOGGER.info("Get Tools Worker DONE");
 
         if (isDelete) {
             DeleteWorker deleteWorker = new DeleteWorker(startUp, master, tool, seqRun);
             deleteWorker.execute();
         } else {
-
-//            switch (toolType) {
-//                case GLOBAL:
-//                    GlobalToolWorker globalToolWorker =
-//                            new GlobalToolWorker(tool, startUp,
-//                            master, seqRun);
-//                    globalToolWorker.execute();
-//                    break;
-//                case LOCAL:
-//                    Object[] options = {"Yes",
-//                        "No",};
-//                    int value = JOptionPane.showOptionDialog(null,
-//                            "Do you want to install the tool in your project. You can"
-//                            + " run it from your project view.", "Tool Installation",
-//                            JOptionPane.YES_NO_OPTION,
-//                            JOptionPane.QUESTION_MESSAGE, null,
-//                            options, options[0]);
-//                    if (value == JOptionPane.YES_OPTION) {
-//                        LocalToolWorker localToolWorker =
-//                                new LocalToolWorker(tool, startUp,
-//                                master, seqRun);
-//                        localToolWorker.execute();
-//
-//                    } else if (value == JOptionPane.NO_OPTION) {
-//                        GetToolsWorker worker = new GetToolsWorker(startUp, master, seqRun);
-//                        worker.execute();
-//                    }
-//                    break;
-//                case PROJECT:
-                    ShowParameterWorker showToolsWorker =
-                            new ShowParameterWorker(tool, startUp,
-                            master, seqRun,toolType);
-                    showToolsWorker.execute();
-//                    break;
-//                default:
-//            }
+            ShowParameterWorker worker =
+                    new ShowParameterWorker(tool, startUp, master, seqRun, toolType);
+            worker.execute();
         }
         super.done();
     }
--- MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/analysisaction/ShowParameterWorker.java
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/analysisaction/ShowParameterWorker.java
@@ -1,19 +1,14 @@
 package de.cebitec.mgx.gui.nodes.analysisaction;
 
-import de.cebitec.mgx.client.exception.MGXServerException;
 import de.cebitec.mgx.gui.controller.MGXMaster;
 import de.cebitec.mgx.gui.datamodel.*;
-import de.cebitec.mgx.gui.wizard.configurations.action.MenuAction;
+import de.cebitec.mgx.gui.datamodel.misc.ToolType;
 import de.cebitec.mgx.gui.wizard.configurations.action.WizardController;
 import de.cebitec.mgx.gui.wizard.configurations.data.impl.Store;
 import de.cebitec.mgx.gui.wizard.configurations.data.util.Transform;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 import org.openide.util.Exceptions;
 
@@ -23,134 +18,56 @@ import org.openide.util.Exceptions;
  *
  * @author pbelmann
  */
-public class ShowParameterWorker extends SwingWorker<List<JobParameter>, Void> {
+public class ShowParameterWorker extends SwingWorker<Collection<JobParameter>, Void> {
 
-    private final static Logger LOGGER =
-            Logger.getLogger(ShowParameterWorker.class.getName());
-    Tool tool;
-    boolean globalToolIsPresent = false;
-    private long project_tool_id;
-    private Job job;
-    private long jobid;
-    private List<JobParameter> jobParameterList;
-    private SeqRun seqrun;
-    private Store store;
+    private Tool tool;
     private WizardController startUp;
     private MGXMaster master;
-    private int toolType=3;
-    private int project = 2;
-    private int global = 0;
-    private int local = 1;
-    
-    
-    public ShowParameterWorker(Tool lTool, WizardController startUp, MGXMaster master, 
+    private ToolType toolType;
+
+    public ShowParameterWorker(Tool lTool, WizardController startUp, MGXMaster master,
             SeqRun seqrun,
-            int toolType) {
+            ToolType toolType) {
         this.master = master;
         tool = lTool;
         this.startUp = startUp;
-        this.seqrun = seqrun;
         this.toolType = toolType;
     }
 
     @Override
-    protected List<JobParameter> doInBackground() {
+    protected Collection<JobParameter> doInBackground() {
 
-        job = new Job();
-        job.setSeqrun(seqrun);
-        job.setCreator(master.getLogin());
-        job.setTool(tool);
-//                    tool.setId(project_tool_id);
+        Collection<JobParameter> list = null;
 
-        LOGGER.info("Before create JobID: " + jobid);
-
-//        jobid = master.Job().create(job);
-
-        LOGGER.info("After create JobID: " + jobid);
-        jobParameterList = null;
-        //parsen
-        List<JobParameter> list = new ArrayList<>();
-        Iterator iterator = null;
-
-//        try {
-//            iterator = master.Job().getParameters(jobid).iterator();
-        
-        switch(toolType){
-            case 0:  iterator = master.Tool().getAvailableParameters(tool.getId(),true).iterator();
-                        break;
-            case 2:  iterator = master.Tool().getAvailableParameters(tool.getId(),false).iterator();
-                        break;
-            case 1:  iterator = master.Tool().getAvailableParameters(tool).iterator();
-                        break;
-        }     
-        LOGGER.info("After Get Parameters");
-//        } catch (MGXServerException ex) {
-//            Exceptions.printStackTrace(ex);
-//        }
-        if (!iterator.hasNext()) {
-            LOGGER.info("Iterator has no next");
+        switch (toolType) {
+            case GLOBAL:
+                list = master.Tool().getAvailableParameters(tool.getId(), true);
+                break;
+            case PROJECT:
+                list = master.Tool().getAvailableParameters(tool.getId(), false);
+                break;
+            case USER_PROVIDED:
+                list = master.Tool().getAvailableParameters(tool);
+                break;
+            default:
+                assert false;
         }
 
-        while (iterator.hasNext()) {
-            list.add((JobParameter) iterator.next());
-        }
-
-        LOGGER.log(Level.INFO, "JobParameter: {0}", list.size());
         Store store = Transform.getFromJobParameterNodeStore(list);
-        List<DirEntry> fetchall = master.File().fetchall();
-        LOGGER.info("after setStore");
+        List<DirEntry> files = master.File().fetchall();
+
         if (list.size() > 0) {
-            this.store = startUp.startParameterConfiguration(store, fetchall,
-                    tool.getName());
+            startUp.startParameterConfiguration(store, files, tool.getName());
         }
         return list;
     }
 
     @Override
     protected void done() {
-        LOGGER.info("DONE SHOWTOOLWORKER");
-        List<JobParameter> list = null;
         try {
-            list = get();
+            get();
         } catch (InterruptedException | ExecutionException ex) {
             Exceptions.printStackTrace(ex);
         }
-//        if (list.size() > 0) {
-//            if (startUp.getStatus() == MenuAction.again) {
-//                LOGGER.info("Again SHOWTOOLWORKER");
-//                GetToolsWorker worker = new GetToolsWorker(startUp, master,
-//                        seqrun);
-//                worker.execute();
-//            } else if (startUp.getStatus() == MenuAction.finish) {
-//                jobParameterList =
-//                        Transform.getFromNodeStoreJobParameter(store);
-//                JobWorker worker = new JobWorker(jobParameterList, jobid, job,
-//                        master, startUp, seqrun);
-//                worker.execute();
-//                LOGGER.info("Finish SHOWTOOLWORKER");
-//            } else if (startUp.getStatus() == MenuAction.cancel) {
-//                LOGGER.info("Cancel SHOWTOOLWORKER");
-//            }
-//        } else {
-//
-//            Object[] options = {"Yes",
-//                "No",};
-//            int value = JOptionPane.showOptionDialog(null,
-//                    "The tool has no configurable parameters. "
-//                    + "Do you want to execute the tool?",
-//                    "Tool Execution",
-//                    JOptionPane.YES_NO_OPTION,
-//                    JOptionPane.QUESTION_MESSAGE, null,
-//                    options, options[0]);
-//            if (value == JOptionPane.YES_OPTION) {
-//                jobParameterList = new ArrayList<>();
-//                JobWorker worker = new JobWorker(jobParameterList, jobid, job,
-//                        master, startUp, seqrun);
-//                worker.execute();
-//            } else if (value == JOptionPane.NO_OPTION) {
-//                GetToolsWorker worker = new GetToolsWorker(startUp, master, seqrun);
-//                worker.execute();
-//            }
-//        }
     }
 }
