915a9b5014f72ca5ec3ed163ef21b423634de559
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2012-11-09 15:36:16 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@972 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/MGXFile.java
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/MGXFile.java
@@ -4,13 +4,34 @@ package de.cebitec.mgx.gui.datamodel;
  *
  * @author sjaenick
  */
-public class MGXFile {
+public class MGXFile extends ModelBase {
 
-    protected MGXMasterI master;
+    protected MGXFile parent = null;
     protected String name;
+    protected boolean isDirectory;
 
-    public MGXMasterI getMaster() {
-        return master;
+    public static MGXFile getRoot(MGXMasterI m) {
+        MGXFile root = new MGXFile();
+        root.setMaster(m);
+        root.setName("");
+        root.isDirectory(true);
+        return root;
+    }
+
+    public void setParent(MGXFile parent) {
+        this.parent = parent;
+    }
+
+    public MGXFile getParent() {
+        return parent;
+    }
+
+    public boolean isDirectory() {
+        return isDirectory;
+    }
+
+    public void isDirectory(boolean isDirectory) {
+        this.isDirectory = isDirectory;
     }
 
     public String getName() {
@@ -19,5 +40,12 @@ public class MGXFile {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getFullPath() {
+        if (parent != null) {
+            return parent.getFullPath() + "/" + getName();
+        }
+        return "." + getName();
     }
 }
