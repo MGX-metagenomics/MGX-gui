df53aac30b262232a18814bc8db474293cb186b0
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-02-15 09:54:37 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1155 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Statistics/src/main/java/de/cebitec/mgx/gui/statistics/viewer/PCAPlot.java
+++ MGX-Statistics/src/main/java/de/cebitec/mgx/gui/statistics/viewer/PCAPlot.java
@@ -4,9 +4,10 @@ import de.cebitec.mgx.gui.attributevisualization.viewer.CategoricalViewerI;
 import de.cebitec.mgx.gui.attributevisualization.viewer.ViewerI;
 import de.cebitec.mgx.gui.controller.MGXMaster;
 import de.cebitec.mgx.gui.datamodel.Attribute;
-import de.cebitec.mgx.gui.datamodel.MGXMasterI;
+import de.cebitec.mgx.gui.datamodel.AttributeType;
 import de.cebitec.mgx.gui.datamodel.misc.Distribution;
 import de.cebitec.mgx.gui.datamodel.misc.Pair;
+import de.cebitec.mgx.gui.groups.ImageExporterI;
 import de.cebitec.mgx.gui.groups.VisualizationGroup;
 import de.cebitec.mgx.gui.statistics.PCA;
 import java.util.List;
@@ -21,7 +22,12 @@ import org.openide.util.lookup.ServiceProvider;
  */
 @ServiceProvider(service = ViewerI.class)
 public class PCAPlot extends CategoricalViewerI {
-    
+
+    @Override
+    public boolean canHandle(AttributeType valueType) {
+        // currently broken, disable viewer
+        return false;
+    }
     JComponent component;
 
     @Override
@@ -50,12 +56,12 @@ public class PCAPlot extends CategoricalViewerI {
         }
         MGXMaster master = (MGXMaster) dists.get(0).getSecond().getMaster();
         int numAttributes = attributes.size();
-        
+
         double[][] x = new double[dists.size()][];
         int curDist = 0;
         for (Pair<VisualizationGroup, Distribution> p : dists) {
             Distribution dist = p.getSecond();
-            x[curDist] = new double[numAttributes]; 
+            x[curDist] = new double[numAttributes];
             int curAttr = 0;
             for (Attribute a : attributes) {
                 x[curDist][curAttr] = dist.containsKey(a) ? dist.get(a).doubleValue() : 0;
@@ -71,6 +77,11 @@ public class PCAPlot extends CategoricalViewerI {
 
     @Override
     public JComponent getCustomizer() {
+        return null;
+    }
+
+    @Override
+    public ImageExporterI getImageExporter() {
         return null;
     }
 }
