4dcb94397089937f4ec22747b91e043ccf2390dd
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-03-01 16:08:51 +0000

    fix NPE
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1192 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Controller/src/main/java/de/cebitec/mgx/gui/controller/RBAC.java
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/controller/RBAC.java
@@ -1,6 +1,5 @@
 package de.cebitec.mgx.gui.controller;
 
-import de.cebitec.gpms.core.RoleI;
 import org.openide.util.Utilities;
 
 /**
@@ -11,13 +10,11 @@ public class RBAC {
 
     public static boolean isUser() {
         MGXMaster m = Utilities.actionsGlobalContext().lookup(MGXMaster.class);
-        RoleI role = m.getMembership().getRole();
-        return role.getName().equals("User");
+        return m != null && m.getMembership().getRole().getName().equals("User");
     }
 
     public static boolean isGuest() {
         MGXMaster m = Utilities.actionsGlobalContext().lookup(MGXMaster.class);
-        RoleI role = m.getMembership().getRole();
-        return role.getName().equals("Guest");
+        return m != null && m.getMembership().getRole().getName().equals("Guest");
     }
 }
