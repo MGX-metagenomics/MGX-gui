c5a69a0fd2556080bdd2c029e04740ad07224570
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-05-03 12:41:31 +0000

    fix task handling
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1306 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Controller/src/main/java/de/cebitec/mgx/gui/dtoconversion/TaskDTOFactory.java
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/dtoconversion/TaskDTOFactory.java
@@ -3,13 +3,14 @@ package de.cebitec.mgx.gui.dtoconversion;
 import de.cebitec.mgx.dto.dto.TaskDTO;
 import de.cebitec.mgx.gui.datamodel.ModelBase;
 import de.cebitec.mgx.gui.datamodel.misc.Task;
+import de.cebitec.mgx.gui.datamodel.misc.Task.TaskType;
 import java.util.UUID;
 
 /**
  *
  * @author sjaenick
  */
-public class TaskDTOFactory<T extends ModelBase> extends DTOConversionBase<Task, TaskDTO> {
+public class TaskDTOFactory extends DTOConversionBase<Task, TaskDTO> {
 
     static {
         instance = new TaskDTOFactory();
@@ -21,10 +22,12 @@ public class TaskDTOFactory<T extends ModelBase> extends DTOConversionBase<Task,
     
     private static ModelBase object;
     private static UUID taskUUid;
+    private static TaskType taskType;
 
-    public static synchronized TaskDTOFactory getInstance(ModelBase obj, UUID uuid) {
+    public static synchronized TaskDTOFactory getInstance(ModelBase obj, UUID uuid, TaskType tType) {
         object = obj;
         taskUUid = uuid;
+        taskType = tType;
         return instance;
     }
 
@@ -35,7 +38,7 @@ public class TaskDTOFactory<T extends ModelBase> extends DTOConversionBase<Task,
 
     @Override
     public Task toModel(TaskDTO dto) {
-        return new Task(object, taskUUid)
+        return new Task(object, taskUUid, taskType)
                 .setStatusMessage(dto.getMessage())
                 .setState(Task.State.values()[dto.getState().ordinal()]);
     }
