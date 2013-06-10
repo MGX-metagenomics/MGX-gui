f89b590d949da2590b0ccdcd5dad3b04d2f17883
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-06-10 18:30:40 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1355 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/misc/Matrix.java
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/misc/Matrix.java
@@ -1,7 +1,7 @@
 package de.cebitec.mgx.gui.datamodel.misc;
 
-import de.cebitec.mgx.gui.datamodel.Attribute;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,13 +9,13 @@ import java.util.List;
  *
  * @author sj
  */
-public class Matrix {
+public class Matrix<T, U>{
 
-    private List<Attribute> rows = new LinkedList<>();
-    private List<Attribute> columns = new LinkedList<>();
-    private double[][] _data = new double[0][0];
+    private List<T> rows = new LinkedList<>();
+    private List<U> columns = new LinkedList<>();
+    private long[][] _data = new long[0][0];
 
-    public void addData(Attribute rowAttr, Attribute colAttr, double value) {
+    public void addData(T rowAttr, U colAttr, long value) {
         if (!rows.contains(rowAttr)) {
             rows.add(rowAttr);
         }
@@ -32,17 +32,59 @@ public class Matrix {
             addColumn();
         }
 
-        _data[rowIdx][colIdx] = value;
+        _data[rowIdx][colIdx] += value;
+    }
+
+    public List<T> getRowHeaders() {
+        return Collections.unmodifiableList(rows);
+    }
+
+    public List<U> getColumnHeaders() {
+        return Collections.unmodifiableList(columns);
+    }
+
+    public long getRowSum(T a) {
+        long ret = 0;
+        int rowIdx = rows.indexOf(a);
+        for (int i = 0; i < _data[rowIdx].length; i++) {
+            ret += _data[rowIdx][i];
+        }
+        return ret;
+    }
+
+    public long getColumnSum(U a) {
+        long ret = 0;
+        int colIdx = columns.indexOf(a);
+        for (int i = 0; i < _data.length; i++) {
+            ret += _data[i][colIdx];
+        }
+        return ret;
+    }
+
+    public long getValue(T rowAttr, U colAttr) {
+        int rowIdx = rows.indexOf(rowAttr);
+        int colIdx = columns.indexOf(colAttr);
+        return _data[rowIdx][colIdx];
+    }
+
+    public int[] getSize() {
+        return new int[]{_data.length, _data[0].length};
+    }
+
+    public void add(Matrix<T, U> m) {
+        for (T rAttr : m.getRowHeaders()) {
+            for (U cAttr : m.getColumnHeaders()) {
+                addData(rAttr, cAttr, m.getValue(rAttr, cAttr));
+            }
+        }
     }
 
     private void addRow() {
-        double[][] newData = new double[rows.size()][columns.size()];
+        long[][] newData = new long[rows.size()][columns.size()];
 
         // copy over old content
-        for (int row = 0; row < rows.size(); row++) {
-            for (int col = 0; col < columns.size(); col++) {
-                newData[row][col] = _data[row][col];
-            }
+        for (int row = 0; row < _data.length; row++) {
+            System.arraycopy(_data[row], 0, newData[row], 0, _data[0].length);
         }
 
         // initialize last row to zero
@@ -53,6 +95,7 @@ public class Matrix {
 
     private void addColumn() {
         for (int row = 0; row < rows.size(); row++) {
+            // missing values are internally zeroed
             _data[row] = Arrays.copyOf(_data[row], columns.size());
         }
     }
---
+++ MGX-Datamodel/src/test/java/de/cebitec/mgx/gui/datamodel/misc/MatrixTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.datamodel.misc;
+
+import de.cebitec.mgx.gui.datamodel.Attribute;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sj
+ */
+public class MatrixTest {
+
+    public MatrixTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void test() {
+        /*
+         *            cattr1
+         *   rattr1     1
+         *   rattr2     5
+         * 
+         */
+        Attribute rowAttr = new Attribute();
+        rowAttr.setValue("rattr1");
+        Attribute rowAttr2 = new Attribute();
+        rowAttr2.setValue("rattr2");
+        Attribute colAttr = new Attribute();
+        colAttr.setValue("cattr1");
+        Matrix<Attribute, Attribute> instance = new Matrix<>();
+        instance.addData(rowAttr, colAttr, 1);
+        instance.addData(rowAttr2, colAttr, 5);
+        assertEquals(instance.getRowHeaders().size(), 2);
+        assertEquals(instance.getColumnHeaders().size(), 1);
+        assertArrayEquals(new int[]{2, 1}, instance.getSize());
+        //
+        long rowSum = instance.getRowSum(rowAttr);
+        assertEquals(1, rowSum);
+        //
+        long columnSum = instance.getColumnSum(colAttr);
+        assertEquals(6, columnSum);
+    }
+}
