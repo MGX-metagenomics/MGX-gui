2bb6906e1aa3d15d38bc5146afdf43860b2cdbfe
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-06-21 09:24:16 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1398 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Controller/src/test/java/de/cebitec/mgx/gui/util/TestMaster.java
+++ MGX-Controller/src/test/java/de/cebitec/mgx/gui/util/TestMaster.java
@@ -5,6 +5,10 @@ import de.cebitec.gpms.rest.GPMSClientI;
 import de.cebitec.mgx.client.MGXDTOMaster;
 import de.cebitec.mgx.gui.controller.MGXMaster;
 import de.cebitec.mgx.restgpms.GPMS;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import static org.junit.Assert.fail;
 
 /**
@@ -15,8 +19,21 @@ public class TestMaster {
 
     public static MGXMaster get() {
         MGXMaster master = null;
-        //GPMSClientI gpms = new GPMS("MyServer", "https://mgx.cebitec.uni-bielefeld.de/MGX-maven-web/webresources/");
-        GPMSClientI gpms = new GPMS("MyServer", "http://localhost:8080/MGX-maven-web/webresources/");
+
+        String serverURI = "https://mgx.cebitec.uni-bielefeld.de/MGX-maven-web/webresources/";
+
+        String config = System.getProperty("user.home") + "/.m2/mgx.junit";
+        File f = new File(config);
+        if (f.exists() && f.canRead()) {
+            Properties p = new Properties();
+            try {
+                p.load(new FileInputStream(f));
+                serverURI = p.getProperty("testserver");
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        GPMSClientI gpms = new GPMS("MyServer", serverURI);
         if (!gpms.login("mgx_unittest", "gut-isM5iNt")) {
             fail();
         }
