060b996f05cceea3ac66732367942fa555535861
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-07-19 16:39:03 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1412 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/util/Reference.java
@@ -0,0 +1,20 @@
+
+package de.cebitec.mgx.gui.util;
+
+/**
+ *
+ * @author sj
+ */
+public class Reference<T> {
+    
+    private T value;
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+    
+}
