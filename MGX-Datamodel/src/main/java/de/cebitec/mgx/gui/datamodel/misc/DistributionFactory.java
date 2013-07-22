e02fd55dde6df26a9330c3eacfa0390043f7b394
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-07-22 15:39:36 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1424 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/misc/DistributionFactory.java
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/misc/DistributionFactory.java
@@ -1,7 +1,10 @@
 package de.cebitec.mgx.gui.datamodel.misc;
 
 import de.cebitec.mgx.gui.datamodel.Attribute;
+import de.cebitec.mgx.gui.datamodel.AttributeType;
 import de.cebitec.mgx.gui.datamodel.MGXMasterI;
+import de.cebitec.mgx.gui.datamodel.tree.Node;
+import de.cebitec.mgx.gui.datamodel.tree.Tree;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -31,5 +34,19 @@ public class DistributionFactory {
         }
 
         return new Distribution(summary, total, anyMaster);
+    }
+
+    public static <T extends Number> Distribution fromTree(Tree<T> tree, AttributeType aType) {
+        Map<Attribute, Number> summary = new HashMap<>();
+        long total = 0;
+        MGXMasterI master = aType.getMaster();
+        
+        for (Node<T> node : tree.getNodes()) {
+            if (node.getAttribute().getAttributeType().getName().equals(aType.getName())) {
+                summary.put(node.getAttribute(), node.getContent());
+                total += node.getContent().longValue();
+            }
+        }
+        return new Distribution(summary, total, master);
     }
 }
