5c680e37bcb1756e420a04e1bbe9765b82a1009b
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-07-23 08:20:30 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1428 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-AttributeVisualization/src/test/java/de/cebitec/mgx/gui/attributevisualization/filter/ExcludeFilterTest.java
+++ MGX-AttributeVisualization/src/test/java/de/cebitec/mgx/gui/attributevisualization/filter/ExcludeFilterTest.java
@@ -4,14 +4,12 @@ import de.cebitec.mgx.gui.controller.MGXMaster;
 import de.cebitec.mgx.gui.datamodel.Attribute;
 import de.cebitec.mgx.gui.datamodel.AttributeType;
 import de.cebitec.mgx.gui.datamodel.misc.Distribution;
-import de.cebitec.mgx.gui.datamodel.misc.Pair;
+import de.cebitec.mgx.gui.datamodel.misc.DistributionFactory;
 import de.cebitec.mgx.gui.datamodel.tree.Node;
 import de.cebitec.mgx.gui.datamodel.tree.Tree;
 import de.cebitec.mgx.gui.datamodel.tree.TreeFactory;
-import de.cebitec.mgx.gui.groups.VisualizationGroup;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -46,24 +44,182 @@ public class ExcludeFilterTest {
     }
 
     @Test
+    public void testCompareDistAndTree() {
+        System.out.println("testCompareDistAndTree");
+        MGXMaster m = TestMaster.get2();
+        // ncbi_class
+        Tree<Long> atb = m.Attribute().getHierarchy(5, 19);
+        assertNotNull(atb);
+        assertEquals(2274, atb.getNodes().size());
+
+        Distribution atbDist = m.Attribute().getDistribution(5, 19);
+        assertNotNull(atbDist);
+        assertEquals(86, atbDist.size());
+        assertEquals(17447, atbDist.getTotalClassifiedElements());
+
+        AttributeType aType = atbDist.keySet().toArray(new Attribute[0])[0].getAttributeType();
+        assertEquals("NCBI_CLASS", aType.getName());
+
+        int nodeCount = 0;
+        long count = 0;
+        for (Node<Long> node : atb.getNodes()) {
+            if (node.getAttribute().getAttributeType().equals(aType)) {
+                nodeCount++;
+                count += node.getContent().longValue();
+            }
+        }
+        assertEquals(86, nodeCount);
+        assertEquals(17447, count);
+
+        Distribution fromTree = DistributionFactory.fromTree(atb, aType);
+        assertEquals(86, fromTree.size());
+        assertEquals(17447, fromTree.getTotalClassifiedElements());
+
+        for (Attribute attr : fromTree.keySet()) {
+            assertEquals("values differ for " + attr.getValue(), fromTree.get(attr), atbDist.get(attr));
+            assertTrue(attr.getValue() + " only in tree, not in dist", atbDist.containsKey(attr));
+        }
+        for (Attribute attr : atbDist.keySet()) {
+            assertEquals("values differ for " + attr.getValue(), fromTree.get(attr), atbDist.get(attr));
+            assertTrue(attr.getValue() + " only in dist, not in tree", fromTree.containsKey(attr));
+        }
+    }
+
+    @Test
+    public void testMerge() {
+        System.out.println("testMerge");
+        MGXMaster m = TestMaster.get2();
+
+        Tree<Long> atb = m.Attribute().getHierarchy(5, 19);
+        assertNotNull(atb);
+        assertEquals(2274, atb.getNodes().size());
+        checkNode(atb, "Actinopterygii", 25L);
+
+        Tree<Long> haw = m.Attribute().getHierarchy(5, 20);
+        assertNotNull(haw);
+        assertEquals(2900, haw.getNodes().size());
+        checkNode(haw, "Actinopterygii", 35L);
+
+        // merge trees
+        Collection<Tree<Long>> data = new HashSet<>();
+        data.add(haw);
+        data.add(atb);
+        Tree<Long> mergedTree = TreeFactory.mergeTrees(data);
+        checkNode(mergedTree, "Actinopterygii", Long.valueOf(60));
+
+
+        AttributeType ncbiClass = null;
+        Attribute attrActinopterygii = null;
+        int numClassNodes = 0;
+        int numActinopterygii = 0;
+        for (Node<Long> node : mergedTree.getNodes()) {
+            if (node.getAttribute().getAttributeType().getName().equals("NCBI_CLASS")) {
+                ncbiClass = node.getAttribute().getAttributeType();
+                numClassNodes++;
+                if (node.getAttribute().getValue().equals("Actinopterygii")) {
+                    numActinopterygii++;
+                    attrActinopterygii = node.getAttribute();
+                }
+            }
+        }
+        assertNotNull(ncbiClass);
+        assertEquals(100, numClassNodes);
+        assertEquals(1, numActinopterygii);
+
+        Distribution atbDist = m.Attribute().getDistribution(5, 19);
+        assertNotNull(atbDist);
+        assertEquals(86, atbDist.size());
+        checkDist(atbDist, "Actinopterygii", 25L);
+
+        Distribution hawDist = m.Attribute().getDistribution(5, 20);
+        assertNotNull(hawDist);
+        assertEquals(93, hawDist.size());
+        checkDist(hawDist, "Actinopterygii", 35L);
+
+        Distribution mergedFromTree = DistributionFactory.fromTree(mergedTree, ncbiClass);
+        assertNotNull(mergedFromTree);
+        assertEquals(100, mergedFromTree.size());
+        Set<String> names = new HashSet<>();
+        for (Attribute a : mergedFromTree.keySet()) {
+            names.add(a.getValue());
+        }
+        assertEquals(100, names.size());
+
+
+        Set<Distribution> set = new HashSet<>();
+        set.add(hawDist);
+        set.add(atbDist);
+        Distribution mergedDists = DistributionFactory.merge(set);
+        assertNotNull(mergedDists);
+        checkDist(mergedDists, "Actinopterygii", 60L);
+
+        // compare content in both directions
+        for (Attribute attr : mergedFromTree.keySet()) {
+            assertEquals("values differ for " + attr.getValue(), mergedFromTree.get(attr), mergedDists.get(attr));
+            assertTrue(attr.getValue() + " only in tree, not in dist", mergedDists.containsKey(attr));
+        }
+
+
+        assertEquals(100, mergedDists.size());
+    }
+
+    @Test
     public void testFilter() {
-        return; // disable test
         System.out.println("filterSingleTree");
         MGXMaster m = TestMaster.get2();
         // ncbi_class
         Tree<Long> atb = m.Attribute().getHierarchy(5, 19);
-        //Tree<Long> haw = m.Attribute().getHierarchy(5, 20);
+        assertNotNull(atb);
+        assertEquals(2274, atb.getNodes().size());
+        Tree<Long> haw = m.Attribute().getHierarchy(5, 20);
+        assertNotNull(haw);
+        assertEquals(2900, haw.getNodes().size());
 
+        // merge trees
         Collection<Tree<Long>> data = new HashSet<>();
-        //data.add(haw);
+        data.add(haw);
         data.add(atb);
+        assertEquals(2, data.size());
         Tree<Long> tree = TreeFactory.mergeTrees(data);
         assertNotNull(tree);
+        assertEquals(3092, tree.getNodes().size());
 
         AttributeType ncbiClass = null;
+        for (Node<Long> node : tree.getNodes()) {
+            if (node.getAttribute().getAttributeType().getName().equals("NCBI_CLASS")) {
+                ncbiClass = node.getAttribute().getAttributeType();
+                break;
+            }
+        }
+        assertNotNull(ncbiClass);
+
+        Distribution mergedFromTree = DistributionFactory.fromTree(tree, ncbiClass);
+        assertNotNull(mergedFromTree);
+
+        Distribution atbDist = m.Attribute().getDistribution(5, 19);
+        assertNotNull(atbDist);
+        assertEquals(86, atbDist.size());
+        Distribution hawDist = m.Attribute().getDistribution(5, 20);
+        assertNotNull(hawDist);
+        assertEquals(93, hawDist.size());
+
+
+        Set<Distribution> set = new HashSet<>();
+        set.add(hawDist);
+        set.add(atbDist);
+        Distribution mergedDists = DistributionFactory.merge(set);
+        assertNotNull(mergedDists);
+
+        assertEquals(100, mergedFromTree.size());
+        assertEquals(100, mergedDists.size());
+
+
         Set<Attribute> filterEntries = new HashSet<>();
         for (Node<Long> node : tree.getNodes()) {
             if (node.getAttribute().getAttributeType().getName().equals("NCBI_CLASS")) {
+                if (ncbiClass != null) {
+                    assertEquals(ncbiClass, node.getAttribute().getAttributeType());
+                }
                 ncbiClass = node.getAttribute().getAttributeType();
             }
             if (node.getAttribute().getValue().equals("Bacteria")) {
@@ -74,13 +230,10 @@ public class ExcludeFilterTest {
             }
         }
 
-        assertNotNull(ncbiClass);
         assertEquals(2, filterEntries.size());
 
 
         Set<Attribute> blackList = new HashSet<>();
-
-
         for (Node<Long> node : tree.getNodes()) {
             if (ncbiClass.equals(node.getAttribute().getAttributeType())) {
                 for (Node<Long> pathNode : node.getPath()) {
@@ -91,31 +244,52 @@ public class ExcludeFilterTest {
                 }
             }
         }
+        assertEquals(91, blackList.size());
 
-        assertEquals(78, blackList.size());
+
+        Distribution fromTree = DistributionFactory.fromTree(tree, ncbiClass);
 
 
-        Distribution atbDist = m.Attribute().getDistribution(5, 19);
-        assertEquals(86, atbDist.size());
         ExcludeFilter filter = new ExcludeFilter(blackList);
+        assertEquals(86, atbDist.size());
         filter.filterDist(atbDist);
         assertEquals(8, atbDist.size());
 
-//        Distribution hawDist = m.Attribute().getDistribution(5, 20);
-//        assertEquals(93, hawDist.size());
-//        filter.filterDist(hawDist);
-//        assertEquals(79, hawDist.size());
+        assertEquals(93, hawDist.size());
+        filter.filterDist(hawDist);
+        assertEquals(9, hawDist.size());
 
 
+    }
+
+    private void checkNode(Tree<Long> tree, String name, Long content) {
+        assertNotNull(name);
+        assertNotNull(content);
+        boolean nodeFound = false;
         for (Node<Long> node : tree.getNodes()) {
-            if (node.getAttribute().getAttributeType().getName().equals(ncbiClass.getName())) {
-                if (!blackList.contains(node.getAttribute())) {
-                    System.err.println("   " + node.getAttribute().getValue());
-                }
+            if (node.getAttribute().getValue().equals(name)) {
+                assertFalse(nodeFound);
+                nodeFound = true;
+                assertEquals(content, node.getContent());
             }
         }
+        if (!nodeFound) {
+            assertEquals("Node not found for " + name, 0L, content.longValue());
+        }
+    }
 
-        //TreeFactory.dumpNode(tree.getRoot(), 2);
-
+    private void checkDist(Distribution dist, String name, Long content) {
+        assertNotNull(name);
+        assertNotNull(content);
+        boolean found = false;
+        for (Attribute attr : dist.keySet()) {
+            if (attr.getValue().equals(name)) {
+                found = true;
+                assertEquals(content, dist.get(attr));
+            }
+        }
+        if (!found) {
+            assertEquals(0L, content.longValue());
+        }
     }
 }
