370cbe4e15789d7698163f74a61d710539fb4c77
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-07-29 16:19:19 +0000

    add ctor
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1433 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Controller/src/main/java/de/cebitec/mgx/gui/util/Reference.java
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/util/Reference.java
@@ -9,6 +9,10 @@ public class Reference<T> {
     
     private T value;
 
+    public Reference(T value) {
+        this.value = value;
+    }
+
     public T getValue() {
         return value;
     }
