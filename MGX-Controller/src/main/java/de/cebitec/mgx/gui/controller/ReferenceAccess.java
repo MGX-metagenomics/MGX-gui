fd6f20d1fb487c425e64a33e1d46fe2ad975e703
Peter Belmann <pbelmann@CeBiTec.Uni-Bielefeld.DE>
2013-08-27 18:13:51 +0000

    ReferenceAccess
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1455 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/controller/ReferenceAccess.java
@@ -0,0 +1,107 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.controller;
+
+import de.cebitec.mgx.client.exception.MGXClientException;
+import de.cebitec.mgx.client.exception.MGXServerException;
+import de.cebitec.mgx.dto.dto;
+import de.cebitec.mgx.dto.dto.ReferenceDTO;
+import de.cebitec.mgx.gui.datamodel.DNAExtract;
+import de.cebitec.mgx.gui.datamodel.Identifiable;
+import de.cebitec.mgx.gui.datamodel.ModelBase;
+import de.cebitec.mgx.gui.datamodel.Reference;
+import de.cebitec.mgx.gui.datamodel.misc.Task;
+import de.cebitec.mgx.gui.dtoconversion.DNAExtractDTOFactory;
+import de.cebitec.mgx.gui.dtoconversion.ReferenceDTOFactory;
+import java.util.Iterator;
+import java.util.UUID;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * @author belmann
+ */
+public class ReferenceAccess extends AccessBase<Reference> {
+
+    @Override
+    public long create(Reference obj) {
+        dto.ReferenceDTO dto = ReferenceDTOFactory.getInstance().toDTO(obj);
+        long id = Identifiable.INVALID_IDENTIFIER;
+        try {
+            id = getDTOmaster().Reference().create(dto);
+        } catch (MGXServerException | MGXClientException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        obj.setId(id);
+        obj.setMaster(this.getMaster());
+        return id;
+    }
+
+    @Override
+    public Reference fetch(long id) {
+        dto.ReferenceDTO dto = null;
+        try {
+            dto = getDTOmaster().Reference().fetch(id);
+        } catch (MGXServerException | MGXClientException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        Reference ret = ReferenceDTOFactory.getInstance().toModel(dto);
+        ret.setMaster(this.getMaster());
+        return ret;
+    }
+
+    @Override
+    public Iterator<Reference> fetchall() {
+        try {
+
+            return new Iterator<Reference>() {
+                final Iterator<ReferenceDTO> iter = getDTOmaster().Reference().fetchall();
+
+                @Override
+                public boolean hasNext() {
+                    return iter.hasNext();
+                }
+
+                @Override
+                public Reference next() {
+                    Reference ret = ReferenceDTOFactory.getInstance().toModel(iter.next());
+                    ret.setMaster(getMaster());
+                    return ret;
+                }
+
+                @Override
+                public void remove() {
+                    throw new UnsupportedOperationException("Not supported.");
+                }
+            };
+        } catch (MGXServerException | MGXClientException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        return null;
+    }
+
+    @Override
+    public void update(Reference obj) {
+        ReferenceDTO dto = ReferenceDTOFactory.getInstance().toDTO(obj);
+        try {
+            getDTOmaster().Reference().update(dto);
+        } catch (MGXServerException | MGXClientException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        obj.firePropertyChange(ModelBase.OBJECT_MODIFIED, null, obj);
+    }
+
+    @Override
+    public Task delete(Reference obj) {
+        Task ret = null;
+        try {
+            UUID uuid = getDTOmaster().Reference().delete(obj.getId());
+            ret = getMaster().Task().get(obj, uuid, Task.TaskType.DELETE);
+        } catch (MGXServerException | MGXClientException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        return ret;
+    }
+}
