47102d1bf7c4d3967d31586a9a4883889bfffc9d
Peter Belmann <pbelmann@CeBiTec.Uni-Bielefeld.DE>
2013-09-01 19:51:44 +0000

    RegionDTOFactory
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1477 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/dtoconversion/RegionDTOFactory.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.dtoconversion;
+
+import de.cebitec.mgx.dto.dto.RegionDTO;
+import de.cebitec.mgx.gui.datamodel.Region;
+import de.cebitec.mgx.dto.dto.RegionDTO.Builder;
+import de.cebitec.mgx.gui.datamodel.Identifiable;
+
+/**
+ *
+ * @author belmann
+ */
+public class RegionDTOFactory extends DTOConversionBase<Region, RegionDTO>{
+    
+
+    static {
+        instance = new RegionDTOFactory();
+    }
+    protected final static RegionDTOFactory instance;
+
+    private RegionDTOFactory() {
+    }
+
+    public static RegionDTOFactory getInstance() {
+        return instance;
+    }
+    
+    
+    @Override
+    public RegionDTO toDTO(Region reg) {
+        Builder b = RegionDTO.newBuilder();
+        if (reg.getId() != Identifiable.INVALID_IDENTIFIER) {
+            b.setId(reg.getId());
+        }
+        b = b.setName(reg.getName());
+        b = b.setDescription(reg.getDescription());
+        b = b.setStart(reg.getStart());
+        b = b.setStop(reg.getStop());
+        return b.build();
+    }
+
+    @Override
+    public Region toModel(RegionDTO dto) {
+       Region d = new Region();
+        d.setName(dto.getName());
+        d.setDescription(dto.getDescription());
+        d.setStart(dto.getStart());
+        d.setStop(dto.getStop());
+        d.setId(dto.getId());
+        return d;
+    }
+
+
+}
