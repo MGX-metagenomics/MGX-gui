86e9714fa5e9006f3c892a447f0996719b82b278
Peter Belmann <pbelmann@CeBiTec.Uni-Bielefeld.DE>
2013-09-01 20:00:24 +0000

    byReferenceInterval
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1478 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Controller/src/main/java/de/cebitec/mgx/gui/controller/ReferenceAccess.java
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/controller/ReferenceAccess.java
@@ -8,13 +8,17 @@ import de.cebitec.mgx.client.exception.MGXClientException;
 import de.cebitec.mgx.client.exception.MGXServerException;
 import de.cebitec.mgx.dto.dto;
 import de.cebitec.mgx.dto.dto.ReferenceDTO;
+import de.cebitec.mgx.dto.dto.RegionDTO;
 import de.cebitec.mgx.gui.datamodel.DNAExtract;
 import de.cebitec.mgx.gui.datamodel.Identifiable;
 import de.cebitec.mgx.gui.datamodel.ModelBase;
 import de.cebitec.mgx.gui.datamodel.Reference;
+import de.cebitec.mgx.gui.datamodel.Region;
 import de.cebitec.mgx.gui.datamodel.misc.Task;
 import de.cebitec.mgx.gui.dtoconversion.DNAExtractDTOFactory;
 import de.cebitec.mgx.gui.dtoconversion.ReferenceDTOFactory;
+import de.cebitec.mgx.gui.dtoconversion.RegionDTOFactory;
+import de.cebitec.mgx.gui.util.BaseIterator;
 import java.util.Iterator;
 import java.util.UUID;
 import org.openide.util.Exceptions;
@@ -93,6 +97,25 @@ public class ReferenceAccess extends AccessBase<Reference> {
         obj.firePropertyChange(ModelBase.OBJECT_MODIFIED, null, obj);
     }
 
+    public Iterator<Region> byReferenceInterval(Long id,int from,int to) {
+        Iterator<RegionDTO> fetchall;
+        try {
+            fetchall = getDTOmaster().Reference().byReferenceInterval(id, from, to);
+            return new BaseIterator<RegionDTO, Region>(fetchall) {
+                @Override
+                public Region next() {
+                    Region s = RegionDTOFactory.getInstance().toModel(iter.next());
+                    s.setMaster(getMaster());
+                    return s;
+                }
+            };   
+        } catch (MGXServerException | MGXClientException ex) {
+             Exceptions.printStackTrace(ex);
+        }
+        return null;
+    }
+    
+    
     @Override
     public Task delete(Reference obj) {
         Task ret = null;
