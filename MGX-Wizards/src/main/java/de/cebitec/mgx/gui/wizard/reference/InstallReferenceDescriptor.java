ac24c913f9b48c9406c8cfbaa1ce011c7d2d543d
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-10-01 10:37:48 +0000

    add reference installation wizard
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1530 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Wizards/src/main/java/de/cebitec/mgx/gui/wizard/reference/InstallReferenceDescriptor.java
@@ -0,0 +1,72 @@
+package de.cebitec.mgx.gui.wizard.reference;
+
+import de.cebitec.mgx.gui.controller.MGXMaster;
+import de.cebitec.mgx.gui.datamodel.Reference;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
+import javax.swing.SwingWorker;
+import org.openide.WizardDescriptor;
+import org.openide.util.Exceptions;
+import org.openide.util.Utilities;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class InstallReferenceDescriptor extends WizardDescriptor {
+
+    private InstallReferenceWizardPanel1 p1 = new InstallReferenceWizardPanel1();
+    public static final String PROP_REFERENCE = "propRef";
+
+    public InstallReferenceDescriptor() {
+        List<Panel<WizardDescriptor>> panels = new ArrayList<>();
+        panels.add(p1);
+        this.setPanelsAndSettings(new ArrayIterator<>(panels), this);
+        this.setTitleFormat(new MessageFormat("{0}"));
+        this.setTitle("Install reference genome");
+        putProperty(WizardDescriptor.PROP_CONTENT_DATA, new String[]{p1.getName()});
+        putProperty(WizardDescriptor.PROP_AUTO_WIZARD_STYLE, Boolean.TRUE);
+        putProperty(WizardDescriptor.PROP_CONTENT_DISPLAYED, Boolean.TRUE);
+        putProperty(WizardDescriptor.PROP_CONTENT_NUMBERED, Boolean.TRUE);
+        setData();
+    }
+
+    public Reference getSelectedReference() {
+        return (Reference) getProperty(PROP_REFERENCE);
+    }
+
+    private void setData() {
+        final MGXMaster m = Utilities.actionsGlobalContext().lookup(MGXMaster.class);
+        SwingWorker<Void, Void> sw = new SwingWorker<Void, Void>() {
+            @Override
+            protected Void doInBackground() {
+                Set<Reference> refs = new TreeSet<>();
+                Set<String> projRefNames = new HashSet<>();
+                java.util.Iterator<Reference> projrefiter = m.Reference().fetchall();
+                while (projrefiter.hasNext()) {
+                    projRefNames.add(projrefiter.next().getName());
+                }
+                java.util.Iterator<Reference> refiter = m.Reference().listGlobalReferences();
+                while (refiter.hasNext()) {
+                    Reference r = refiter.next();
+                    if (!projRefNames.contains(r.getName())) {
+                        refs.add(r);
+                    }
+                }
+                p1.setReferences(refs);
+                return null;
+            }
+        };
+        sw.execute();
+        try {
+            sw.get();
+        } catch (InterruptedException | ExecutionException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+}
---
+++ MGX-Wizards/src/main/java/de/cebitec/mgx/gui/wizard/reference/InstallReferenceWizardPanel1.java
@@ -0,0 +1,90 @@
+package de.cebitec.mgx.gui.wizard.reference;
+
+import de.cebitec.mgx.gui.datamodel.Reference;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Set;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.EventListenerList;
+import org.openide.WizardDescriptor;
+import org.openide.util.HelpCtx;
+
+public class InstallReferenceWizardPanel1 implements WizardDescriptor.Panel<WizardDescriptor>, PropertyChangeListener {
+
+    /**
+     * The visual component that displays this panel. If you need to access the component from this class, just use getComponent().
+     */
+    private InstallReferenceVisualPanel1 component;
+    private boolean isValid = false;
+
+    @Override
+    public InstallReferenceVisualPanel1 getComponent() {
+        if (component == null) {
+            component = new InstallReferenceVisualPanel1();
+        }
+        return component;
+    }
+
+    public void setReferences(Set<Reference> refs) {
+        getComponent().setReferences(refs);
+    }
+
+    @Override
+    public HelpCtx getHelp() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+
+    @Override
+    public boolean isValid() {
+        return isValid;
+    }
+    private WizardDescriptor model = null;
+
+    @Override
+    public void readSettings(WizardDescriptor settings) {
+        model = settings;
+        getComponent().addPropertyChangeListener(this);
+    }
+
+    @Override
+    public void storeSettings(WizardDescriptor settings) {
+        model = settings;
+        model.putProperty(InstallReferenceDescriptor.PROP_REFERENCE, getComponent().getReference());
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        boolean oldState = isValid;
+        isValid = checkValidity();
+        fireChangeEvent(this, oldState, isValid);
+    }
+
+    private boolean checkValidity() {
+        return getComponent().getReference() != null;
+    }
+
+    protected final void fireChangeEvent(Object src, boolean old, boolean newState) {
+        if (old != newState) {
+            ChangeEvent ev = new ChangeEvent(src);
+            for (ChangeListener cl : listeners.getListeners(ChangeListener.class)) {
+                cl.stateChanged(ev);
+            }
+        }
+    }
+    private final EventListenerList listeners = new EventListenerList();
+
+    @Override
+    public final void addChangeListener(ChangeListener l) {
+        listeners.add(ChangeListener.class, l);
+    }
+
+    @Override
+    public final void removeChangeListener(ChangeListener l) {
+        listeners.remove(ChangeListener.class, l);
+    }
+
+    public String getName() {
+        return "Select reference";
+    }
+}
