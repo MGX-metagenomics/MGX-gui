313aaf983882b490a7686d0ce3571b6bb8cf7439
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-10-10 11:18:50 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1577 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Controller/src/main/java/de/cebitec/mgx/gui/util/FileChooserUtils.java
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/util/FileChooserUtils.java
@@ -2,6 +2,7 @@ package de.cebitec.mgx.gui.util;
 
 import java.io.File;
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 import org.openide.util.NbPreferences;
