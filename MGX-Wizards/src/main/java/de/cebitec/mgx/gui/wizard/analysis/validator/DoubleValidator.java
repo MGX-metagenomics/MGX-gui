9a3764ba880b654b13e643e667561dc178f527ab
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-10-31 14:16:53 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1662 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Wizards/src/main/java/de/cebitec/mgx/gui/wizard/analysis/validator/DoubleValidator.java
+++ MGX-Wizards/src/main/java/de/cebitec/mgx/gui/wizard/analysis/validator/DoubleValidator.java
@@ -4,16 +4,17 @@ package de.cebitec.mgx.gui.wizard.analysis.validator;
  *
  * @author sjaenick
  */
-public class DoubleValidator extends ValidatorI<Double> {
+public class DoubleValidator extends ValidatorI<String> {
 
     @Override
     public boolean validate(String input) {
         try {
-            value = Double.parseDouble(input);
+            Double d = Double.parseDouble(input);
         } catch (NumberFormatException nfe) {
             error = DEFAULT_ERROR_MSG;
             return false;
         }
+        value = input;
         return true;
     }
 
