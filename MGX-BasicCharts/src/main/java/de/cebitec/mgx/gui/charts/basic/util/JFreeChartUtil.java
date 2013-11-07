b2dfed04029ee041cd3df910b2caf1a66dd47a4b
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2013-11-07 14:06:29 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1664 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-BasicCharts/src/main/java/de/cebitec/mgx/gui/charts/basic/util/JFreeChartUtil.java
+++ MGX-BasicCharts/src/main/java/de/cebitec/mgx/gui/charts/basic/util/JFreeChartUtil.java
@@ -7,7 +7,10 @@ import de.cebitec.mgx.gui.groups.ImageExporterI;
 import de.cebitec.mgx.gui.groups.VisualizationGroup;
 import de.cebitec.mgx.gui.util.FileChooserUtils;
 import de.cebitec.mgx.gui.util.FileType;
+import java.awt.Rectangle;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +21,7 @@ import org.jfree.data.general.DefaultKeyedValues2DDataset;
 import org.jfree.data.general.KeyedValues2DDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.graphics2d.svg.SVGGraphics2D;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 import org.openide.util.Exceptions;
@@ -70,7 +74,7 @@ public class JFreeChartUtil {
                     maxAttrVal = attrVal > maxAttrVal ? attrVal : maxAttrVal;
                 }
             }
-            
+
             if (createBounds) {
                 series.add(minAttrVal - Double.MIN_VALUE, 0);
             }
@@ -79,7 +83,7 @@ public class JFreeChartUtil {
                 double[] pair = values[i];
                 series.add(pair[0], pair[1]);
             }
-            
+
             if (createBounds) {
                 series.add(maxAttrVal + Double.MIN_VALUE, 0);
             }
@@ -125,17 +129,30 @@ public class JFreeChartUtil {
         return new ImageExporterI() {
             @Override
             public void export() {
-                String fname = FileChooserUtils.selectNewFilename(new FileType[]{FileType.PNG});
+                String fname = FileChooserUtils.selectNewFilename(new FileType[]{FileType.PNG, FileType.SVG});
                 if (fname == null) {
                     return;
                 }
 
-                try {
-                    ChartUtilities.saveChartAsPNG(new File(fname), chart, 1280, 1024);
-                } catch (IOException ex) {
-                    NotifyDescriptor nd = new NotifyDescriptor.Message("Error: " + ex.getMessage(), NotifyDescriptor.ERROR_MESSAGE);
-                    DialogDisplayer.getDefault().notify(nd);
-                    return;
+                if (fname.endsWith("png")) {
+                    try {
+                        ChartUtilities.saveChartAsPNG(new File(fname), chart, 1280, 1024);
+                    } catch (IOException ex) {
+                        NotifyDescriptor nd = new NotifyDescriptor.Message("Error: " + ex.getMessage(), NotifyDescriptor.ERROR_MESSAGE);
+                        DialogDisplayer.getDefault().notify(nd);
+                        return;
+                    }
+                } else { // SVG
+                    SVGGraphics2D g2 = new SVGGraphics2D(1280, 1024);
+                    chart.draw(g2, new Rectangle(0, 0, 1280, 1024));
+                    String svgElement = g2.getSVGElement();
+                    try (BufferedWriter bw = new BufferedWriter(new FileWriter(fname))) {
+                        bw.write(svgElement);
+                    } catch (IOException ex) {
+                        NotifyDescriptor nd = new NotifyDescriptor.Message("Error: " + ex.getMessage(), NotifyDescriptor.ERROR_MESSAGE);
+                        DialogDisplayer.getDefault().notify(nd);
+                        return;
+                    }
                 }
                 NotifyDescriptor nd = new NotifyDescriptor.Message("Chart saved to " + fname, NotifyDescriptor.INFORMATION_MESSAGE);
                 DialogDisplayer.getDefault().notify(nd);
