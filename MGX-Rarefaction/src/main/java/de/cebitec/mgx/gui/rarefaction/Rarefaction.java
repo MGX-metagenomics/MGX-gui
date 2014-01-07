6ccb0319292e0b7eaea7da68595851843a8a0ac2
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-01-07 18:47:35 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1698 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Rarefaction/src/main/java/de/cebitec/mgx/gui/rarefaction/Rarefaction.java
+++ MGX-Rarefaction/src/main/java/de/cebitec/mgx/gui/rarefaction/Rarefaction.java
@@ -1,16 +1,9 @@
 package de.cebitec.mgx.gui.rarefaction;
 
-import de.cebitec.mgx.gui.datamodel.Attribute;
+import de.cebitec.mgx.gui.controller.MGXMaster;
 import de.cebitec.mgx.gui.datamodel.misc.Distribution;
-import java.util.HashMap;
+import de.cebitec.mgx.gui.datamodel.misc.Point;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import net.sf.javaml.core.Dataset;
-import net.sf.javaml.core.DefaultDataset;
-import net.sf.javaml.core.DenseInstance;
-import net.sf.javaml.core.Instance;
-import net.sf.javaml.sampling.Sampling;
 
 /**
  *
@@ -18,90 +11,9 @@ import net.sf.javaml.sampling.Sampling;
  */
 public class Rarefaction {
 
-    private final Distribution dist;
-    private final Dataset dataset;
-    private final long N;
-    private final long K;
-    private final int numIterations;
-    private final static int DEFAULT_NUM_ITERATIONS = 25;
-    
-    public Rarefaction(Distribution dist) {
-        this(dist, DEFAULT_NUM_ITERATIONS);
-    }
-
-    public Rarefaction(Distribution dist, int numIter) {
-        this.dist = dist;
-        this.dataset = toDataset(dist);
-        this.numIterations = numIter;
-        N = dist.getTotalClassifiedElements();
-        K = dist.keySet().size();
-    }
-
-    public double rarefy(int size) {
-        assert size > 0;
-        assert size <= N;
-        double d = 0;
-        for (long i = 0; i < numIterations; i++) {
-            Dataset subsampleDS = Sampling.SubSampling.sample(dataset, size).x();
-            d += rarefy(toDistribution(subsampleDS));
-        }
-
-        return d / numIterations;
-    }
-
-    private double rarefy(final Distribution subsample) {
-        long sum = 0;
-        final long n = subsample.getTotalClassifiedElements();
-        for (Attribute a : dist.keySet()) {
-            if (subsample.containsKey(a)) {
-                sum += binom(N - subsample.get(a).longValue(), n);
-            } else {
-                sum += binom(N, n);
-            }
-        }
-
-        return K - Math.pow(binom(N, n), -1) * sum;
-    }
-
-    private static Distribution toDistribution(final Dataset ds) {
-        Map<Attribute, Long> data = new HashMap<>();
-        long total = 0;
-        Iterator<Instance> iter = ds.iterator();
-        while (iter.hasNext()) {
-            total++;
-            Instance next = iter.next();
-            Attribute attr = (Attribute) next.classValue();
-            if (data.containsKey(attr)) {
-                data.put(attr, Long.valueOf(data.get(attr).longValue() + 1));
-            } else {
-                data.put((Attribute) next.classValue(), Long.valueOf(1));
-            }
-        }
-
-        return new Distribution(data, total, null);
-    }
-
-    private static Dataset toDataset(final Distribution d) {
-        Dataset data = new DefaultDataset();
-
-        for (Entry<Attribute, Number> e : d.entrySet()) {
-            long cnt = e.getValue().longValue();
-            for (long i = 1; i <= cnt; i++) {
-                data.add(new DenseInstance(new double[]{1.00}, e.getKey()));
-            }
-        }
-        return data;
-    }
-
-    private static long binom(long n, final long k) {
-        final long min = (k < n - k ? k : n - k);
-        long bin = 1;
-        for (long i = 1; i <= min; i++) {
-            bin *= n;
-            bin /= i;
-            n--;
-        }
-        return bin;
+    public static Iterator<Point> rarefy(Distribution dist) {
+        MGXMaster m = (MGXMaster) dist.getMaster();
+        return m.Statistics().Rarefaction(dist);
     }
 
 }
---
+++ MGX-Rarefaction/src/main/java/de/cebitec/mgx/gui/rarefaction/plot/RarefactionCurve.java
@@ -0,0 +1,158 @@
+package de.cebitec.mgx.gui.rarefaction.plot;
+
+import de.cebitec.mgx.gui.attributevisualization.viewer.ViewerI;
+import de.cebitec.mgx.gui.datamodel.AttributeType;
+import de.cebitec.mgx.gui.datamodel.misc.Distribution;
+import de.cebitec.mgx.gui.datamodel.misc.Pair;
+import de.cebitec.mgx.gui.datamodel.misc.Point;
+import de.cebitec.mgx.gui.groups.ImageExporterI;
+import de.cebitec.mgx.gui.groups.VisualizationGroup;
+import de.cebitec.mgx.gui.rarefaction.Rarefaction;
+import de.cebitec.mgx.gui.util.FileChooserUtils;
+import de.cebitec.mgx.gui.util.FileType;
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.JComponent;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.TickUnitSource;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author sj
+ */
+@ServiceProvider(service = ViewerI.class)
+public class RarefactionCurve extends ViewerI<Distribution> {
+
+    private ChartPanel cPanel = null;
+    private JFreeChart chart = null;
+
+    @Override
+    public JComponent getComponent() {
+        return cPanel;
+    }
+
+    @Override
+    public String getName() {
+        return "Rarefaction";
+    }
+
+    @Override
+    public void show(List<Pair<VisualizationGroup, Distribution>> dists) {
+
+        XYSeriesCollection dataset = createXYSeries(dists);
+
+        String xAxisLabel = "";
+        String yAxisLabel = "";
+
+        chart = ChartFactory.createXYLineChart(getTitle(), xAxisLabel, yAxisLabel, dataset, PlotOrientation.VERTICAL, true, true, false);
+        chart.setBorderPaint(Color.WHITE);
+        chart.setBackgroundPaint(Color.WHITE);
+        cPanel = new ChartPanel(chart);
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.WHITE);
+
+        // x axis
+        ValueAxis valueAxis;
+        final TickUnitSource tusX;
+
+        valueAxis = (NumberAxis) plot.getDomainAxis();
+        tusX = NumberAxis.createIntegerTickUnits();
+        valueAxis.setStandardTickUnits(tusX);
+        valueAxis.setInverted(false);
+        plot.setDomainAxis(valueAxis);
+
+        // y axis
+        final NumberAxis rangeAxis;
+        final TickUnitSource tus;
+
+        rangeAxis = (NumberAxis) plot.getRangeAxis();
+        tus = NumberAxis.createIntegerTickUnits();
+        rangeAxis.setStandardTickUnits(tus);
+        plot.setRangeAxis(rangeAxis);
+
+        // set the colors
+        int i = 0;
+        XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) plot.getRenderer();
+        for (Pair<VisualizationGroup, Distribution> groupDistribution : dists) {
+            renderer.setSeriesPaint(i++, groupDistribution.getFirst().getColor());
+        }
+
+    }
+
+    private static XYSeriesCollection createXYSeries(List<Pair<VisualizationGroup, Distribution>> in) {
+        XYSeriesCollection dataset = new XYSeriesCollection();
+
+        for (Pair<VisualizationGroup, Distribution> groupDistribution : in) {
+            XYSeries series = new XYSeries(groupDistribution.getFirst().getName());
+            Distribution dist = groupDistribution.getSecond();
+            
+            Iterator<Point> iter = Rarefaction.rarefy(dist);
+            while (iter.hasNext()) {
+                Point p = iter.next();
+                series.add(p.getX(), p.getY());
+            }
+            dataset.addSeries(series);
+        }
+        return dataset;
+    }
+
+    @Override
+    public JComponent getCustomizer() {
+        return null;
+    }
+
+    @Override
+    public Class getInputType() {
+        return Distribution.class;
+    }
+
+    @Override
+    public ImageExporterI getImageExporter() {
+        return new ImageExporterI() {
+            @Override
+            public void export() {
+                String fname = FileChooserUtils.selectNewFilename(new FileType[]{FileType.PNG});
+                if (fname == null) {
+                    return;
+                }
+
+                try {
+                    ChartUtilities.saveChartAsPNG(new File(fname), chart, 1280, 1024);
+                } catch (IOException ex) {
+                    NotifyDescriptor nd = new NotifyDescriptor.Message("Error: " + ex.getMessage(), NotifyDescriptor.ERROR_MESSAGE);
+                    DialogDisplayer.getDefault().notify(nd);
+                    return;
+                }
+                NotifyDescriptor nd = new NotifyDescriptor.Message("Chart saved to " + fname, NotifyDescriptor.INFORMATION_MESSAGE);
+                DialogDisplayer.getDefault().notify(nd);
+            }
+        };
+    }
+
+    @Override
+    public boolean canHandle(AttributeType valueType) {
+        return true; // valueType.getValueType() == AttributeType.VALUE_NUMERIC;
+    }
+
+    @Override
+    public void setAttributeType(AttributeType aType) {
+        super.setAttributeType(aType);
+        super.setTitle("Distribution of " + aType.getName());
+    }
+}
