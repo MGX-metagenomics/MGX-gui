0ae7eb471835db153e8da847c37898164f5d788a
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-01-07 18:03:50 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1694 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/misc/Point.java
@@ -0,0 +1,26 @@
+
+
+package de.cebitec.mgx.gui.datamodel.misc;
+
+/**
+ *
+ * @author sj
+ */
+public class Point {
+    
+    private final double x;
+    private final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
