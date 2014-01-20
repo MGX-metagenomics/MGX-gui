0ad283bb938fde5b7cad1eb7ddd8be37be4aa99f
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-01-20 17:30:25 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1785 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-AttributeVisualization/src/test/java/de/cebitec/mgx/gui/attributevisualization/filter/TestMaster.java
+++ MGX-AttributeVisualization/src/test/java/de/cebitec/mgx/gui/attributevisualization/filter/TestMaster.java
@@ -52,23 +52,28 @@ public class TestMaster {
     public static MGXMaster get2() {
         MGXMaster master = null;
 
-        String serverURI = "https://mgx.cebitec.uni-bielefeld.de/MGX-maven-web/webresources/";
+        String serverURI = "http://scooter.cebitec.uni-bielefeld.de:8080/MGX-maven-web/webresources/";
+        String username = null;
+        String password = null;
 
-        String config = System.getProperty("user.home") + "/.m2/mgx.junit";
+        Properties p = new Properties();
+        String config = System.getProperty("user.home") + "/.m2/mgx.private";
         File f = new File(config);
         if (f.exists() && f.canRead()) {
-            Properties p = new Properties();
             try {
                 p.load(new FileInputStream(f));
                 serverURI = p.getProperty("testserver");
+                password = p.getProperty("password");
+                username = p.getProperty("username");
             } catch (IOException ex) {
                 System.out.println(ex.getMessage());
             }
         }
         GPMSClientI gpms = new GPMS("MyServer", serverURI);
-        if (!gpms.login("sjaenick", "Diwdf@")) {
+        if (!gpms.login(username, password)) {
             return null;
         }
+
         for (MembershipI m : gpms.getMemberships()) {
             if ("MGX".equals(m.getProject().getProjectClass().getName()) && ("MGX_Viersen".equals(m.getProject().getName()))) {
                 MGXDTOMaster dtomaster = new MGXDTOMaster(gpms, m);
