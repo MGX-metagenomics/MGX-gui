f5de8da07d6a41a86ba789435677abfa52e3bc50
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-02-13 09:51:20 +0000

    update jzy3d, fix jogl public packages list, remove dependency on custom jzy3d artifact
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1944 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Jzy3D-Wrapper/src/main/java/de/cebitec/mgx/gui/TestClass.java
+++ MGX-Jzy3D-Wrapper/src/main/java/de/cebitec/mgx/gui/TestClass.java
@@ -1,44 +1,44 @@
-package de.cebitec.mgx.gui;
-
-import org.jzy3d.chart.Chart;
-import org.jzy3d.colors.Color;
-import org.jzy3d.colors.ColorMapper;
-import org.jzy3d.colors.colormaps.ColorMapRainbow;
-import org.jzy3d.maths.Coord3d;
-import org.jzy3d.plot3d.primitives.MultiColorScatter;
-import org.jzy3d.plot3d.rendering.legends.colorbars.ColorbarLegend;
-import org.jzy3d.ui.ChartLauncher;
-
-/**
- *
- * @author sj
- */
-public class TestClass {
-
-    public static void main(String[] args) {
-        int size = 100000;
-        float x;
-        float y;
-        float z;
-
-        Coord3d[] points = new Coord3d[size];
-
-        for (int i = 0; i < size; i++) {
-            x = (float) Math.random() - 0.5f;
-            y = (float) Math.random() - 0.5f;
-            z = (float) Math.random() - 0.5f;
-            points[i] = new Coord3d(x, y, z);
-        }
-
-        MultiColorScatter scatter = new MultiColorScatter(points, new ColorMapper(new ColorMapRainbow(), -0.5f, 0.5f));
-
-        Chart chart = new Chart();
-        chart.getAxeLayout().setMainColor(Color.WHITE);
-        chart.getView().setBackgroundColor(Color.BLACK);
-        chart.getScene().add(scatter);
-        scatter.setLegend(new ColorbarLegend(scatter, chart.getView().getAxe().getLayout(), Color.WHITE, null));
-        scatter.setLegendDisplayed(true);
-
-        ChartLauncher.openChart(chart);
-    }
-}
+//package de.cebitec.mgx.gui;
+//
+//import org.jzy3d.chart.Chart;
+//import org.jzy3d.colors.Color;
+//import org.jzy3d.colors.ColorMapper;
+//import org.jzy3d.colors.colormaps.ColorMapRainbow;
+//import org.jzy3d.maths.Coord3d;
+//import org.jzy3d.plot3d.primitives.MultiColorScatter;
+//import org.jzy3d.plot3d.rendering.legends.colorbars.ColorbarLegend;
+//import org.jzy3d.ui.ChartLauncher;
+//
+///**
+// *
+// * @author sj
+// */
+//public class TestClass {
+//
+//    public static void main(String[] args) {
+//        int size = 100000;
+//        float x;
+//        float y;
+//        float z;
+//
+//        Coord3d[] points = new Coord3d[size];
+//
+//        for (int i = 0; i < size; i++) {
+//            x = (float) Math.random() - 0.5f;
+//            y = (float) Math.random() - 0.5f;
+//            z = (float) Math.random() - 0.5f;
+//            points[i] = new Coord3d(x, y, z);
+//        }
+//
+//        MultiColorScatter scatter = new MultiColorScatter(points, new ColorMapper(new ColorMapRainbow(), -0.5f, 0.5f));
+//
+//        Chart chart = new Chart();
+//        chart.getAxeLayout().setMainColor(Color.WHITE);
+//        chart.getView().setBackgroundColor(Color.BLACK);
+//        chart.getScene().add(scatter);
+//        scatter.setLegend(new ColorbarLegend(scatter, chart.getView().getAxe().getLayout(), Color.WHITE, null));
+//        scatter.setLegendDisplayed(true);
+//
+//        ChartLauncher.openChart(chart);
+//    }
+//}
