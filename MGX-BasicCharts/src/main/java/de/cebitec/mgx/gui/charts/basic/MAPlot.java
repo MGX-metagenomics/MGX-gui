a9315aa2b7913c8a662245b0e0b50ec9ebab0bda
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-02-26 14:45:08 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@1990 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-BasicCharts/src/main/java/de/cebitec/mgx/gui/charts/basic/MAPlot.java
+++ MGX-BasicCharts/src/main/java/de/cebitec/mgx/gui/charts/basic/MAPlot.java
@@ -3,7 +3,6 @@ package de.cebitec.mgx.gui.charts.basic;
 import de.cebitec.mgx.gui.attributevisualization.filter.ToFractionFilter;
 import de.cebitec.mgx.gui.attributevisualization.viewer.NumericalViewerI;
 import de.cebitec.mgx.gui.attributevisualization.viewer.ViewerI;
-import de.cebitec.mgx.gui.charts.basic.customizer.XYPlotCustomizer;
 import de.cebitec.mgx.gui.charts.basic.util.JFreeChartUtil;
 import de.cebitec.mgx.gui.datamodel.Attribute;
 import de.cebitec.mgx.gui.datamodel.AttributeType;
@@ -13,20 +12,24 @@ import de.cebitec.mgx.gui.groups.ImageExporterI;
 import de.cebitec.mgx.gui.groups.VGroupManager;
 import de.cebitec.mgx.gui.groups.VisualizationGroup;
 import java.awt.Color;
-import java.util.ArrayList;
+import java.awt.geom.Ellipse2D;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.swing.JComponent;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.annotations.XYTextAnnotation;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.TickUnitSource;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.labels.StandardXYToolTipGenerator;
+import org.jfree.chart.labels.XYToolTipGenerator;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYDotRenderer;
+import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -40,8 +43,8 @@ import org.openide.util.lookup.ServiceProvider;
 public class MAPlot extends NumericalViewerI {
 
     private ChartPanel cPanel = null;
-    private XYPlotCustomizer customizer = null;
     private JFreeChart chart = null;
+    private Map<XYDataItem, String> toolTips = new HashMap<>();
 
     @Override
     public JComponent getComponent() {
@@ -67,23 +70,33 @@ public class MAPlot extends NumericalViewerI {
 
         XYSeriesCollection dataset = new XYSeriesCollection();
         XYSeries series = new XYSeries("");
-        List<XYTextAnnotation> annot = new ArrayList<>();
 
         Distribution first = relevant.get(0).getSecond();
         Distribution second = relevant.get(1).getSecond();
 
-        for (Attribute attr : first.keySet()) {
+        for (Entry<Attribute, Number> e : first.entrySet()) {
             // extract attributes occuring in both datasets
-            if (second.keySet().contains(attr)) {
-                double firstVal = first.get(attr).doubleValue();
-                double secondVal = second.get(attr).doubleValue();
+            if (second.keySet().contains(e.getKey())) {
+                double firstVal = e.getValue().doubleValue();
+                double secondVal = second.get(e.getKey()).doubleValue();
 
                 double x = (Math.log(firstVal) + Math.log(secondVal)) / 2;
                 double y = Math.log(firstVal / secondVal);
-                series.add(x, y);
+                XYDataItem item = new XYDataItem(x, y);
+                series.add(item);
 
-                XYTextAnnotation xyTextAnnotation = new XYTextAnnotation(attr.getValue(), x + 1, y + 1);
-                annot.add(xyTextAnnotation);
+                long numAssigned1 = Math.round(firstVal * first.getTotalClassifiedElements());
+                long numAssigned2 = Math.round(secondVal * second.getTotalClassifiedElements());
+                
+                String toolTipText = new StringBuilder(e.getKey().getValue())
+                        .append("<br><hr>")
+                        .append(relevant.get(0).getFirst().getName())
+                        .append(": ").append(numAssigned1).append(" sequences").append("<br>")
+                        .append(relevant.get(1).getFirst().getName())
+                        .append(": ").append(numAssigned2).append(" sequences")
+                        .toString();
+                
+                toolTips.put(item, toolTipText);
             }
         }
         dataset.addSeries(series);
@@ -93,12 +106,13 @@ public class MAPlot extends NumericalViewerI {
         String yAxisLabel = "log(" + relevant.get(0).getFirst().getName() + "/"
                 + relevant.get(1).getFirst().getName() + ")";
 
-        chart = ChartFactory.createXYLineChart(getTitle(), xAxisLabel, yAxisLabel, dataset, PlotOrientation.VERTICAL, true, true, false);
+        chart = ChartFactory.createXYLineChart(getTitle(), xAxisLabel, yAxisLabel, dataset, PlotOrientation.VERTICAL, false, true, false);
         chart.setBorderPaint(Color.WHITE);
         chart.setBackgroundPaint(Color.WHITE);
         cPanel = new ChartPanel(chart);
         cPanel.setDisplayToolTips(true);
         XYPlot plot = (XYPlot) chart.getPlot();
+
         plot.setBackgroundPaint(Color.WHITE);
 
         // x axis
@@ -110,7 +124,6 @@ public class MAPlot extends NumericalViewerI {
         valueAxis.setInverted(false);
         plot.setDomainAxis(valueAxis);
 
-
         // y axis
         final NumberAxis rangeAxis;
         final TickUnitSource tus;
@@ -119,42 +132,39 @@ public class MAPlot extends NumericalViewerI {
         rangeAxis.setStandardTickUnits(tus);
         plot.setRangeAxis(rangeAxis);
 
-
         XYDotRenderer dot = new XYDotRenderer();
         dot.setDotHeight(5);
         dot.setDotWidth(5);
-        dot.setBaseToolTipGenerator(new StandardXYToolTipGenerator() {
-            private static final long serialVersionUID = 1L;
+        dot.setSeriesShape(0, new Ellipse2D.Double(0, 0, 5, 5));
+
+        dot.setBaseToolTipGenerator(new XYToolTipGenerator() {
 
             @Override
-            public String generateToolTip(XYDataset dataset, int series, int item) {
-                String toolTipStr = "asdf";
-                return toolTipStr;
+            public String generateToolTip(XYDataset xyd, int series, int item) {
+                XYSeriesCollection dataset = (XYSeriesCollection) xyd;
+                XYDataItem dataItem = dataset.getSeries(series).getDataItem(item);
+                return toolTips.get(dataItem);
             }
         });
 
         plot.setRenderer(dot);
-//        
-//        for (XYTextAnnotation a : annot) {
-//            dot.addAnnotation(a);
-//        }
-//        // set the colors
-//        int i = 0;
-//        XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) plot.getRenderer();
-//        for (Pair<VisualizationGroup, Distribution> groupDistribution : dists) {
-//            renderer.setSeriesPaint(i++, groupDistribution.getFirst().getColor());
-//        }
     }
 
     @Override
     public JComponent getCustomizer() {
-        return customizer;
+        return null;
     }
 
     @Override
     public boolean canHandle(AttributeType valueType) {
         return valueType.getValueType() == AttributeType.VALUE_DISCRETE
                 && VGroupManager.getInstance().getActiveGroups().size() == 2;
+    }
+
+    @Override
+    public void setAttributeType(AttributeType aType) {
+        super.setAttributeType(aType);
+        super.setTitle("M/A plot for " + aType.getName());
     }
 
     @Override
