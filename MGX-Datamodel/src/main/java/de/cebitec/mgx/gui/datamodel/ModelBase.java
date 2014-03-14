41b82830742749b880cd9b2fcd0151353348eb7c
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-03-14 09:20:48 +0000

    switch to weak property change listeners
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@2026 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/ModelBase.java
+++ MGX-Datamodel/src/main/java/de/cebitec/mgx/gui/datamodel/ModelBase.java
@@ -5,8 +5,10 @@ import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
 import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -15,10 +17,12 @@ import java.io.IOException;
 public abstract class ModelBase implements Transferable {
 
     protected MGXMasterI master;
-    protected final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    //protected final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
     public final static String OBJECT_DELETED = "objectDeleted";
     public final static String OBJECT_MODIFIED = "objectModified";
     private final DataFlavor dataflavor;
+    //
+    private final Set<WeakReference<PropertyChangeListener>> listeners = new HashSet<>();
 
     public ModelBase(DataFlavor dataflavor) {
         this.dataflavor = dataflavor;
@@ -56,36 +60,63 @@ public abstract class ModelBase implements Transferable {
             throw new UnsupportedFlavorException(flavor);
         }
     }
-    
+
     public final void modified() {
         firePropertyChange(ModelBase.OBJECT_MODIFIED, this, null);
     }
-    
+
     public final void deleted() {
         firePropertyChange(ModelBase.OBJECT_DELETED, this, null);
     }
 
     public void addPropertyChangeListener(PropertyChangeListener listener) {
-        pcs.addPropertyChangeListener(listener);
+        boolean present = false;
+        for (WeakReference<PropertyChangeListener> wr : listeners) {
+              if (wr.get() != null && wr.get().equals(listener)) {
+                present = true;
+            }
+        }
+        if  (!present) {
+            listeners.add(new WeakReference<>(listener));
+        }
+        //pcs.addPropertyChangeListener(listener);
     }
 
     public void removePropertyChangeListener(PropertyChangeListener listener) {
-        pcs.removePropertyChangeListener(listener);
+        WeakReference<PropertyChangeListener> removeMe = null;
+        for (WeakReference<PropertyChangeListener> wr : listeners) {
+            if (wr.get() != null && wr.get().equals(listener)) {
+                removeMe = wr;
+            }
+        }
+        listeners.remove(removeMe);
+        //pcs.removePropertyChangeListener(listener);
     }
 
     public void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
-        pcs.firePropertyChange(propertyName, oldValue, newValue);
+        PropertyChangeEvent evt = new PropertyChangeEvent(this, propertyName, oldValue, newValue);
+        firePropertyChange(evt);
     }
 
     public void firePropertyChange(String propertyName, int oldValue, int newValue) {
-        pcs.firePropertyChange(propertyName, oldValue, newValue);
+        PropertyChangeEvent evt = new PropertyChangeEvent(this, propertyName, oldValue, newValue);
+        firePropertyChange(evt);
+        //pcs.firePropertyChange(propertyName, oldValue, newValue);
     }
 
     public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {
-        pcs.firePropertyChange(propertyName, oldValue, newValue);
+        PropertyChangeEvent evt = new PropertyChangeEvent(this, propertyName, oldValue, newValue);
+        firePropertyChange(evt);
+        //pcs.firePropertyChange(propertyName, oldValue, newValue);
     }
 
     public void firePropertyChange(PropertyChangeEvent event) {
-        pcs.firePropertyChange(event);
+        //pcs.firePropertyChange(event);
+        for (WeakReference<PropertyChangeListener> wr : listeners) {
+            PropertyChangeListener target = wr.get();
+            if (target != null) {
+                target.propertyChange(event);
+            }
+        }
     }
 }
