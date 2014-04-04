b97ad1c559535d5df866b097e9c98fb6f83d239c
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-04-04 13:13:38 +0000

    refactor exporter interface, add filetype
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@2070 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Controller/src/main/java/de/cebitec/mgx/gui/groups/ImageExporterI.java
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/groups/ImageExporterI.java
@@ -1,10 +1,14 @@
 package de.cebitec.mgx.gui.groups;
 
+import de.cebitec.mgx.gui.util.FileType;
+
 /**
  *
  * @author sjaenick
  */
 public interface ImageExporterI {
+    
+    public FileType[] getSupportedTypes();
 
-    public void export();
+    public boolean export(FileType type, String fName) throws Exception;
 }
