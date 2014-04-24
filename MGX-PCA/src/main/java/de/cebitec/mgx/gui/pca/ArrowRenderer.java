923c5648e22e80b6d09a13cc2382de1ac238ac6e
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-04-24 12:09:51 +0000

    fix PCA, add custom renderer for loadings
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@2149 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-PCA/src/main/java/de/cebitec/mgx/gui/pca/ArrowRenderer.java
@@ -0,0 +1,171 @@
+package de.cebitec.mgx.gui.pca;
+
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Rectangle2D;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.plot.CrosshairState;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRendererState;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.util.LineUtilities;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.RectangleEdge;
+import org.jfree.util.ShapeUtilities;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class ArrowRenderer extends XYLineAndShapeRenderer {
+
+    public ArrowRenderer() {
+        super();
+        GeneralPath s = new GeneralPath();
+        s.moveTo(-4, 0);
+        s.lineTo(0, -6);
+        s.lineTo(4, 0);
+        s.lineTo(-4, 0);
+        setBaseShape(s);
+        setSeriesShape(0, s);
+        setSeriesShape(1, s);
+        //setDrawOutlines(true);
+
+    }
+
+    @Override
+    protected void drawPrimaryLine(XYItemRendererState state, Graphics2D g2, XYPlot plot, XYDataset dataset, int pass, int series, int item, ValueAxis domainAxis, ValueAxis rangeAxis, Rectangle2D dataArea) {
+        // get the data point...
+        double x1 = dataset.getXValue(series, item);
+        double y1 = dataset.getYValue(series, item);
+        if (Double.isNaN(y1) || Double.isNaN(x1)) {
+            return;
+        }
+
+        RectangleEdge xAxisLocation = plot.getDomainAxisEdge();
+        RectangleEdge yAxisLocation = plot.getRangeAxisEdge();
+
+        double transX0 = domainAxis.valueToJava2D(0, dataArea, xAxisLocation);
+        double transY0 = rangeAxis.valueToJava2D(0, dataArea, yAxisLocation);
+
+        double transX1 = domainAxis.valueToJava2D(x1, dataArea, xAxisLocation);
+        double transY1 = rangeAxis.valueToJava2D(y1, dataArea, yAxisLocation);
+
+        // only draw if we have good values
+        if (Double.isNaN(transX0) || Double.isNaN(transY0)
+                || Double.isNaN(transX1) || Double.isNaN(transY1)) {
+            return;
+        }
+
+        PlotOrientation orientation = plot.getOrientation();
+        boolean visible;
+        if (orientation == PlotOrientation.HORIZONTAL) {
+            state.workingLine.setLine(transY0, transX0, transY1, transX1);
+        } else if (orientation == PlotOrientation.VERTICAL) {
+            state.workingLine.setLine(transX0, transY0, transX1, transY1);
+        }
+        visible = LineUtilities.clipLine(state.workingLine, dataArea);
+        if (visible) {
+            drawFirstPassShape(g2, pass, series, item, state.workingLine);
+        }
+    }
+
+    @Override
+    protected void drawSecondaryPass(Graphics2D g2, XYPlot plot,
+            XYDataset dataset, int pass, int series, int item,
+            ValueAxis domainAxis, Rectangle2D dataArea, ValueAxis rangeAxis,
+            CrosshairState crosshairState, EntityCollection entities) {
+        
+        Shape entityArea = null;
+
+        // get the data point...
+        double x1 = dataset.getXValue(series, item);
+        double y1 = dataset.getYValue(series, item);
+        if (Double.isNaN(y1) || Double.isNaN(x1)) {
+            return;
+        }
+
+        PlotOrientation orientation = plot.getOrientation();
+        RectangleEdge xAxisLocation = plot.getDomainAxisEdge();
+        RectangleEdge yAxisLocation = plot.getRangeAxisEdge();
+
+        double transX0 = domainAxis.valueToJava2D(0, dataArea, xAxisLocation);
+        double transY0 = rangeAxis.valueToJava2D(0, dataArea, yAxisLocation);
+
+        double transX1 = domainAxis.valueToJava2D(x1, dataArea, xAxisLocation);
+        double transY1 = rangeAxis.valueToJava2D(y1, dataArea, yAxisLocation);
+
+        double vecX = transX1 - transX0;
+        double vecY = transY1 - transY0;
+
+        if (getItemShapeVisible(series, item)) {
+            Shape shape = getItemShape(series, item);
+            // align shape direction
+            double angle = Math.PI - Math.acos(vecY / Math.sqrt(vecX * vecX + vecY * vecY));
+            shape = AffineTransform.getRotateInstance(angle).createTransformedShape(shape);
+            if (vecX < 0) {
+                // arrow leftwards, flip shape
+                shape = AffineTransform.getScaleInstance(-1, 1).createTransformedShape(shape);
+            }
+
+            if (orientation == PlotOrientation.HORIZONTAL) {
+                shape = ShapeUtilities.createTranslatedShape(shape, transY1,
+                        transX1);
+            } else if (orientation == PlotOrientation.VERTICAL) {
+                shape = ShapeUtilities.createTranslatedShape(shape, transX1,
+                        transY1);
+            }
+
+            entityArea = shape;
+            if (shape.intersects(dataArea)) {
+                if (getItemShapeFilled(series, item)) {
+                    if (getUseFillPaint()) {
+                        g2.setPaint(getItemFillPaint(series, item));
+                    } else {
+                        g2.setPaint(getItemPaint(series, item));
+                    }
+                    g2.fill(shape);
+                }
+                if (getDrawOutlines()) {
+                    if (getUseOutlinePaint()) {
+                        g2.setPaint(getItemOutlinePaint(series, item));
+                    } else {
+                        g2.setPaint(getItemPaint(series, item));
+                    }
+                    g2.setStroke(getItemOutlineStroke(series, item));
+                    g2.draw(shape); 
+                   // g2.drawString(String.valueOf(angle), (float)transX1 + 5, (float)transY1 + 5);
+                }
+            }
+        }
+
+        double xx = transX1;
+        double yy = transY1;
+        if (orientation == PlotOrientation.HORIZONTAL) {
+            xx = transY1;
+            yy = transX1;
+        }
+
+        // draw the item label if there is one...
+        if (isItemLabelVisible(series, item)) {
+            drawItemLabel(g2, orientation, dataset, series, item, xx, yy,
+                    (y1 < 0.0));
+        }
+
+        int domainAxisIndex = plot.getDomainAxisIndex(domainAxis);
+        int rangeAxisIndex = plot.getRangeAxisIndex(rangeAxis);
+        updateCrosshairValues(crosshairState, x1, y1, domainAxisIndex,
+                rangeAxisIndex, transX1, transY1, orientation);
+
+        // add an entity for the item, but only if it falls within the data
+        // area...
+        if (entities != null && isPointInRect(dataArea, xx, yy)) {
+            addEntity(entities, entityArea, dataset, series, item, xx, yy);
+        }
+    }
+
+}
--- MGX-PCA/src/main/java/de/cebitec/mgx/gui/pca/PCAPlot.java
+++ MGX-PCA/src/main/java/de/cebitec/mgx/gui/pca/PCAPlot.java
@@ -3,19 +3,23 @@ package de.cebitec.mgx.gui.pca;
 import de.cebitec.mgx.gui.attributevisualization.viewer.ViewerI;
 import de.cebitec.mgx.gui.charts.basic.util.JFreeChartUtil;
 import de.cebitec.mgx.gui.controller.MGXMaster;
+import de.cebitec.mgx.gui.datamodel.Attribute;
 import de.cebitec.mgx.gui.datamodel.AttributeType;
 import de.cebitec.mgx.gui.datamodel.misc.Distribution;
 import de.cebitec.mgx.gui.datamodel.misc.PCAResult;
 import de.cebitec.mgx.gui.datamodel.misc.Pair;
 import de.cebitec.mgx.gui.datamodel.misc.Point;
+import de.cebitec.mgx.gui.groups.ConflictingJobsException;
 import de.cebitec.mgx.gui.groups.ImageExporterI;
 import de.cebitec.mgx.gui.groups.VGroupManager;
 import de.cebitec.mgx.gui.groups.VisualizationGroup;
-import de.cebitec.mgx.gui.swingutils.NonEDT;
 import java.awt.Color;
 import java.awt.geom.Ellipse2D;
-import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import javax.swing.JComponent;
 import javax.swing.SwingWorker;
@@ -24,7 +28,6 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.AxisLocation;
 import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.labels.StandardXYItemLabelGenerator;
 import org.jfree.chart.labels.XYItemLabelGenerator;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
@@ -64,7 +67,14 @@ public class PCAPlot extends ViewerI<Distribution> {
 
     @Override
     public boolean canHandle(AttributeType valueType) {
-        return VGroupManager.getInstance().getActiveGroups().size() > 1;
+        Set<Attribute> attrs = new HashSet<>();
+        try {
+            for (Pair<VisualizationGroup, Distribution> p : VGroupManager.getInstance().getDistributions()) {
+                attrs.addAll(p.getSecond().keySet());
+            }
+        } catch (ConflictingJobsException ex) {
+        }
+        return attrs.size() > 1 && VGroupManager.getInstance().getActiveGroups().size() > 1;
     }
 
     @Override
@@ -75,7 +85,7 @@ public class PCAPlot extends ViewerI<Distribution> {
     @Override
     public void show(final List<Pair<VisualizationGroup, Distribution>> dists) {
         final MGXMaster master = (MGXMaster) dists.get(0).getSecond().getMaster();
-        
+
         SwingWorker<PCAResult, Void> sw = new SwingWorker<PCAResult, Void>() {
 
             @Override
@@ -84,7 +94,7 @@ public class PCAPlot extends ViewerI<Distribution> {
             }
         };
         sw.execute();
-        
+
         PCAResult pca = null;
         try {
             pca = sw.get();
@@ -101,29 +111,34 @@ public class PCAPlot extends ViewerI<Distribution> {
         String pc1rel = String.format("%2.2f%n", variances[0] * 100 / varSum);
         String pc2rel = String.format("%2.2f%n", variances[1] * 100 / varSum);
 
+        final Map<XYDataItem, String> toolTips = new HashMap<>();
+
         XYSeriesCollection dataset = new XYSeriesCollection();
-        XYSeries series = new XYSeries("FOO");
+        XYSeries series = new XYSeries("");
         for (Point p : pca.getDatapoints()) {
-            series.add(new XYDataItem(p.getX(), p.getY()));
+            XYDataItem item = new XYDataItem(p.getX(), p.getY());
+            series.add(item);
+            toolTips.put(item, p.getName());
         }
         dataset.addSeries(series);
-        XYSeries loadings = new XYSeries("BAR");
-        for (Point p : pca.getLoadings()) {
-            loadings.add(new XYDataItem(p.getX(), p.getY()));
-        }
-        dataset.addSeries(loadings);
 
-        chart = ChartFactory.createXYLineChart(getTitle(), "PC1 (" + pc1rel + "%)", "PC2 (" + pc2rel + "%)", dataset, PlotOrientation.VERTICAL, false, true, false);
+        // second dataseries with loadings
+        XYSeriesCollection loadingset = new XYSeriesCollection();
+        XYSeries loadings = new XYSeries("");
+        for (Point p : pca.getLoadings()) {
+            XYDataItem item = new XYDataItem(p.getX(), p.getY());
+            loadings.add(item);
+            toolTips.put(item, p.getName());
+        }
+        loadingset.addSeries(loadings);
+
+        chart = ChartFactory.createScatterPlot(getTitle(), "PC1 (" + pc1rel + "%)", "PC2 (" + pc2rel + "%)", dataset, PlotOrientation.VERTICAL, false, true, false);
         chart.setBorderPaint(Color.WHITE);
         chart.setBackgroundPaint(Color.WHITE);
         cPanel = new ChartPanel(chart);
         cPanel.setDisplayToolTips(true);
         XYPlot plot = (XYPlot) chart.getPlot();
         plot.setBackgroundPaint(Color.WHITE);
-
-        NumberAxis axis2 = new NumberAxis("Range Axis 2");
-        plot.setRangeAxis(1, axis2);
-        plot.setRangeAxisLocation(1, AxisLocation.BOTTOM_OR_RIGHT);
 
         // renderer for data points
         final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
@@ -132,13 +147,28 @@ public class PCAPlot extends ViewerI<Distribution> {
         XYItemLabelGenerator labelGen = new XYItemLabelGenerator() {
 
             @Override
-            public String generateLabel(XYDataset xyd, int i, int i1) {
-                return "FOOO";
+            public String generateLabel(XYDataset xyd, int series, int item) {
+                XYSeriesCollection dataset = (XYSeriesCollection) xyd;
+                XYDataItem dataItem = dataset.getSeries(series).getDataItem(item);
+                return toolTips.get(dataItem);
             }
         };
-        renderer.setSeriesItemLabelGenerator(1, labelGen);
-        renderer.setSeriesItemLabelsVisible(1, Boolean.TRUE);
-        plot.setRenderer(1, renderer);
+        renderer.setSeriesItemLabelGenerator(0, labelGen);
+        renderer.setSeriesItemLabelsVisible(0, Boolean.TRUE);
+        plot.setRenderer(0, renderer);
+
+        // add loadings
+        plot.setDataset(1, loadingset);
+        NumberAxis axis = new NumberAxis("loadings");
+        plot.setRangeAxis(1, axis);
+        plot.setRangeAxisLocation(1, AxisLocation.BOTTOM_OR_RIGHT);
+        XYItemRenderer arrowRenderer = new ArrowRenderer();
+        arrowRenderer.setSeriesItemLabelGenerator(0, labelGen);
+        arrowRenderer.setSeriesItemLabelsVisible(0, Boolean.TRUE);
+        arrowRenderer.setSeriesItemLabelGenerator(1, labelGen);
+        arrowRenderer.setSeriesItemLabelsVisible(1, Boolean.TRUE);
+        plot.setRenderer(1, arrowRenderer);
+        plot.mapDatasetToRangeAxis(1, 1);
     }
 
     @Override
@@ -146,4 +176,8 @@ public class PCAPlot extends ViewerI<Distribution> {
         return null;
     }
 
+    @Override
+    protected String getTitle() {
+        return "PCA plot for " + getAttributeType().getName();
+    }
 }
