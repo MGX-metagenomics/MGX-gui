067b088d2a50764f0dbd762fb6626546213ad2de
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-05-09 14:24:55 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@2213 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Nodes/src/main/java/de/cebitec/mgx/gui/actions/OpenMappingByReference.java
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/actions/OpenMappingByReference.java
@@ -6,10 +6,20 @@
 package de.cebitec.mgx.gui.actions;
 
 import de.cebitec.mgx.gui.controller.MGXMaster;
+import de.cebitec.mgx.gui.datamodel.Job;
 import de.cebitec.mgx.gui.datamodel.Mapping;
 import de.cebitec.mgx.gui.datamodel.Reference;
+import de.cebitec.mgx.gui.mapping.MappingCtx;
+import de.cebitec.mgx.gui.mapping.viewer.TopComponentViewer;
+import de.cebitec.mgx.gui.swingutils.NonEDT;
+import de.cebitec.mgx.gui.wizard.mapping.MappingWizardWizardAction;
 import java.awt.event.ActionEvent;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import javax.swing.SwingWorker;
+import org.openide.util.Exceptions;
 import org.openide.util.Utilities;
 
 /**
@@ -18,20 +28,83 @@ import org.openide.util.Utilities;
  */
 public class OpenMappingByReference extends OpenMappingBase {
 
-    private final Iterator<Mapping> mappings;
-    private final boolean hasData;
+    private boolean hasData = false;
 
     public OpenMappingByReference() {
         super();
         final MGXMaster m = Utilities.actionsGlobalContext().lookup(MGXMaster.class);
         final Reference ref = Utilities.actionsGlobalContext().lookup(Reference.class);
-        mappings = m.Mapping().ByReference(ref.getId());
-        hasData = mappings.hasNext();
+
+        if (m == null || ref == null) {
+            return;
+        }
+        NonEDT.invokeAndWait(new Runnable() {
+
+            @Override
+            public void run() {
+                Iterator<Mapping> mappings = m.Mapping().ByReference(ref.getId());
+                hasData = mappings.hasNext();
+            }
+        });
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        final MGXMaster master = Utilities.actionsGlobalContext().lookup(MGXMaster.class);
+        final Reference ref = Utilities.actionsGlobalContext().lookup(Reference.class);
+
+        SwingWorker<List<MappingCtx>, Void> worker = new SwingWorker<List<MappingCtx>, Void>() {
+
+            @Override
+            protected List<MappingCtx> doInBackground() throws Exception {
+                List<MappingCtx> ctxs = new LinkedList<>();
+                Iterator<Mapping> mappings = master.Mapping().ByReference(ref.getId());
+                while (mappings.hasNext()) {
+                    Mapping m = mappings.next();
+
+                    Job job = master.Job().fetch(m.getJobID());
+                    if (job.getSeqrun() == null) {
+                        job.setSeqrun(master.SeqRun().fetch(m.getSeqrunID()));
+                    }
+                    if (job.getTool() == null) {
+                        job.setTool(master.Tool().ByJob(job.getId()));
+                    }
+                    ctxs.add(new MappingCtx(m, ref, job));
+                }
+                return ctxs;
+            }
+
+            @Override
+            protected void done() {
+                List<MappingCtx> ctxs = null;
+                try {
+                    ctxs = get();
+                } catch (InterruptedException | ExecutionException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+                MappingCtx selectedMapping = null;
+
+                if (ctxs != null && ctxs.size() == 1) {
+                    // only one mapping exists, show it..
+                    selectedMapping = ctxs.get(0);
+                } else if (ctxs != null && ctxs.size() > 1) {
+                    // let user choose
+                    selectedMapping = MappingWizardWizardAction.selectMapping(ctxs);
+                } else {
+                    // should not happen
+                    assert false;
+                }
+
+                if (selectedMapping != null) {
+                    TopComponentViewer component = new TopComponentViewer(selectedMapping);
+                    component.open();
+                }
+                super.done();
+            }
+
+        };
+        worker.execute();
     }
 
     @Override
--- MGX-Nodes/src/main/java/de/cebitec/mgx/gui/actions/OpenMappingBySeqRun.java
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/actions/OpenMappingBySeqRun.java
@@ -8,6 +8,7 @@ package de.cebitec.mgx.gui.actions;
 import de.cebitec.mgx.gui.controller.MGXMaster;
 import de.cebitec.mgx.gui.datamodel.Mapping;
 import de.cebitec.mgx.gui.datamodel.SeqRun;
+import de.cebitec.mgx.gui.swingutils.NonEDT;
 import java.awt.event.ActionEvent;
 import java.util.Iterator;
 import org.openide.util.Utilities;
@@ -18,15 +19,26 @@ import org.openide.util.Utilities;
  */
 public class OpenMappingBySeqRun extends OpenMappingBase {
 
-    private final Iterator<Mapping> mappings;
-    private final boolean hasData;
+    private Iterator<Mapping> mappings;
+    private boolean hasData = false;
 
     public OpenMappingBySeqRun() {
         super();
         final MGXMaster m = Utilities.actionsGlobalContext().lookup(MGXMaster.class);
         final SeqRun run = Utilities.actionsGlobalContext().lookup(SeqRun.class);
-        mappings = m.Mapping().BySeqRun(run.getId());
-        hasData = mappings.hasNext();
+        
+        if (m == null || run == null) {
+            return;
+        }
+        NonEDT.invokeAndWait(new Runnable() {
+
+            @Override
+            public void run() {
+                mappings = m.Mapping().BySeqRun(run.getId());
+                hasData = mappings.hasNext();
+            }
+        });
+
     }
 
     @Override
