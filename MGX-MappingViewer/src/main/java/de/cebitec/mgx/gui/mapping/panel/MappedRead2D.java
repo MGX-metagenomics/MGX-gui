e067110570ea5657ff123cb514161c7dc0c88321
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-05-16 13:02:43 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@2278 2f99df6c-41e9-6383-b5e2-de53e728a4e2

---
+++ MGX-MappingViewer/src/main/java/de/cebitec/mgx/gui/mapping/panel/MappedRead2D.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.mapping.panel;
+
+import de.cebitec.mgx.gui.datamodel.MappedSequence;
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ *
+ * @author sj
+ */
+public class MappedRead2D implements Shape {
+
+    private final MappedSequence ms;
+    private final Shape shape;
+    private final static Color[] gradient = new Color[100];
+
+    static {
+        generateGradient();
+    }
+
+    public MappedRead2D(MappedSequence ms, double x, double y, double length) {
+        this.ms = ms;
+        shape = new Rectangle2D.Double(x, y, length, 4);
+    }
+    
+    public Color getColor() {
+        return gradient[ms.getIdentity()];
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        return shape.getBounds();
+    }
+
+    @Override
+    public Rectangle2D getBounds2D() {
+        return shape.getBounds2D();
+    }
+
+    @Override
+    public boolean contains(double x, double y) {
+        return shape.contains(x, y);
+    }
+
+    @Override
+    public boolean contains(Point2D p) {
+        return shape.contains(p);
+    }
+
+    @Override
+    public boolean intersects(double x, double y, double w, double h) {
+        return shape.intersects(x, y, w, h);
+    }
+
+    @Override
+    public boolean intersects(Rectangle2D r) {
+        return shape.intersects(r);
+    }
+
+    @Override
+    public boolean contains(double x, double y, double w, double h) {
+        return shape.contains(x, y, w, h);
+    }
+
+    @Override
+    public boolean contains(Rectangle2D r) {
+        return shape.contains(r);
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform at) {
+        return shape.getPathIterator(at);
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform at, double flatness) {
+        return shape.getPathIterator(at, flatness);
+    }
+
+    private static void generateGradient() {
+        Color color1 = Color.RED;
+        Color color2 = Color.GREEN;
+        for (int i = 0; i < 100; i++) {
+            float ratio = (float) i / (float) 100;
+            int red = (int) (color2.getRed() * ratio + color1.getRed() * (1 - ratio));
+            int green = (int) (color2.getGreen() * ratio + color1.getGreen() * (1 - ratio));
+            int blue = (int) (color2.getBlue() * ratio + color1.getBlue() * (1 - ratio));
+            gradient[i] = new Color(red, green, blue);
+        }
+    }
+}
