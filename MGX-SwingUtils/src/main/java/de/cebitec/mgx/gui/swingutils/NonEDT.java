393539cd8514d86a9baf8a1881e253f6f190a6a8
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-05-20 21:22:24 +0000

    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@2297 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-SwingUtils/src/main/java/de/cebitec/mgx/gui/swingutils/NonEDT.java
+++ MGX-SwingUtils/src/main/java/de/cebitec/mgx/gui/swingutils/NonEDT.java
@@ -1,5 +1,6 @@
 package de.cebitec.mgx.gui.swingutils;
 
+import java.awt.EventQueue;
 import java.util.concurrent.ExecutionException;
 import javax.swing.SwingWorker;
 import org.openide.util.Exceptions;
@@ -11,39 +12,47 @@ import org.openide.util.Exceptions;
 public class NonEDT {
 
     public static void invoke(final Runnable run) {
-        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
-            @Override
-            protected Void doInBackground() throws Exception {
-                run.run();
-                return null;
-            }
-
-            @Override
-            protected void done() {
-                try {
-                    get();
-                } catch (InterruptedException | ExecutionException ex) {
-                    Exceptions.printStackTrace(ex);
+        if (EventQueue.isDispatchThread()) {
+            SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+                @Override
+                protected Void doInBackground() throws Exception {
+                    run.run();
+                    return null;
                 }
-                super.done();
-            }
-        };
-        worker.execute();
+
+                @Override
+                protected void done() {
+                    try {
+                        get();
+                    } catch (InterruptedException | ExecutionException ex) {
+                        Exceptions.printStackTrace(ex);
+                    }
+                    super.done();
+                }
+            };
+            worker.execute();
+        } else {
+            run.run();
+        }
     }
 
     public static void invokeAndWait(final Runnable run) {
-        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
-            @Override
-            protected Void doInBackground() throws Exception {
-                run.run();
-                return null;
+        if (EventQueue.isDispatchThread()) {
+            SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+                @Override
+                protected Void doInBackground() throws Exception {
+                    run.run();
+                    return null;
+                }
+            };
+            worker.execute();
+            try {
+                worker.get();
+            } catch (InterruptedException | ExecutionException ex) {
+                Exceptions.printStackTrace(ex);
             }
-        };
-        worker.execute();
-        try {
-            worker.get();
-        } catch (InterruptedException | ExecutionException ex) {
-            Exceptions.printStackTrace(ex);
+        } else {
+            run.run();
         }
     }
 }
