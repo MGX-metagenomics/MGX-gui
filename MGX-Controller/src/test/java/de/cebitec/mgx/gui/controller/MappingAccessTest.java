f7e68834f90f582d0388c03646c3fd68d902f556
Sebastian Jaenicke <sjaenick@CebiTec.Uni-Bielefeld.DE>
2014-05-26 15:11:25 +0000

    more tests
    git-svn-id: https://svn.cebitec.uni-bielefeld.de/svn/mgx@2360 2f99df6c-41e9-6383-b5e2-de53e728a4e2

--- MGX-Controller/src/test/java/de/cebitec/mgx/gui/controller/MappingAccessTest.java
+++ MGX-Controller/src/test/java/de/cebitec/mgx/gui/controller/MappingAccessTest.java
@@ -8,6 +8,8 @@ package de.cebitec.mgx.gui.controller;
 import de.cebitec.mgx.gui.datamodel.MappedSequence;
 import de.cebitec.mgx.gui.util.TestMaster;
 import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.UUID;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -61,4 +63,42 @@ public class MappingAccessTest {
         assertEquals(3, numMappedReads);
     }
 
+    @Test
+    public void testMappedSeqs2() throws Exception {
+        System.out.println("testMappedSeqs2");
+        MGXMaster master = TestMaster.getRO();
+        UUID uuid = master.Mapping().openMapping(30);
+        int numMappedReads = 0;
+        assertNotNull(uuid);
+        Iterator<MappedSequence> iter = master.Mapping().byReferenceInterval(uuid, 566470, 566480);
+        assertNotNull(iter);
+
+        SortedSet<MappedSequence> set = new TreeSet<>();
+
+        while (iter.hasNext()) {
+            MappedSequence ms = iter.next();
+            assertNotNull(ms);
+            //System.err.println("got "+ms);
+            assertFalse(set.contains(ms));
+            //System.err.println("adding "+ms);
+            set.add(ms);
+            assertTrue(set.contains(ms));
+            numMappedReads++;
+        }
+
+        master.Mapping().closeMapping(uuid);
+
+        assertEquals(3, numMappedReads);
+        assertEquals(3, set.size());
+
+        for (long l : new long[]{53748, 3436, 26467}) {
+            boolean present = false;
+            for (MappedSequence ms : set) {
+                if (ms.getSeqId() == l) {
+                    present = true;
+                }
+            }
+            assertTrue("expected seqid " + l + " not in result", present);
+        }
+    }
 }
