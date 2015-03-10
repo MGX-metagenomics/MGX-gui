99972c8f1b05697dd7fc9925831e68d5d1bcb009
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2015-03-10 09:48:28 +0100

    minor updates

---
+++ MGX-VizFilter/src/main/java/de/cebitec/mgx/gui/vizfilter/LongToDouble.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.vizfilter;
+
+import de.cebitec.mgx.api.groups.VisualizationGroupI;
+import de.cebitec.mgx.api.misc.DistributionI;
+import de.cebitec.mgx.api.misc.Pair;
+import de.cebitec.mgx.api.model.AttributeI;
+import de.cebitec.mgx.api.visualization.filter.VisFilterI;
+import de.cebitec.mgx.gui.datamodel.misc.NormalizedDistribution;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class LongToDouble implements VisFilterI<DistributionI<Long>, DistributionI<Double>> {
+
+    @Override
+    public List<Pair<VisualizationGroupI, DistributionI<Double>>> filter(List<Pair<VisualizationGroupI, DistributionI<Long>>> in) {
+        List<Pair<VisualizationGroupI, DistributionI<Double>>> ret = new ArrayList<>();
+        for (Pair<VisualizationGroupI, DistributionI<Long>> p : in) {
+            VisualizationGroupI vg = p.getFirst();
+            DistributionI<Long> d = p.getSecond();
+            Map<AttributeI, Double> tmp = new HashMap<>();
+            for (Map.Entry<AttributeI, Long> e : d.entrySet()) {
+                tmp.put(e.getKey(), e.getValue().doubleValue());
+            }
+            DistributionI<Double> converted = new NormalizedDistribution(d.getMaster(), tmp);
+            ret.add(new Pair<>(vg, converted));
+        }
+        return ret;
+    }
+
+}
