7cf10daaefd0cf7bb9909ed7561c6f1ea7d01deb
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2015-07-08 15:24:00 +0200

    refactoring

---
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/actions/EditSeqRun.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.actions;
+
+import de.cebitec.mgx.api.MGXMasterI;
+import de.cebitec.mgx.api.model.SeqRunI;
+import de.cebitec.mgx.gui.controller.RBAC;
+import de.cebitec.mgx.gui.wizard.seqrun.SeqRunWizardDescriptor;
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.util.concurrent.ExecutionException;
+import javax.swing.AbstractAction;
+import javax.swing.SwingWorker;
+import org.openide.DialogDisplayer;
+import org.openide.WizardDescriptor;
+import org.openide.util.Exceptions;
+import org.openide.util.Utilities;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class EditSeqRun extends AbstractAction {
+
+    public EditSeqRun() {
+        putValue(NAME, "Edit");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        final SeqRunI seqrun = Utilities.actionsGlobalContext().lookup(SeqRunI.class);
+        SeqRunWizardDescriptor wd = new SeqRunWizardDescriptor(seqrun);
+        Dialog dialog = DialogDisplayer.getDefault().createDialog(wd);
+        dialog.setVisible(true);
+        dialog.toFront();
+        boolean cancelled = wd.getValue() != WizardDescriptor.FINISH_OPTION;
+        if (!cancelled) {
+            final SeqRunI run = wd.getSeqRun(seqrun.getMaster());
+            SwingWorker<Void, Void> sw = new SwingWorker<Void, Void>() {
+                @Override
+                protected Void doInBackground() throws Exception {
+                    MGXMasterI m = Utilities.actionsGlobalContext().lookup(MGXMasterI.class);
+                    m.SeqRun().update(run);
+                    return null;
+                }
+
+                @Override
+                protected void done() {
+                    try {
+                        get();
+                    } catch (InterruptedException | ExecutionException ex) {
+                        Exceptions.printStackTrace(ex);
+                    }
+                    super.done();
+                }
+            };
+            sw.execute();
+            //                setDisplayName(seqrun.getName());
+            //                setShortDescription(getToolTipText(seqrun));
+            //                fireDisplayNameChange(oldDisplayName, seqrun.getName());
+        }
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return super.isEnabled() && RBAC.isUser();
+    }
+    
+}
