1c39ad74c633139b601fff5276e0b07ef91fabbb
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2015-08-04 15:52:30 +0200

    extend tests

---
+++ MGX-Controller/src/test/java/de/cebitec/mgx/gui/controller/ReferenceAccessTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.controller;
+
+import de.cebitec.mgx.api.MGXMasterI;
+import de.cebitec.mgx.api.access.datatransfer.UploadBaseI;
+import de.cebitec.mgx.api.exception.MGXException;
+import de.cebitec.mgx.api.misc.TaskI;
+import de.cebitec.mgx.api.model.MGXReferenceI;
+import de.cebitec.mgx.gui.util.TestMaster;
+import java.io.File;
+import java.util.Iterator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Assume;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * @author sjaenick
+ */
+public class ReferenceAccessTest {
+
+    @Test
+    public void testCreateUploader() throws MGXException {
+        System.out.println("createUploader");
+        MGXMasterI master = TestMaster.getPrivate("MGX_Mammoth");
+        Assume.assumeNotNull(master);
+
+        File f = new File("/vol/biodb/ncbi_genomes/Bacteria/Variovorax_paradoxus_B4_uid218005/NC_022234.gbk");
+        Assume.assumeTrue(f.exists());
+
+        UploadBaseI up = master.Reference().createUploader(f);
+        boolean success = up.upload();
+
+        if (!success) {
+            String err = up.getErrorMessage();
+            assertNotNull(err);
+            assertNotEquals("", err);
+            fail(err);
+        }
+        assertTrue(success);
+
+        MGXReferenceI ref = null;
+        Iterator<MGXReferenceI> iter = master.Reference().fetchall();
+        while (iter.hasNext()) {
+            ref = iter.next();
+            if (ref.getName().equals("Variovorax paradoxus B4 chromosome 2")) {
+                break;
+            }
+        }
+        assertNotNull(ref);
+        TaskI<MGXReferenceI> task = master.Reference().delete(ref);
+        while ((task.getState() != TaskI.State.FINISHED) || (task.getState() != TaskI.State.FAILED)) {
+            System.err.println(" --> " + task.getState());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+            if ((task.getState() == TaskI.State.FINISHED) || (task.getState() == TaskI.State.FAILED)) {
+                break;
+            } else {
+                master.<MGXReferenceI>Task().refresh(task);
+            }
+        }
+    }
+
+}
