53413be2ac82ffc290b874f3bde321937c7a6b43
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2015-08-12 13:39:51 +0200

    adapt image exporters to api change

--- MGX-Treeview/src/main/java/de/cebitec/mgx/gui/treeview/TreeView.java
+++ MGX-Treeview/src/main/java/de/cebitec/mgx/gui/treeview/TreeView.java
@@ -477,14 +477,17 @@ public class TreeView extends HierarchicalViewerI {
             }
 
             @Override
-            public boolean export(FileType type, String fName) throws Exception {
+            public Result export(FileType type, String fName) throws Exception {
                 switch (type) {
                     case PNG:
                         try (OutputStream os = new BufferedOutputStream(new FileOutputStream(fName))) {
-                            return display.saveImage(os, type.getSuffices()[0].toUpperCase(), 2);
+                            if (display.saveImage(os, type.getSuffices()[0].toUpperCase(), 2)) {
+                                return Result.SUCCESS;
+                            }
+                            return Result.ERROR;
                         }
                     default:
-                        return false;
+                        return Result.ERROR;
                 }
 
             }
