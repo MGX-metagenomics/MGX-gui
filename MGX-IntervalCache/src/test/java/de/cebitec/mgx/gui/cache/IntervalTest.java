2322d2bd0be62f5c03e509887cbd9c3548c681d5
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-02-26 13:48:57 +0100

    handle GPMS-related exception after minor API change

--- MGX-IntervalCache/src/test/java/de/cebitec/mgx/gui/cache/IntervalTest.java
+++ MGX-IntervalCache/src/test/java/de/cebitec/mgx/gui/cache/IntervalTest.java
@@ -98,8 +98,10 @@ public class IntervalTest {
             }
         }
         GPMSClientI gpms = new GPMSClient("MyServer", serverURI);
-        if (!gpms.login("mgx_unittestRO", "gut-isM5iNt")) {
-            fail();
+        try {
+            gpms.login("mgx_unittestRO", "gut-isM5iNt");
+        } catch (GPMSException ex) {
+            fail(ex.getMessage());
         }
         Iterator<MembershipI> mIter = null;
         try {
