a846a690a265916cd619746055f9b074ab5f9fe9
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-04-07 16:40:33 +0200

    add test

--- MGX-Controller/src/test/java/de/cebitec/mgx/gui/groups/VisualizationGroupTest.java
+++ MGX-Controller/src/test/java/de/cebitec/mgx/gui/groups/VisualizationGroupTest.java
@@ -7,22 +7,31 @@ package de.cebitec.mgx.gui.groups;
 
 import de.cebitec.mgx.api.MGXMasterI;
 import de.cebitec.mgx.api.groups.ConflictingJobsException;
+import de.cebitec.mgx.api.groups.ReplicateGroupI;
+import de.cebitec.mgx.api.groups.ReplicateI;
 import de.cebitec.mgx.api.groups.VGroupManagerI;
 import de.cebitec.mgx.api.groups.VisualizationGroupI;
 import de.cebitec.mgx.api.misc.AttributeRank;
+import de.cebitec.mgx.api.misc.DistributionI;
+import de.cebitec.mgx.api.misc.Pair;
+import de.cebitec.mgx.api.misc.Triple;
 import de.cebitec.mgx.api.model.AttributeTypeI;
 import de.cebitec.mgx.api.model.JobI;
 import de.cebitec.mgx.api.model.JobState;
 import de.cebitec.mgx.api.model.SeqRunI;
+import de.cebitec.mgx.api.model.tree.TreeI;
+import de.cebitec.mgx.api.visualization.ConflictResolver;
 import de.cebitec.mgx.common.VGroupManager;
 import de.cebitec.mgx.gui.util.TestMaster;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 
@@ -32,11 +41,10 @@ import org.junit.Test;
  */
 public class VisualizationGroupTest {
 
-
     @Test
     public void testSetName() {
         System.out.println("setName");
-        VisualizationGroupI vg = VGroupManager.getInstance().createVisualizationGroup();
+        VisualizationGroupI vg = VGroupManager.getTestInstance().createVisualizationGroup();
         String name = UUID.randomUUID().toString();
         vg.setName(name);
         assertEquals(name, vg.getDisplayName());
@@ -46,7 +54,7 @@ public class VisualizationGroupTest {
     public void testGetNumSequences() throws Exception {
         System.out.println("getNumSequences");
 
-        VGroupManagerI vgmgr = VGroupManager.getInstance();
+        VGroupManagerI vgmgr = VGroupManager.getTestInstance();
         synchronized (vgmgr) {
             VisualizationGroupI vg = vgmgr.createVisualizationGroup();
             MGXMasterI master = TestMaster.getRO();
@@ -64,7 +72,7 @@ public class VisualizationGroupTest {
     @Test
     public void testGetSeqRuns() throws Exception {
         System.out.println("getSeqRuns");
-        VGroupManagerI vgmgr = VGroupManager.getInstance();
+        VGroupManagerI vgmgr = VGroupManager.getTestInstance();
         synchronized (vgmgr) {
             VisualizationGroupI vg = vgmgr.createVisualizationGroup();
             MGXMasterI master = TestMaster.getRO();
@@ -82,7 +90,7 @@ public class VisualizationGroupTest {
     @Test
     public void testGetAttributeTypes() throws Exception {
         System.out.println("getAttributeTypes");
-        VGroupManagerI vgmgr = VGroupManager.getInstance();
+        VGroupManagerI vgmgr = VGroupManager.getTestInstance();
         synchronized (vgmgr) {
             VisualizationGroupI vg = vgmgr.createVisualizationGroup();
             MGXMasterI master = TestMaster.getRO();
@@ -105,7 +113,7 @@ public class VisualizationGroupTest {
     @Test
     public void testaddAndRemoveRuns() throws Exception {
         System.out.println("addAndRemoveRuns");
-        VGroupManagerI vgmgr = VGroupManager.getInstance();
+        VGroupManagerI vgmgr = VGroupManager.getTestInstance();
         VisualizationGroupI vg = vgmgr.createVisualizationGroup();
         MGXMasterI master = TestMaster.getRO();
 
@@ -143,7 +151,7 @@ public class VisualizationGroupTest {
     public void testGetSelectedAttributeTypeRegression() throws Exception {
         System.out.println("getSelectedAttributeTypeRegression");
 
-        VGroupManagerI vgmgr = VGroupManager.getInstance();
+        VGroupManagerI vgmgr = VGroupManager.getTestInstance();
         VisualizationGroupI vg = vgmgr.createVisualizationGroup();
         MGXMasterI master = TestMaster.getRO();
         SeqRunI run = master.SeqRun().fetch(1);
@@ -171,7 +179,7 @@ public class VisualizationGroupTest {
     @Test
     public void testGetSelectedAttributeType() throws Exception {
         System.out.println("getSelectedAttributeType");
-        VGroupManagerI vgmgr = VGroupManager.getInstance();
+        VGroupManagerI vgmgr = VGroupManager.getTestInstance();
         VisualizationGroupI vg = vgmgr.createVisualizationGroup();
         MGXMasterI master = TestMaster.getRO();
         Iterator<SeqRunI> iter = master.SeqRun().fetchall();
@@ -198,89 +206,58 @@ public class VisualizationGroupTest {
         }
         fail();
     }
-//    @Test
-//    public void testSelectAttributeType() throws Exception {
-//        System.out.println("selectAttributeType");
-//        AttributeRank rank = null;
-//        String attrType = "";
-//        VisualizationGroup instance = null;
-//        instance.selectAttributeType(rank, attrType);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//    @Test
-//    public void testGetConflicts() {
-//        System.out.println("getConflicts");
-//        VisualizationGroup instance = null;
-//        List<Triple<AttributeRank, SeqRun, List<Job>>> expResult = null;
-//        List<Triple<AttributeRank, SeqRun, List<Job>>> result = instance.getConflicts();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//    @Test
-//    public void testResolveConflict() {
-//        System.out.println("resolveConflict");
-//        AttributeRank rank = null;
-//        SeqRun sr = null;
-//        Job j = null;
-//        VisualizationGroup instance = null;
-//        instance.resolveConflict(rank, sr, j);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//    @Test
-//    public void testAddSeqRun() {
-//        System.out.println("addSeqRun");
-//        SeqRun sr = null;
-//        VisualizationGroup instance = null;
-//        instance.addSeqRun(sr);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    @Test
-//    public void testRemoveSeqRun() {
-//        System.out.println("removeSeqRun");
-//        SeqRun sr = null;
-//        VisualizationGroup instance = null;
-//        instance.removeSeqRun(sr);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    @Test
-//    public void testGetHierarchy() {
-//        System.out.println("getHierarchy");
-//        VisualizationGroup instance = null;
-//        Tree<Long> expResult = null;
-//        Tree<Long> result = instance.getHierarchy();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    @Test
-//    public void testGetDistribution() throws Exception {
-//        System.out.println("getDistribution");
-//        VisualizationGroup instance = null;
-//        Distribution expResult = null;
-//        Distribution result = instance.getDistribution();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//
-//    @Test
-//    public void testGetSaveSet() {
-//        System.out.println("getSaveSet");
-//        List<String> requestedAttrs = null;
-//        VisualizationGroup instance = null;
-//        Map<SeqRun, Set<Attribute>> expResult = null;
-//        Map<SeqRun, Set<Attribute>> result = instance.getSaveSet(requestedAttrs);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+
+    @Test
+    public void testReplicateGroupRegression() throws Exception {
+        System.out.println("testReplicateGroupRegression");
+        VGroupManagerI vgmgr = VGroupManager.getTestInstance();
+        SeqRunI seqrun = TestMaster.getRO().SeqRun().fetch(2); //dataset2
+
+        VisualizationGroupI vg = vgmgr.createVisualizationGroup();
+        vgmgr.removeVisualizationGroup(vg);
+        assertEquals(0, vgmgr.getAllVisualizationGroups().size());
+
+        ReplicateGroupI rg1 = vgmgr.createReplicateGroup();
+        ReplicateGroupI rg2 = vgmgr.createReplicateGroup();
+
+        ReplicateI rg1r1 = vgmgr.createReplicate(rg1);
+        ReplicateI rg2r1 = vgmgr.createReplicate(rg2);
+
+        rg1r1.addSeqRun(seqrun);
+        rg2r1.addSeqRun(seqrun);
+
+        vgmgr.registerResolver(new DummyResolver());
+
+        boolean ok = vgmgr.selectAttributeType(AttributeRank.PRIMARY, "NCBI_GENUS");
+        assertTrue(ok);
+
+        List<Pair<VisualizationGroupI, DistributionI<Long>>> dists = vgmgr.getDistributions();
+        List<Pair<VisualizationGroupI, TreeI<Long>>> trees = vgmgr.getHierarchies();
+        assertEquals(2, dists.size());
+        assertEquals(2, trees.size());
+
+        vgmgr.removeReplicateGroup(rg1);
+
+        dists = vgmgr.getDistributions();
+        trees = vgmgr.getHierarchies();
+        assertEquals(1, dists.size());
+        assertEquals(1, trees.size());
+
+    }
+
+    private final class DummyResolver implements ConflictResolver {
+
+        @Override
+        public boolean resolve(List<VisualizationGroupI> vgroups) {
+            for (VisualizationGroupI vg : vgroups) {
+                List<Triple<AttributeRank, SeqRunI, Set<JobI>>> conflicts = vg.getConflicts();
+                for (Triple<AttributeRank, SeqRunI, Set<JobI>> t : conflicts) {
+                    // always resolve to conflict to first job
+                    vg.resolveConflict(AttributeRank.PRIMARY, t.getSecond(), t.getThird().toArray(new JobI[]{})[0]);
+                }
+            }
+            vgroups.clear();
+            return true;
+        }
+    }
 }
