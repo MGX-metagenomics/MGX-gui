f29b23e34a0d38ddd58edb23825957852d5266a3
Patrick Blumenkamp <patrick.blumenkamp@computational.bio.uni-giessen.de>
2016-05-10 17:28:02 +0200

    finish AddGoldstandard

--- MGX-Goldstandard/src/main/java/de/cebitec/mgx/gui/goldstandard/actions/AddGoldstandard.java
+++ MGX-Goldstandard/src/main/java/de/cebitec/mgx/gui/goldstandard/actions/AddGoldstandard.java
@@ -6,6 +6,7 @@ import de.cebitec.mgx.api.misc.Triple;
 import de.cebitec.mgx.api.model.AttributeI;
 import de.cebitec.mgx.api.model.JobI;
 import de.cebitec.mgx.api.model.JobParameterI;
+import de.cebitec.mgx.api.model.JobState;
 import de.cebitec.mgx.api.model.SeqRunI;
 import de.cebitec.mgx.api.model.SequenceI;
 import de.cebitec.mgx.api.model.ToolI;
@@ -18,6 +19,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import static javax.swing.Action.NAME;
 import org.openide.DialogDisplayer;
 import org.openide.WizardDescriptor;
@@ -43,9 +45,9 @@ public final class AddGoldstandard extends NodeAction implements LookupListener 
     private final static String TOOL_AUTHOR = "Patrick Blumenkamp";
     private final static String TOOL_LONG_DESCRIPTION = "fake tool for gold standard results";
     private final static String TOOL_WEBSITE = "";
-    private final static String TOOL_XML = "";
+    private final static String TOOL_XML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><graph description=\"\" name=\"Goldstandard\" service=\"MGX\"><composites/><nodes><node id=\"1\" name=\"\" type=\"Conveyor.MGX.GetMGXJob\" x=\"407\" y=\"162\"><configuration_items/><typeParameters/></node><node id=\"2\" name=\"\" type=\"Conveyor.Core.Discard\" x=\"412\" y=\"310\"><configuration_items/><typeParameters/></node></nodes><links><link from_connector=\"output\" from_node=\"1\" to_connector=\"input\" to_node=\"2\"/></links></graph>";
     private final static float TOOL_VERSION = 1.0f;
-
+    
     public AddGoldstandard() {
         this(Utilities.actionsGlobalContext());
     }
@@ -106,59 +108,21 @@ public final class AddGoldstandard extends NodeAction implements LookupListener 
         if (!cancelled) {            
             for (final SeqRunI seqrun : seqruns){
                 try {
-                    ToolI tool = master.Tool().create(TOOL_NAME, TOOL_LONG_DESCRIPTION, TOOL_AUTHOR, TOOL_WEBSITE, TOOL_VERSION, TOOL_XML);
+                    ToolI tool = getTool(master, TOOL_NAME, TOOL_LONG_DESCRIPTION, TOOL_AUTHOR, TOOL_WEBSITE, TOOL_VERSION, TOOL_XML);
                     Collection<JobParameterI> params = new ArrayList<>(1);
                     JobI job = master.Job().create(tool, seqrun, params);
                     MGSReader reader = new MGSReader(wd.getGoldstandardFile().getAbsolutePath(), master, job);
                     while (reader.hasNext()){
                         MGSEntry entry = reader.next();
-                        SequenceI seq = master.Sequence().fetch(seqrun, entry.getHeader());
+                        SequenceI seq = master.Sequence().fetch(seqrun, entry.getHeader().split(" ")[0]);
                         for (Triple<AttributeI, Integer, Integer> t : entry.getAttributes()){
                             master.Observation().create(seq, t.getFirst(), t.getSecond(), t.getThird());
                         }
                     }
-//            ToolI tool = (ToolI) wiz.getProperty(AnalysisWizardIterator.PROP_TOOL);
-//            ToolType tooltype = (ToolType) wiz.getProperty(AnalysisWizardIterator.PROP_TOOLTYPE);
-//            @SuppressWarnings(value = "unchecked")
-//            List<JobParameterI> params = (List<JobParameterI>) wiz.getProperty(AnalysisWizardIterator.PROP_PARAMETERS);
-//
-//            for (final SeqRunI seqrun : seqruns) {
-//                CountDownLatch toolIsCreated = new CountDownLatch(1);
-//
-//                // skip empty seqruns
-//                if (seqrun.getNumSequences() == 0) {
-//                    NotifyDescriptor d = new NotifyDescriptor.Message("Sequencing run has no sequences, skipping..");
-//                    DialogDisplayer.getDefault().notify(d);
-//                    continue;
-//                }
-//
-//                final SubmitTask sTask = new SubmitTask(tool, tooltype, seqrun, params, toolIsCreated);
-//                NonEDT.invoke(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        TaskManager.getInstance().addTask(sTask);
-//                    }
-//                });
-//
-//                // first submission will create a tool instance within the project, so all
-//                // subsequent invocations are project-based tools
-//                //
-//                // however, we will need to wait until the first task has finished processing
-//                // before we can be sure the tool has been added to the project
-//                //
-//                try {
-//                    toolIsCreated.await();
-//                } catch (InterruptedException ex) {
-//                    Exceptions.printStackTrace(ex);
-//                }
-//                tooltype = ToolType.PROJECT;
-//            }                
-                } catch (MGXException ex) {
-                    Exceptions.printStackTrace(ex);
-                } catch (FileNotFoundException ex) {
-                    Exceptions.printStackTrace(ex);
-                } catch (IOException ex) {
-                    Exceptions.printStackTrace(ex);
+                    job.setStatus(JobState.FINISHED);
+                    master.Job().update(job);
+                } catch (MGXException|IOException ex) {
+                    Exceptions.printStackTrace(ex);                    
                 }
             }
         }
@@ -173,60 +137,16 @@ public final class AddGoldstandard extends NodeAction implements LookupListener 
             numSeqs += sr.getNumSequences();
         }
         // make sure we don't accidentally start analysis on datasets without sequences
-        return super.isEnabled() && RBAC.isUser() && numSeqs > 0;
+        return super.isEnabled() && RBAC.isUser() && numSeqs > 0 && seqruns.size() == 1;
     }
 
-//    private final class SubmitTask extends MGXTask {
-//
-//        private final ToolI tool;
-//        private final SeqRunI run;
-//        private final ToolType tooltype;
-//        private final Collection<JobParameterI> params;
-//        private final CountDownLatch toolCreated;
-//
-//        public SubmitTask(ToolI tool, ToolType tooltype, SeqRunI run, Collection<JobParameterI> params, CountDownLatch toolCreated) {
-//            super("Submit " + run.getName() + " / " + tool.getName());
-//            this.tool = tool;
-//            this.tooltype = tooltype;
-//            this.params = params;
-//            this.run = run;
-//            this.toolCreated = toolCreated;
-//        }
-//
-//        @Override
-//        public boolean process() {
-//            final MGXMasterI master = tool.getMaster();
-//            try {
-//                ToolI selectedTool = null;
-//                switch (tooltype) {
-//                    case GLOBAL:
-//                        long projToolId = master.Tool().installTool(tool.getId());
-//                        tool.setId(projToolId);
-//                        selectedTool = master.Tool().fetch(projToolId);
-//                        break;
-//                    case PROJECT:
-//                        selectedTool = tool;
-//                        break;
-//                    case USER_PROVIDED:
-//                        master.Tool().create(tool);
-//                        selectedTool = tool;
-//                        break;
-//                    default:
-//                        assert false;
-//                }
-//                toolCreated.countDown();
-//                setStatus("Creating job..");
-//                JobI job = master.Job().create(selectedTool, run, params);
-//                setStatus("Validating configuration..");
-//                master.Job().verify(job);
-//                setStatus("Submitting..");
-//                return master.Job().execute(job);
-//            } catch (MGXException ex) {
-//                setStatus(ex.getMessage());
-//                failed(ex.getMessage());
-//                Exceptions.printStackTrace(ex);
-//            }
-//            return false;
-//        }
-//    }
+    private ToolI getTool(MGXMasterI master, String name, String desc, String author, String web, float version, String xml) throws MGXException {
+        Iterator<ToolI> tools = master.Tool().fetchall();
+        while(tools.hasNext()){
+            ToolI tool = tools.next();
+            if (tool.getName().equals(name))
+                return tool;
+        }
+        return master.Tool().create(name, desc, author, web, version, xml);
+    }
 }
