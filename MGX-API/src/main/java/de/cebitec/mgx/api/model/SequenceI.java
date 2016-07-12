953c09e510ad4ed5e77bc8e258f49707826f79fa
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-07-12 15:04:36 +0200

    rework sequence implementation and access; don't attach to MGXMaster instances as a propertychangelistener in order to avoid large amounts of listeners that never de-register

--- MGX-API/src/main/java/de/cebitec/mgx/api/model/SequenceI.java
+++ MGX-API/src/main/java/de/cebitec/mgx/api/model/SequenceI.java
@@ -3,22 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package de.cebitec.mgx.api.model;
-
-import de.cebitec.mgx.api.MGXMasterI;
-import java.awt.datatransfer.DataFlavor;
 
 /**
  *
  * @author sj
  */
-public abstract class SequenceI extends Identifiable<SequenceI> {
-    //
-    public static final DataFlavor DATA_FLAVOR = new DataFlavor(SequenceI.class, "SequenceI");
+public abstract class SequenceI implements Comparable<SequenceI> { //extends Identifiable<SequenceI> {
 
-    public SequenceI(MGXMasterI m) {
-        super(m, DATA_FLAVOR);
+    public static final long INVALID_IDENTIFIER = Identifiable.INVALID_IDENTIFIER;
+    protected long id = INVALID_IDENTIFIER;
+
+    public final void setId(long id) {
+        this.id = id;
+    }
+
+    public final long getId() {
+        return id;
     }
 
     public abstract String getName();
@@ -29,8 +30,29 @@ public abstract class SequenceI extends Identifiable<SequenceI> {
 
     public abstract void setSequence(String sequence);
 
-    public abstract int getLength() ;
+    public abstract int getLength();
 
     public abstract void setLength(int length);
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash = (int) (31 * hash + this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o != null && o instanceof Identifiable) {
+            Identifiable other = (Identifiable) o;
+
+            if ((this.id == INVALID_IDENTIFIER && other.getId() != INVALID_IDENTIFIER) || (this.id != INVALID_IDENTIFIER && this.id != other.getId())) {
+                return false;
+            }
+
+            return this.getId() == other.getId();
+        }
+        return false;
+    }
+
 }
