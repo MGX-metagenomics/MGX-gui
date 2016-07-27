5cbc15de5c107e75e52293bc629439f9c509a91f
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2016-07-27 15:06:56 +0200

    bugfix: make sure exclude filter is only applied after a distribution is converted to fractions

--- MGX-VizFilter/src/main/java/de/cebitec/mgx/gui/vizfilter/ExcludeFilter.java
+++ MGX-VizFilter/src/main/java/de/cebitec/mgx/gui/vizfilter/ExcludeFilter.java
@@ -19,43 +19,43 @@ import java.util.Set;
  *
  * @author sj
  */
-public class ExcludeFilter<T extends Number> implements VisFilterI<DistributionI<T>, DistributionI<T>> {
+public class ExcludeFilter implements VisFilterI<DistributionI<Double>, DistributionI<Double>> {
 
     private final Set<AttributeI> blacklist;
 
     public ExcludeFilter(Set<AttributeI> blacklist) {
         if (blacklist == null) {
-           throw new IllegalArgumentException();
+            throw new IllegalArgumentException();
         }
         this.blacklist = Collections.unmodifiableSet(blacklist);
     }
 
     @Override
-    public List<Pair<VisualizationGroupI, DistributionI<T>>> filter(List<Pair<VisualizationGroupI, DistributionI<T>>> in) {
-        List<Pair<VisualizationGroupI, DistributionI<T>>> ret = new ArrayList<>(in.size());
-        for (Pair<VisualizationGroupI, DistributionI<T>> p : in) {
-            DistributionI<T> filteredDist = filterDist(p.getSecond());
+    public List<Pair<VisualizationGroupI, DistributionI<Double>>> filter(List<Pair<VisualizationGroupI, DistributionI<Double>>> in) {
+        List<Pair<VisualizationGroupI, DistributionI<Double>>> ret = new ArrayList<>(in.size());
+        for (Pair<VisualizationGroupI, DistributionI<Double>> p : in) {
+            DistributionI<Double> filteredDist = filterDist(p.getSecond());
             ret.add(new Pair<>(p.getFirst(), filteredDist));
         }
         return ret;
     }
 
     @SuppressWarnings("unchecked")
-    public DistributionI<T> filterDist(DistributionI<T> dist) {
-        Map<AttributeI, T> data = new HashMap<>();
-        for (Entry<AttributeI, T> e : dist.entrySet()) {
+    public DistributionI<Double> filterDist(DistributionI<Double> dist) {
+        Map<AttributeI, Double> data = new HashMap<>();
+        for (Entry<AttributeI, Double> e : dist.entrySet()) {
             if (!blacklist.contains(e.getKey())) {
                 data.put(e.getKey(), e.getValue());
             }
         }
-        DistributionI<T> ret = null;
-        if (dist.getEntryType().equals(Long.class)) {
-            Map<AttributeI, Long> tmp = (Map<AttributeI, Long>) data;
-            ret = (DistributionI<T>) new Distribution(dist.getMaster(), tmp);
-        } else if (dist.getEntryType().equals(Double.class)) {
-              Map<AttributeI, Double> tmp = (Map<AttributeI, Double>) data;
-            ret =  (DistributionI<T>) new NormalizedDistribution(dist.getMaster(), tmp, dist.getTotalClassifiedElements());
-        }
+        DistributionI<Double> ret = null;
+//        if (dist.getEntryType().equals(Long.class)) {
+//            Map<AttributeI, Long> tmp = (Map<AttributeI, Long>) data;
+//            ret = (DistributionI<Double>) new Distribution(dist.getMaster(), tmp);
+//        } else if (dist.getEntryType().equals(Double.class)) {
+        Map<AttributeI, Double> tmp = (Map<AttributeI, Double>) data;
+        ret = (DistributionI<Double>) new NormalizedDistribution(dist.getMaster(), tmp, dist.getTotalClassifiedElements());
+//        }
         return ret;
     }
 }
--- MGX-VizFilter/src/test/java/de/cebitec/mgx/gui/vizfilter/ExcludeFilterTest.java
+++ MGX-VizFilter/src/test/java/de/cebitec/mgx/gui/vizfilter/ExcludeFilterTest.java
@@ -5,12 +5,16 @@
  */
 package de.cebitec.mgx.gui.vizfilter;
 
+import de.cebitec.mgx.api.groups.VisualizationGroupI;
 import de.cebitec.mgx.api.misc.DistributionI;
+import de.cebitec.mgx.api.misc.Pair;
 import de.cebitec.mgx.api.model.AttributeI;
 import de.cebitec.mgx.gui.datamodel.Attribute;
 import de.cebitec.mgx.gui.datamodel.misc.Distribution;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.junit.Test;
@@ -34,17 +38,23 @@ public class ExcludeFilterTest {
         a2.setValue("BAR");
         map.put(a2, Long.valueOf(5));
         DistributionI<Long> dist = new Distribution(null, map);
-         assertEquals(2, dist.size());
-        
+        assertEquals(2, dist.size());
+
         Set<AttributeI> blacklist = new HashSet<>();
         blacklist.add(a2);
-        
-        ExcludeFilter<Long> ef = new ExcludeFilter<>(blacklist);
+
+        ExcludeFilter ef = new ExcludeFilter(blacklist);
         assertNotNull(ef);
+
+        List<Pair<VisualizationGroupI, DistributionI<Long>>> xx = new ArrayList<>();
+        VisualizationGroupI vg = null;
+        xx.add(new Pair<>(vg, dist));
+
+        List<Pair<VisualizationGroupI, DistributionI<Double>>> yy = new LongToDouble().filter(xx);
         
-        DistributionI<Long> filtered = ef.filterDist(dist);
+        DistributionI<Double> filtered = ef.filterDist(yy.get(0).getSecond());
         assertEquals(1, filtered.size());
-        
+
         assertNotSame(filtered, dist);
     }
 
