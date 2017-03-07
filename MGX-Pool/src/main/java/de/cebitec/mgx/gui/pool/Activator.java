fa624475f967155a29c895e4cd1551a0b6d17b7f
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2017-03-07 14:12:04 +0100

    add modular execution pool

---
+++ MGX-Pool/src/main/java/de/cebitec/mgx/gui/pool/Activator.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.pool;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ *
+ * @author sj
+ */
+public class Activator implements BundleActivator {
+
+    ServiceRegistration<?> registerService;
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        if (MGXPool.isUpAndRunning()) {
+            MGXPool.getInstance().shutdown();
+            Logger.getLogger(MGXPool.class.getPackage().getName()).log(Level.INFO, "MGX processing pool shut down, {0} tasks completed.", MGXPool.completedTaskNum());
+        }
+    }
+
+}
