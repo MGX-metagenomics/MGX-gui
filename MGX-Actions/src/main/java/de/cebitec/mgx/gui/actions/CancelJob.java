f72bf634151ce084ffbfda7f3ef8aabcbbd1410d
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2017-03-29 17:11:53 +0200

    fix EDT block in Job#cancel; closes #96

--- MGX-Actions/src/main/java/de/cebitec/mgx/gui/actions/CancelJob.java
+++ MGX-Actions/src/main/java/de/cebitec/mgx/gui/actions/CancelJob.java
@@ -5,7 +5,6 @@
  */
 package de.cebitec.mgx.gui.actions;
 
-import de.cebitec.mgx.api.MGXMasterI;
 import de.cebitec.mgx.api.model.JobI;
 import de.cebitec.mgx.api.model.JobState;
 import de.cebitec.mgx.gui.rbac.RBAC;
@@ -24,30 +23,35 @@ import org.openide.util.Utilities;
 public class CancelJob extends AbstractAction {
 
     public CancelJob() {
-        putValue(NAME, "Cancel");
+        super.putValue(NAME, "Cancel");
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         final JobI job = Utilities.actionsGlobalContext().lookup(JobI.class);
-        final MGXMasterI m = job.getMaster();
         SwingWorker<Boolean, Void> sw = new SwingWorker<Boolean, Void>() {
             @Override
             protected Boolean doInBackground() throws Exception {
-                return m.Job().cancel(job);
+                return job.getMaster().Job().cancel(job);
             }
+
+            @Override
+            protected void done() {
+                super.done();
+                try {
+                    get();
+                } catch (InterruptedException | ExecutionException ex) {
+                    String msg = ex.getMessage();
+                    if (ex.getCause() != null && ex.getCause().getMessage() != null) {
+                        msg = ex.getCause().getMessage();
+                    }
+                    NotifyDescriptor nd = new NotifyDescriptor("Could not cancel job: " + msg, "Job cancellation failed", NotifyDescriptor.OK_CANCEL_OPTION, NotifyDescriptor.ERROR_MESSAGE, null, null);
+                    DialogDisplayer.getDefault().notifyLater(nd);
+                }
+            }
+
         };
         sw.execute();
-        try {
-            sw.get();
-        } catch (InterruptedException | ExecutionException ex) {
-            String msg = ex.getMessage();
-            if (ex.getCause() != null && ex.getCause().getMessage() != null) {
-                msg = ex.getCause().getMessage();
-            }
-            NotifyDescriptor nd = new NotifyDescriptor("Could not cancel job: " + msg, "Job cancellation failed", NotifyDescriptor.OK_CANCEL_OPTION, NotifyDescriptor.ERROR_MESSAGE, null, null);
-            DialogDisplayer.getDefault().notifyLater(nd);
-        }
     }
 
     @Override
@@ -56,5 +60,5 @@ public class CancelJob extends AbstractAction {
         JobState state = job.getStatus();
         return super.isEnabled() && RBAC.isUser() && !(state.equals(JobState.FINISHED) || state.equals(JobState.FAILED) || state.equals(JobState.IN_DELETION) || state.equals(JobState.ABORTED));
     }
-    
+
 }
