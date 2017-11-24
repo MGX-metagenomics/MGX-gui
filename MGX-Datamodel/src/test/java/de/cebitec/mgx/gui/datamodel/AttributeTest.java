2c58f2ae4ce85c1b4122e7720a8375fa6272ac6d
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2017-11-24 13:42:26 +0100

    add basic equality tests for attributes

---
+++ MGX-Datamodel/src/test/java/de/cebitec/mgx/gui/datamodel/AttributeTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.mgx.gui.datamodel;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sj
+ */
+public class AttributeTest {
+
+    @Test
+    public void testEquals() {
+        System.out.println("testEquals");
+        Attribute a1 = new Attribute();
+        a1.setValue("FOO");
+        Attribute a2 = new Attribute();
+        a2.setValue("FOO");
+        assertEquals(a1, a2);
+    }
+
+    @Test
+    public void testNotEquals() {
+        System.out.println("testNotEquals");
+        Attribute a1 = new Attribute();
+        a1.setValue("FOO");
+        Attribute a2 = new Attribute();
+        a2.setValue("BAR");
+        assertNotEquals(a1, a2);
+    }
+
+}
