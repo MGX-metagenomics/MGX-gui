46cd79988a3feab0813bc2cc2a05f9c2844c6fc2
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2021-04-28 13:16:11 +0200

    disable java version check

--- MGX-Login/src/main/java/de/cebitec/mgx/gui/login/dialog/LoginHandler.java
+++ MGX-Login/src/main/java/de/cebitec/mgx/gui/login/dialog/LoginHandler.java
@@ -44,9 +44,9 @@ public class LoginHandler implements ActionListener {
     }
 
     public void showDialog(GPMSClientI gpmsClient) {
-        if (!checkVersion()) {
-            return;
-        }
+//        if (!checkVersion()) {
+//            return;
+//        }
         //dialog = new DialogDescriptor(panel, "Login", true, this);
         dialog.setValid(false);
         dialog.setClosingOptions(new Object[]{DialogDescriptor.CANCEL_OPTION, DialogDescriptor.OK_OPTION});
@@ -85,18 +85,18 @@ public class LoginHandler implements ActionListener {
 
     }
 
-    private static boolean checkVersion() {
-        String version = System.getProperty("java.version");
-        float ver = Float.valueOf(version.substring(0, 3));
-        if (ver < 1.7) {
-            String msg = "Your Java runtime version (" + version + ") is too old. MGX requires at least Java 7.";
-            NotifyDescriptor nd = new NotifyDescriptor(msg, "Java too old",
-                    NotifyDescriptor.OK_CANCEL_OPTION, NotifyDescriptor.ERROR_MESSAGE, null, null);
-            DialogDisplayer.getDefault().notify(nd);
-            return false;
-        }
-        return true;
-    }
+//    private static boolean checkVersion() {
+//        String version = System.getProperty("java.version");
+//        float ver = Float.valueOf(version.substring(0, 3));
+//        if (ver < 1.7) {
+//            String msg = "Your Java runtime version (" + version + ") is too old. MGX requires at least Java 7.";
+//            NotifyDescriptor nd = new NotifyDescriptor(msg, "Java too old",
+//                    NotifyDescriptor.OK_CANCEL_OPTION, NotifyDescriptor.ERROR_MESSAGE, null, null);
+//            DialogDisplayer.getDefault().notify(nd);
+//            return false;
+//        }
+//        return true;
+//    }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
