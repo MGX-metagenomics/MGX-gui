a89f8ff6090dc0b6e6679f57c66b603753dcfd78
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2022-03-22 10:55:11 +0100

    rm unused import

--- MGX-MappingViewer/src/main/java/de/cebitec/mgx/gui/mapping/viewer/IdentityHistogramTopComponent.java
+++ MGX-MappingViewer/src/main/java/de/cebitec/mgx/gui/mapping/viewer/IdentityHistogramTopComponent.java
@@ -19,7 +19,6 @@ import org.knowm.xchart.style.markers.Marker;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 import org.netbeans.api.settings.ConvertAsProperties;
 import org.openide.awt.ActionID;
-import org.openide.awt.ActionReference;
 import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.LookupEvent;
