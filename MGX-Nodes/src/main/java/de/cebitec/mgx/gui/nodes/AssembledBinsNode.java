bf4709c338a2429e3052f284774622064b76c081
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-02-15 17:32:49 +0100

    #141: extend lookup of AssembledBinsNode

--- MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/AssembledBinsNode.java
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/AssembledBinsNode.java
@@ -4,6 +4,7 @@ import de.cebitec.mgx.api.MGXMasterI;
 import de.cebitec.mgx.api.model.assembly.AssemblyI;
 import de.cebitec.mgx.gui.nodefactory.BinNodeFactory;
 import javax.swing.Action;
+import org.openide.filesystems.FileUtil;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
 import org.openide.util.lookup.Lookups;
@@ -15,11 +16,11 @@ import org.openide.util.lookup.Lookups;
 public class AssembledBinsNode extends AbstractNode { //MGXNodeBase<MGXMasterI> {
 
     public AssembledBinsNode(MGXMasterI m, AssemblyI asm) {
-        this(m, new BinNodeFactory(asm));
+        this(m, asm, new BinNodeFactory(asm));
     }
 
-    private AssembledBinsNode(MGXMasterI m, BinNodeFactory bnf) {
-        super(Children.create(bnf, true), Lookups.fixed(m));
+    private AssembledBinsNode(MGXMasterI m, AssemblyI asm, BinNodeFactory bnf) {
+        super(Children.create(bnf, true), Lookups.fixed(m, asm));
         super.setDisplayName("Bins");
         setIconBaseWithExtension("de/cebitec/mgx/gui/nodes/ProjectData.png");
     }
@@ -31,7 +32,8 @@ public class AssembledBinsNode extends AbstractNode { //MGXNodeBase<MGXMasterI> 
 
     @Override
     public Action[] getActions(boolean context) {
-        return new Action[]{};
+        Action binTable = FileUtil.getConfigObject("Actions/File/de-cebitec-mgx-gui-bintable-BinTableAction.instance", Action.class);
+        return new Action[]{binTable};
     }
 
 }
