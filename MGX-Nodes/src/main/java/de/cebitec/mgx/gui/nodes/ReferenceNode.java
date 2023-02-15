f87b77eba2ca400a68fac73c864ee371371869e6
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-02-15 12:16:27 +0100

    #106: add reference genome icon

--- MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/ReferenceNode.java
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/ReferenceNode.java
@@ -3,6 +3,7 @@ package de.cebitec.mgx.gui.nodes;
 import de.cebitec.mgx.gui.actions.DeleteReference;
 import de.cebitec.mgx.api.model.MGXReferenceI;
 import de.cebitec.mgx.gui.actions.OpenMappingByReference;
+import java.awt.Image;
 import java.text.NumberFormat;
 import java.util.Locale;
 import javax.swing.Action;
@@ -19,8 +20,15 @@ public class ReferenceNode extends MGXNodeBase<MGXReferenceI> {
     public ReferenceNode(MGXReferenceI ref) {
         super(Children.LEAF, Lookups.fixed(ref.getMaster(), ref), ref);
         super.setDisplayName(ref.getName());
-        super.setIconBaseWithExtension("de/cebitec/mgx/gui/nodes/Habitat.png");
+        super.setIconBaseWithExtension("de/cebitec/mgx/gui/nodes/Genome.png");
         super.setShortDescription(getToolTipText(ref));
+    }
+
+    @Override
+    public Image getIcon(int type) {
+        Image image = super.getIcon(type);
+        Image scaledInstance = image.getScaledInstance(16, 16, Image.SCALE_SMOOTH);
+        return scaledInstance;
     }
 
     private String getToolTipText(MGXReferenceI ref) {
@@ -39,7 +47,7 @@ public class ReferenceNode extends MGXNodeBase<MGXReferenceI> {
     @Override
     public void updateModified() {
         setDisplayName(getContent().getName());
-        setIconBaseWithExtension("de/cebitec/mgx/gui/nodes/Habitat.png");
+        setIconBaseWithExtension("de/cebitec/mgx/gui/nodes/Genome.png");
         setShortDescription(getToolTipText(getContent()));
     }
 
