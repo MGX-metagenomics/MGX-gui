fdc0e2151efc02370fcf7cd5742d5b14e39b6df2
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-02-16 13:21:15 +0100

    #153: log OSGi service registration

--- MGX-Controller/src/main/java/de/cebitec/mgx/gui/controller/Activator.java
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/controller/Activator.java
@@ -1,10 +1,7 @@
 package de.cebitec.mgx.gui.controller;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/OSGi/Activator.java to edit this template
- */
 import de.cebitec.mgx.api.ControllerCreatorI;
+import java.util.logging.Logger;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
@@ -15,16 +12,20 @@ import org.osgi.framework.ServiceRegistration;
  */
 public class Activator implements BundleActivator {
 
+    private static final Logger LOG = Logger.getLogger(Activator.class.getPackage().getName());
+    
     private ServiceRegistration<?> registerService;
 
     @Override
     public void start(BundleContext context) throws Exception {
         registerService = context.registerService(ControllerCreatorI.class.getName(), new ControllerCreatorImpl(), null);
+        LOG.info("Registered MGX 2 controller.");
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
         context.ungetService(registerService.getReference());
+        LOG.info("Unregistered MGX 2 controller.");
     }
 
 }
