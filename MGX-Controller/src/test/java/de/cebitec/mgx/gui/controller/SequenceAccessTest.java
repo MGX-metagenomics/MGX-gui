7f2b8a994407832e6024b32cf3cdc4643dc1ec0f
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-02-16 13:49:27 +0100

    extend tests

--- MGX-Controller/src/test/java/de/cebitec/mgx/gui/controller/SequenceAccessTest.java
+++ MGX-Controller/src/test/java/de/cebitec/mgx/gui/controller/SequenceAccessTest.java
@@ -6,10 +6,14 @@
 package de.cebitec.mgx.gui.controller;
 
 import de.cebitec.mgx.api.MGXMasterI;
+import de.cebitec.mgx.api.exception.MGXException;
 import de.cebitec.mgx.api.model.SeqRunI;
 import de.cebitec.mgx.api.model.SequenceI;
 import de.cebitec.mgx.testutils.TestMaster;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -34,5 +38,36 @@ public class SequenceAccessTest {
         }
         start = System.currentTimeMillis() - start;
         System.err.println("duration in ms: " + start);
+    }
+
+    @Test
+    public void fetchValidID() {
+        System.out.println("fetchValidID");
+        MGXMasterI master = TestMaster.getRO();
+        SequenceI seq = null;
+        try {
+            seq = master.Sequence().fetch(2109905);
+        } catch (MGXException ex) {
+            fail(ex.getMessage());
+        }
+        assertNotNull(seq);
+        assertEquals("FI5LW4G01DZDXZ", seq.getName());
+    }
+
+    @Test
+    public void fetchInvalidID() {
+        System.out.println("fetchInvalidID");
+        MGXMasterI master = TestMaster.getRO();
+        SequenceI seq = null;
+        try {
+            seq = master.Sequence().fetch(1);
+        } catch (MGXException ex) {
+            if (ex.getMessage().contains("No object of type Sequence for ID 1")) {
+                // ok
+            } else {
+                fail(ex.getMessage());
+            }
+        }
+        assertNull(seq);
     }
 }
