1d14a35f5428b31efbb618b61c3d252c5060b659
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-02-23 13:43:27 +0100

    remove job cache

--- MGX-Controller/src/main/java/de/cebitec/mgx/gui/dtoconversion/JobDTOFactory.java
+++ MGX-Controller/src/main/java/de/cebitec/mgx/gui/dtoconversion/JobDTOFactory.java
@@ -1,7 +1,5 @@
 package de.cebitec.mgx.gui.dtoconversion;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import de.cebitec.mgx.api.MGXMasterI;
 import de.cebitec.mgx.api.model.JobI;
 import de.cebitec.mgx.api.model.JobParameterI;
@@ -13,9 +11,6 @@ import de.cebitec.mgx.dto.dto.JobDTO.Builder;
 import de.cebitec.mgx.dto.dto.JobParameterDTO;
 import de.cebitec.mgx.gui.datamodel.Job;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -27,13 +22,8 @@ public class JobDTOFactory extends DTOConversionBase<JobI, JobDTO> {
         instance = new JobDTOFactory();
     }
     protected static JobDTOFactory instance;
-    private final Cache<CacheKey, JobI> instanceCache;
 
     private JobDTOFactory() {
-        instanceCache = CacheBuilder.<CacheKey, JobI>newBuilder()
-                .expireAfterAccess(1, TimeUnit.MINUTES)
-                .concurrencyLevel(10)
-                .build();
     }
 
     public static JobDTOFactory getInstance() {
@@ -48,7 +38,6 @@ public class JobDTOFactory extends DTOConversionBase<JobI, JobDTO> {
                 .setCreator(j.getCreator())
                 .setState(dto.JobState.forNumber(j.getStatus().getValue()));
 
-        
         // either assembly or seqruns, but not both
         if (j.getAssembly() != null) {
             b.setAssemblyId(j.getAssembly().getId());
@@ -74,71 +63,21 @@ public class JobDTOFactory extends DTOConversionBase<JobI, JobDTO> {
     @Override
     public final JobI toModel(MGXMasterI m, JobDTO dto) {
 
-        JobI job = instanceCache.getIfPresent(new CacheKey(dto.getId(), m));
+        JobI job = new Job(m)
+                .setStatus(JobState.values()[dto.getState().ordinal()])
+                .setCreator(dto.getCreator())
+                .setStartDate(toDate(dto.getStartDate()))
+                .setFinishDate(toDate(dto.getFinishDate()));
 
-        if (job != null) {
-            job.setStatus(JobState.values()[dto.getState().ordinal()]);
-            job.setStartDate(new Date(1000L * dto.getStartDate()));
-            job.setFinishDate(new Date(1000L * dto.getFinishDate()));
-        } else {
-            job = new Job(m)
-                    .setStatus(JobState.values()[dto.getState().ordinal()])
-                    .setCreator(dto.getCreator())
-                    .setStartDate(toDate(dto.getStartDate()))
-                    .setFinishDate(toDate(dto.getFinishDate()));
-
-            if (dto.hasParameters()) {
-                job.setParameters(new ArrayList<>(dto.getParameters().getParameterCount()));
-                for (JobParameterDTO jpdto : dto.getParameters().getParameterList()) {
-                    JobParameterI jp = JobParameterDTOFactory.getInstance().toModel(m, jpdto);
-                    job.getParameters().add(jp);
-                }
+        if (dto.hasParameters()) {
+            job.setParameters(new ArrayList<>(dto.getParameters().getParameterCount()));
+            for (JobParameterDTO jpdto : dto.getParameters().getParameterList()) {
+                JobParameterI jp = JobParameterDTOFactory.getInstance().toModel(m, jpdto);
+                job.getParameters().add(jp);
             }
-            job.setId(dto.getId());
-            instanceCache.put(new CacheKey(dto.getId(), m), job);
         }
+        job.setId(dto.getId());
 
         return job;
-    }
-
-    private final static class CacheKey {
-
-        private final long jobId;
-        private final MGXMasterI master;
-
-        public CacheKey(long jobId, MGXMasterI master) {
-            this.jobId = jobId;
-            this.master = master;
-        }
-
-        @Override
-        public int hashCode() {
-            int hash = 7;
-            hash = 97 * hash + (int) (this.jobId ^ (this.jobId >>> 32));
-            hash = 97 * hash + Objects.hashCode(this.master);
-            return hash;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final CacheKey other = (CacheKey) obj;
-            if (this.jobId != other.jobId) {
-                return false;
-            }
-            if (!Objects.equals(this.master, other.master)) {
-                return false;
-            }
-            return true;
-        }
-
     }
 }
