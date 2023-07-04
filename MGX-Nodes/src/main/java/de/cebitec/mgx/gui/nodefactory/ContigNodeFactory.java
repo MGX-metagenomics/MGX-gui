3b42bdd415f165c33887c734fc1a3706b6e4fe88
Sebastian Jaenicke <sjaenick@CeBiTec.Uni-Bielefeld.DE>
2023-07-04 15:26:40 +0200

    disable (unused) contignodes

--- MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodefactory/ContigNodeFactory.java
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodefactory/ContigNodeFactory.java
@@ -1,39 +1,39 @@
-package de.cebitec.mgx.gui.nodefactory;
-
-import de.cebitec.mgx.api.exception.MGXException;
-import de.cebitec.mgx.api.model.assembly.BinI;
-import de.cebitec.mgx.api.model.assembly.ContigI;
-import de.cebitec.mgx.gui.nodes.ContigNode;
-import java.util.Iterator;
-import java.util.List;
-import org.openide.util.Exceptions;
-
-/**
- *
- * @author sj
- */
-public class ContigNodeFactory extends MGX2NodeFactoryBase<BinI, ContigI> {
-
-    public ContigNodeFactory(BinI a) {
-        super(a);
-    }
-
-    @Override
-    protected boolean addKeys(List<ContigI> toPopulate) {
-        try {
-            Iterator<ContigI> iter = getMaster().Contig().ByBin(getContent());
-            while (iter.hasNext()) {
-                toPopulate.add(iter.next());
-            }
-            return true;
-        } catch (MGXException ex) {
-            Exceptions.printStackTrace(ex);
-        }
-        return true;
-    }
-
-    @Override
-    protected ContigNode createNodeFor(ContigI key) {
-        return new ContigNode(key);
-    }
-}
+//package de.cebitec.mgx.gui.nodefactory;
+//
+//import de.cebitec.mgx.api.exception.MGXException;
+//import de.cebitec.mgx.api.model.assembly.BinI;
+//import de.cebitec.mgx.api.model.assembly.ContigI;
+//import de.cebitec.mgx.gui.nodes.ContigNode;
+//import java.util.Iterator;
+//import java.util.List;
+//import org.openide.util.Exceptions;
+//
+///**
+// *
+// * @author sj
+// */
+//public class ContigNodeFactory extends MGX2NodeFactoryBase<BinI, ContigI> {
+//
+//    public ContigNodeFactory(BinI a) {
+//        super(a);
+//    }
+//
+//    @Override
+//    protected boolean addKeys(List<ContigI> toPopulate) {
+//        try {
+//            Iterator<ContigI> iter = getMaster().Contig().ByBin(getContent());
+//            while (iter.hasNext()) {
+//                toPopulate.add(iter.next());
+//            }
+//            return true;
+//        } catch (MGXException ex) {
+//            Exceptions.printStackTrace(ex);
+//        }
+//        return true;
+//    }
+//
+//    @Override
+//    protected ContigNode createNodeFor(ContigI key) {
+//        return new ContigNode(key);
+//    }
+//}
--- MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/ContigNode.java
+++ MGX-Nodes/src/main/java/de/cebitec/mgx/gui/nodes/ContigNode.java
@@ -1,50 +1,50 @@
-package de.cebitec.mgx.gui.nodes;
-
-import de.cebitec.mgx.api.model.assembly.ContigI;
-import java.text.NumberFormat;
-import java.util.Locale;
-import javax.swing.Action;
-import org.openide.nodes.Children;
-import org.openide.util.lookup.Lookups;
-
-/**
- *
- * @author sj
- */
-public class ContigNode extends MGXNodeBase<ContigI> {
-
-    public ContigNode(ContigI a) {
-        super(Children.LEAF, Lookups.fixed(a.getMaster(), a), a);
-        super.setDisplayName(a.getName());
-        //super.setIconBaseWithExtension("de/cebitec/mgx/gui/nodes/Habitat.png");
-        super.setShortDescription(getToolTipText(a));
-    }
-
-    private String getToolTipText(ContigI h) {
-        return new StringBuilder("<html>").append("<b>Contig: </b>")
-                .append(h.getName())
-                .append("<br><hr><br>")
-                .append("Length: ")
-                .append(NumberFormat.getInstance(Locale.US).format(h.getLength()))
-                .append(" bp<br>")
-                .append("GC: ")
-                .append(String.format(Locale.US, "%.2f", h.getGC()))
-                .append("%<br>Coverage: ")
-                .append(NumberFormat.getInstance(Locale.US).format(h.getCoverage()))
-                .append("<br>Predicted features: ")
-                .append(h.getPredictedSubregions())
-                .append("</html>").toString();
-    }
-
-    @Override
-    public Action[] getActions(boolean context) {
-        return new Action[]{};
-    }
-
-    @Override
-    public void updateModified() {
-        setDisplayName("contig" + getContent().getId());
-        //setIconBaseWithExtension("de/cebitec/mgx/gui/nodes/Habitat.png");
-        setShortDescription(getToolTipText(getContent()));
-    }
-}
+//package de.cebitec.mgx.gui.nodes;
+//
+//import de.cebitec.mgx.api.model.assembly.ContigI;
+//import java.text.NumberFormat;
+//import java.util.Locale;
+//import javax.swing.Action;
+//import org.openide.nodes.Children;
+//import org.openide.util.lookup.Lookups;
+//
+///**
+// *
+// * @author sj
+// */
+//public class ContigNode extends MGXNodeBase<ContigI> {
+//
+//    public ContigNode(ContigI a) {
+//        super(Children.LEAF, Lookups.fixed(a.getMaster(), a), a);
+//        super.setDisplayName(a.getName());
+//        //super.setIconBaseWithExtension("de/cebitec/mgx/gui/nodes/Habitat.png");
+//        super.setShortDescription(getToolTipText(a));
+//    }
+//
+//    private String getToolTipText(ContigI h) {
+//        return new StringBuilder("<html>").append("<b>Contig: </b>")
+//                .append(h.getName())
+//                .append("<br><hr><br>")
+//                .append("Length: ")
+//                .append(NumberFormat.getInstance(Locale.US).format(h.getLength()))
+//                .append(" bp<br>")
+//                .append("GC: ")
+//                .append(String.format(Locale.US, "%.2f", h.getGC()))
+//                .append("%<br>Coverage: ")
+//                .append(NumberFormat.getInstance(Locale.US).format(h.getCoverage()))
+//                .append("<br>Predicted features: ")
+//                .append(h.getPredictedSubregions())
+//                .append("</html>").toString();
+//    }
+//
+//    @Override
+//    public Action[] getActions(boolean context) {
+//        return new Action[]{};
+//    }
+//
+//    @Override
+//    public void updateModified() {
+//        setDisplayName("contig" + getContent().getId());
+//        //setIconBaseWithExtension("de/cebitec/mgx/gui/nodes/Habitat.png");
+//        setShortDescription(getToolTipText(getContent()));
+//    }
+//}
